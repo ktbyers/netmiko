5951f6be8e7d8d022108b6562d0edd4b50ceb3b1
Kirk Byers <ktbyers@twb-tech.com>
2014-11-30 20:17:53 -0800

    Migrating to pytest

---
+++ tests/test_cisco_ios.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import pytest
+
+import netmiko
+from DEVICE_CREDS import *
+
+
+def setup_module(module):
+
+    module.EXPECTED_RESPONSES = {
+        'router_name'   : 'pynet-rtr1',
+        'router_prompt' : 'pynet-rtr1>',
+        'router_enable' : 'pynet-rtr1#',
+        'interface_ip'  : '10.220.88.20'
+    }
+    
+    show_ver_command = 'show version'
+    module.basic_command = 'show ip int brief'
+    
+    print "\n"
+    print "#" * 80
+    SSHClass = netmiko.ssh_dispatcher(cisco_881['device_type'])
+    net_connect = SSHClass(**cisco_881)
+    module.show_version = net_connect.send_command(show_ver_command)
+    module.show_ip = net_connect.send_command(module.basic_command)
+    module.router_name = net_connect.router_name
+
+
+def test_disable_paging():
+    '''
+    Verify paging is disabled by looking for string after when paging would
+    normally occur
+    '''
+    assert 'Configuration register is' in show_version
+
+
+def test_verify_ssh_connect():
+    '''
+    Verify the connection was established successfully
+    '''
+    assert 'Cisco IOS Software' in show_version
+
+
+def test_verify_send_command():
+    '''
+    Verify a command can be sent down the channel successfully
+    '''
+    assert EXPECTED_RESPONSES['interface_ip'] in show_ip
+
+
+def test_find_prompt():
+    '''
+    Verify the router prompt is detected correctly
+    '''
+    assert router_name == EXPECTED_RESPONSES['router_name']
+
+
+def test_strip_prompt():
+    '''
+    Ensure the router prompt is not in the command output
+    '''
+    assert EXPECTED_RESPONSES['router_prompt'] not in show_ip
+
+
+def test_strip_command():
+    '''
+    Ensure that the command that was executed does not show up in the 
+    command output
+    '''
+    assert basic_command not in show_ip
+
+
+def test_normalize_linefeeds():
+    '''
+    Ensure no '\r\n' sequences
+    '''
+    assert not '\r\n' in show_version
+
+
