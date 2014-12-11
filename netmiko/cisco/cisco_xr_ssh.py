35015705d4765224e659b00deb875b55623ae6f5
plucena24 <plucena24@gmail.com>
2014-12-11 18:44:33 -0500

    changed spacings in the xr module for better readability

--- netmiko/cisco/cisco_xr_ssh.py
+++ netmiko/cisco/cisco_xr_ssh.py
@@ -21,7 +21,8 @@ class CiscoXrSSH(SSHConnection):
             self.config_mode()
             output += self.send_command('\n', strip_prompt=False, strip_command=False)
 
-        output += ''.join(self.send_command(a_command, strip_prompt=False, strip_command=False) for a_command in config_commands)
+        output += ''.join(self.send_command(a_command, strip_prompt=False,
+                          strip_command=False) for a_command in config_commands)
 
         if commit is True:
             output += self.commit()
@@ -42,11 +43,14 @@ class CiscoXrSSH(SSHConnection):
 
         output = ''
 
-        output = ''.join(self.send_command(a_command, strip_prompt=False, strip_command=False) for a_command in commit_cmd)
+        output = ''.join(self.send_command(a_command, strip_prompt=False,
+                         strip_command=False) for a_command in commit_cmd)
 
         if "Failed to commit one or more configuration" in output:
-            fail_msg = self.send_command('show configuration failed', strip_prompt=False, strip_command=False)
-            raise ValueError('Commit failed with the following errors:\n\n {fail_msg}'.format(fail_msg=fail_msg))
+            fail_msg = self.send_command('show configuration failed',
+                                         strip_prompt=False, strip_command=False)
+            raise ValueError('Commit failed with the following errors:\n\n\
+            {fail_msg}'.format(fail_msg=fail_msg))
 
         else:
             output += self.exit_config_mode()
@@ -71,11 +75,11 @@ class CiscoXrSSH(SSHConnection):
 
 
 
-		
-
-		
 
 
 
 
-	   		
+
+
+
+
