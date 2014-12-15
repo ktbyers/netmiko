6b04b44a2d54aa557ae60caff443f1cb0e248cdf
Ron Nilekani <nilekani.raunaq@gmail.com>
2014-12-15 13:20:21 -0600

    Update __init__.py

--- netmiko/__init__.py
+++ netmiko/__init__.py
@@ -2,5 +2,6 @@ from ssh_dispatcher import ssh_dispatcher
 from cisco import CiscoIosSSH
 from cisco import CiscoAsaSSH
 from arista import AristaSSH
+from f5 import F5LtmSSH
 
 __version__ = '0.1.2'
