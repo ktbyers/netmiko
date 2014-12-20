b2d77e30f66def737a6b8879bdada9c851f6624c
Kirk Byers <ktbyers@twb-tech.com>
2014-12-19 16:20:13 -0800

    Adding Juniper test code

---
+++ tests/test_juniper_srx.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import pytest
+
+import netmiko
+import time
+from DEVICE_CREDS import *
+
+
+def setup_module(module):
+
+    module.EXPECTED_RESPONSES = {
+        'router_prompt'    : 'root@pynet-jnpr-srx1>',
+        'router_conf_mode'  : 'root@pynet-jnpr-srx1#',
+        'interface_ip'      : '10.220.88.39',
+    }
+    
+    show_ver_command = 'show version'
+    multiple_line_command = 'show configuration'
+    module.basic_command = 'show interfaces terse'
+    
+    SSHClass = netmiko.ssh_dispatcher(juniper_srx['device_type'])
+    net_connect = SSHClass(**juniper_srx)
+
+    module.show_version = net_connect.send_command(show_ver_command)
+    module.multiple_line_output = net_connect.send_command(multiple_line_command, delay_factor=2)
+    module.show_ip = net_connect.send_command(module.basic_command)
+    module.router_prompt = net_connect.router_prompt
+
+    # Test buffer clearing
+    net_connect.remote_conn.send(show_ver_command)
+    time.sleep(2)
+    net_connect.clear_buffer()
+    # Should not be anything there on the second pass
+    module.clear_buffer_check = net_connect.clear_buffer()
+    
+
+def test_disable_paging():
+    '''
+    Verify paging is disabled by looking for string after when paging would
+    normally occur
+    '''
+    assert 'security-zone untrust' in multiple_line_output
+
+
+def test_verify_ssh_connect():
+    '''
+    Verify the connection was established successfully
+    '''
+    assert 'JUNOS Software Release' in show_version
+
+
+def test_verify_send_command():
+    '''
+    Verify a command can be sent down the channel successfully
+    '''
+    assert EXPECTED_RESPONSES['interface_ip'] in show_ip
+
+
+def test_find_prompt():
+    '''
+    Verify the router prompt is detected correctly
+    '''
+    assert router_prompt == EXPECTED_RESPONSES['router_prompt']
+
+
+def test_strip_prompt():
+    '''
+    Ensure the router prompt is not in the command output
+    '''
+    assert EXPECTED_RESPONSES['router_prompt'] not in show_version
+
+
+def test_strip_command():
+    '''
+    Ensure that the command that was executed does not show up in the 
+    command output
+    '''
+    assert basic_command not in show_ip
+
+
+def test_normalize_linefeeds():
+    '''
+    Ensure no '\r' sequences
+    '''
+    assert not '\r' in show_ip
+
+
+def test_clear_buffer():
+    '''
+    Test that clearing the buffer works
+    '''
+    assert clear_buffer_check is None
---
+++ tests/test_juniper_srx_enable.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import pytest
+
+import netmiko
+import time
+from DEVICE_CREDS import *
+
+
+def setup_module(module):
+    
+    module.EXPECTED_RESPONSES = {
+        'router_prompt'     : 'root@pynet-jnpr-srx1>',
+        'router_conf_mode'  : 'root@pynet-jnpr-srx1#',
+        'interface_ip'      : '10.220.88.39',
+        'config_mode'       : '[edit',
+    }
+    
+    show_ver_command = 'show version'
+    multiple_line_command = 'show configuration'
+    module.basic_command = 'show interfaces terse'
+    
+    SSHClass = netmiko.ssh_dispatcher(juniper_srx['device_type'])
+    net_connect = SSHClass(**juniper_srx)
+    
+    module.show_version = net_connect.send_command(show_ver_command)
+    module.show_ip = net_connect.send_command(module.basic_command)
+
+    module.router_prompt_initial = net_connect.router_prompt
+    module.config_mode = net_connect.config_mode()
+    module.router_prompt = net_connect.router_prompt
+
+    config_commands = [
+        'set system syslog archive size 110k files 3',
+        'set system syslog archive size 120k files 3',
+        'set system time-zone America/New_York',
+    ]
+    net_connect.send_config_set(config_commands, commit=True)
+
+    module.exit_config_mode = net_connect.exit_config_mode()
+
+    module.config_commands_output = net_connect.send_command('show configuration | match time-zone')
+
+    # Test a manual commit
+    net_connect.config_mode()
+    net_connect.send_command('set system time-zone America/Los_Angeles')
+    net_connect.commit()
+    net_connect.exit_config_mode()
+    module.manual_commit = net_connect.send_command('show configuration | match time-zone')
+
+    net_connect.disconnect()
+
+
+def test_config_mode():
+    assert EXPECTED_RESPONSES['config_mode'] in config_mode
+
+
+def test_command_set():
+    assert 'time-zone America/New_York' in config_commands_output
+   
+ 
+def test_exit_config_mode():
+    assert not EXPECTED_RESPONSES['config_mode'] in exit_config_mode
+
+
+def test_manual_commit():
+    assert 'time-zone America/Los_Angeles' in manual_commit
