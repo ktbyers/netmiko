ffc351e7021282bc26794dd440976d3e3c5edd66
Kirk Byers <ktbyers@twb-tech.com>
2015-01-28 10:10:47 -0800

    Restoring cisco_nxos_ssh to original state

--- netmiko/cisco/cisco_nxos_ssh.py
+++ netmiko/cisco/cisco_nxos_ssh.py
@@ -4,60 +4,10 @@ import re
 class CiscoNxosSSH(SSHConnection):
 
     def normalize_linefeeds(self, a_string):
-	    '''
-	    Convert '\r\n' or '\r\r\n' to '\n, and remove extra '\r's in the text
-	    '''
-
-	    newline = re.compile(r'(\r\r\n|\r\n)')
-
-	    return newline.sub('\n', a_string).replace('\r', '')
-
-    def config_mode(self):
         '''
-        Enter config mode
+        Convert '\r\n' or '\r\r\n' to '\n, and remove extra '\r's in the text
         '''
-        
-        DEBUG = False
-        
-        if self.check_config_mode():
-            return True
-        
-        output = self.send_command('configure terminal\n')
 
-        if DEBUG: print output
+        newline = re.compile(r'(\r\r\n|\r\n)')
 
-        if self.check_config_mode():
-            return True
-        else:
-            return False
-
-    def exit_config_mode(self):
-        '''
-        Exit config mode
-        '''
-        
-        DEBUG = False
-        
-        if not self.check_config_mode():
-            return True
-        
-        output = self.send_command('end\n')
-
-        if DEBUG: print output
-
-        if self.check_config_mode():
-            return False
-        else:
-            return True
-	   		
-    def check_config_mode(self):
-        '''
-        Finds the Cisco NXOS prompt and checks if the prompt contains the right config mode characters '(config)#'
-        
-        '''
-        
-        self.find_prompt()
-        if self.router_prompt[-9:] == '(config)#':
-            return True
-        else:
-            return False
+        return newline.sub('\n', a_string).replace('\r', '')
