b9ffcf4c3386885226f89eebf83482f4fbde0bc3
Kirk Byers <ktbyers@twb-tech.com>
2015-03-21 00:41:16 -0700

    Creating a factory function for selecting the right SSH class

--- netmiko/__init__.py
+++ netmiko/__init__.py
@@ -1,3 +1,4 @@
-from ssh_dispatcher import ssh_dispatcher
+from netmiko.ssh_dispatcher import ConnectHandler
+from netmiko.ssh_dispatcher import ssh_dispatcher
 
 __version__ = '0.1.4'
--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -25,9 +25,23 @@ CLASS_MAPPER = {
     'brocade_vdx'   : BrocadeVdxSSH,
 }
 
+
+def ConnectHandler(*args, **kwargs):
+    '''
+    Factory function that selects the proper class and instantiates the object based on device_type
+
+    Returns the object 
+    '''
+
+    ConnectionClass = ssh_dispatcher(kwargs['device_type'])
+    return ConnectionClass(*args, **kwargs)
+
+
 def ssh_dispatcher(device_type):
     '''
     Select the class to be instantiated based on vendor/platform
     '''
 
     return CLASS_MAPPER[device_type]
+
+
