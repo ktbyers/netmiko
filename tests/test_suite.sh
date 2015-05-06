bc56d216fea25aa59d8291b8f2999f734f4e0d69
Kirk Byers <ktbyers@twb-tech.com>
2015-05-05 21:06:38 -0700

    Updating test suite for CI testing

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -9,10 +9,10 @@ py.test -v test_netmiko_config.py --test_device cisco881
 py.test -v test_netmiko_show.py --test_device arista_sw4
 py.test -v test_netmiko_config.py --test_device arista_sw4
 
-py.test -v test_netmiko_show.py --test_device hp_procurve
-py.test -v test_netmiko_config.py --test_device hp_procurve
+#py.test -v test_netmiko_show.py --test_device hp_procurve
+#py.test -v test_netmiko_config.py --test_device hp_procurve
 
-py.test -v test_netmiko_show.py --test_device juniper_srx
-py.test -v test_netmiko_config.py --test_device juniper_srx
-py.test -v test_netmiko_commit.py --test_device juniper_srx
+#py.test -v test_netmiko_show.py --test_device juniper_srx
+#py.test -v test_netmiko_config.py --test_device juniper_srx
+#py.test -v test_netmiko_commit.py --test_device juniper_srx
 
