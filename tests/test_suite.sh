fc7360cf4195dff8ab9208a6ac73f8a9f5054f67
Kirk Byers <ktbyers@twb-tech.com>
2015-05-05 20:43:25 -0700

    Simplifying tests for Travis CI

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -3,16 +3,16 @@
 py.test -v test_netmiko_show.py --test_device cisco881
 py.test -v test_netmiko_config.py --test_device cisco881
 
-py.test -v test_netmiko_show.py --test_device cisco_asa
-py.test -v test_netmiko_config.py --test_device cisco_asa
+#py.test -v test_netmiko_show.py --test_device cisco_asa
+#py.test -v test_netmiko_config.py --test_device cisco_asa
 
-py.test -v test_netmiko_show.py --test_device arista_sw4
-py.test -v test_netmiko_config.py --test_device arista_sw4
+#py.test -v test_netmiko_show.py --test_device arista_sw4
+#py.test -v test_netmiko_config.py --test_device arista_sw4
 
-py.test -v test_netmiko_show.py --test_device hp_procurve
-py.test -v test_netmiko_config.py --test_device hp_procurve
+#py.test -v test_netmiko_show.py --test_device hp_procurve
+#py.test -v test_netmiko_config.py --test_device hp_procurve
 
-py.test -v test_netmiko_show.py --test_device juniper_srx
-py.test -v test_netmiko_config.py --test_device juniper_srx
-py.test -v test_netmiko_commit.py --test_device juniper_srx
+#py.test -v test_netmiko_show.py --test_device juniper_srx
+#py.test -v test_netmiko_config.py --test_device juniper_srx
+#py.test -v test_netmiko_commit.py --test_device juniper_srx
 
