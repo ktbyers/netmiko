0f05b60bb232864e3c48343c973da01f4eed23f5
Kirk Byers <ktbyers@twb-tech.com>
2015-05-08 10:42:03 -0700

    Handling test return code properly

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+RETURN_CODE=0
 py.test -v test_netmiko_show.py --test_device cisco881
-py.test -v test_netmiko_config.py --test_device cisco881
+if [ $? -ne 0 ]; then
+    RETURN_CODE=1
+fi
+#py.test -v test_netmiko_config.py --test_device cisco881
 
 #py.test -v test_netmiko_show.py --test_device cisco_asa
 #py.test -v test_netmiko_config.py --test_device cisco_asa
 
-py.test -v test_netmiko_show.py --test_device arista_sw4
-py.test -v test_netmiko_config.py --test_device arista_sw4
+#py.test -v test_netmiko_show.py --test_device arista_sw4
+#py.test -v test_netmiko_config.py --test_device arista_sw4
 
-py.test -v test_netmiko_show.py --test_device hp_procurve
-py.test -v test_netmiko_config.py --test_device hp_procurve
+#py.test -v test_netmiko_show.py --test_device hp_procurve
+#py.test -v test_netmiko_config.py --test_device hp_procurve
 
-py.test -v test_netmiko_show.py --test_device juniper_srx
-py.test -v test_netmiko_config.py --test_device juniper_srx
-py.test -v test_netmiko_commit.py --test_device juniper_srx
+#py.test -v test_netmiko_show.py --test_device juniper_srx
+#py.test -v test_netmiko_config.py --test_device juniper_srx
+#py.test -v test_netmiko_commit.py --test_device juniper_srx
+
+
+exit $RETURN_CODE
 
