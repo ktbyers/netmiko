57dbe7d76861e27e33dae8fa58fddcaafd2a38ff
Kirk Byers <ktbyers@twb-tech.com>
2015-06-07 21:48:53 -0700

    IOS XR Commit code

--- netmiko/cisco/cisco_xr_ssh.py
+++ netmiko/cisco/cisco_xr_ssh.py
@@ -15,7 +15,7 @@ class CiscoXrSSH(SSHConnection):
         return super(CiscoXrSSH, self).send_config_set(config_commands=config_commands, exit_config_mode=False, **kwargs)
 
 
-    def commit_new(self, confirm=False, confirm_delay=None, comment='', label='', delay_factor=10):
+    def commit(self, confirm=False, confirm_delay=None, comment='', label='', delay_factor=10):
         """
         Commit the candidate configuration.
 
@@ -28,43 +28,34 @@ class CiscoXrSSH(SSHConnection):
         comment:
             command_string = commit comment <comment>
 
-        combinations
-        label and confirm:      [Valid]
+        supported combinations
+        label and confirm:
             command_string = commit label <label> confirmed <confirm_delay>
-        label and comment:       [Valid] 
+        label and comment:
             command_string = commit label <label> comment <comment>
-        label, confirm, and comment:  [Invalid, not allowed]
-            command_string = commit label <label> comment <comment> confirmed <confirm_delay>
-        comment and confirm:   [Invalid not allowed]
-            command_string = commit comment <comment> confirmed <confirm_delay>
+
+        All other combinations will result in an exception.
 
         failed commit message:
         % Failed to commit one or more configuration items during a pseudo-atomic operation. All 
-        changes made have been reverted. Please issue 'show configuration failed [inheritance]' from 
-        this session to view the errors        
+        changes made have been reverted. Please issue 'show configuration failed [inheritance]' 
+        from this session to view the errors
+
+        message XR shows if other commits occurred:
+        One or more commits have occurred from other configuration sessions since this session 
+        started or since the last commit was made from this session. You can use the 'show 
+        configuration commit changes' command to browse the changes.
 
         Exit of configuration mode with pending changes will cause the changes to be discarded and
         an exception to be generated.
-
-One or more commits have occurred from other
-configuration sessions since this session started
-or since the last commit was made from this session.
-You can use the 'show configuration commit changes'
-command to browse the changes.
-Do you wish to proceed with this commit anyway? [no]: yes
-
         """
 
         if confirm and not confirm_delay:
-            raise ValueError("Invalid arguments supplied to commit-confirm but no confirm_delay")
+            raise ValueError("Invalid arguments supplied to XR commit")
         if confirm_delay and not confirm:
-            raise ValueError("Invalid arguments supplied to commit-confirm_delay but no confirm")
-
-        # Select proper command string based on arguments provided
-        command_string = 'commit'
-        commit_marker = 'Failed to'
-        if confirm:
-            command_string = 'commit confirmed ' + str(confirm_delay)
+            raise ValueError("Invalid arguments supplied to XR commit")
+        if comment and confirm: 
+            raise ValueError("Invalid arguments supplied to XR commit")
 
         # wrap the comment in quotes
         if comment:
@@ -72,17 +63,36 @@ Do you wish to proceed with this commit anyway? [no]: yes
                 raise ValueError("Invalid comment contains double quote")
             comment = '"{0}"'.format(comment)
 
-            command_string += ' comment ' + comment
+        label = str(label)
+        error_marker = 'Failed to'
+        alt_error_marker = 'One or more commits have occurred from other'
 
-
+        # Select proper command string based on arguments provided
+        if label:
+            if comment:
+                command_string = 'commit label {0} comment {1}'.format(label, comment)
+            elif confirm:
+                command_string = 'commit label {0} confirmed {1}'.format(label, str(confirm_delay))
+            else:
+                command_string = 'commit label {0}'.format(label)
+        elif confirm:
+            command_string = 'commit confirmed {0}'.format(str(confirm_delay))
+        elif comment:
+            command_string = 'commit comment {0}'.format(comment)
+        else:
+            command_string = 'commit'
 
         # Enter config mode (if necessary)
         output = self.config_mode()
         output += self.send_command(command_string, strip_prompt=False, strip_command=False,
                                     delay_factor=delay_factor)
-        if not commit_marker in output:
-            raise ValueError("Commit failed with the following errors:\n\n{0}"
-                             .format(output))
+        if error_marker in output:
+            raise ValueError("Commit failed with the following errors:\n\n{0}".format(output))
+        if alt_error_marker in output:
+            # Other commits occurred, don't proceed with commit
+            output += self.send_command("no", strip_prompt=False, strip_command=False,
+                                        delay_factor=delay_factor)
+            raise ValueError("Commit failed with the following errors:\n\n{0}".format(output))
 
         return output
 
@@ -91,17 +101,14 @@ Do you wish to proceed with this commit anyway? [no]: yes
         '''
         First check whether in configuration mode.
 
-        If so, exit config mode
+        If so, exit config mode. Uncommitted changes will be discarded.
         '''
-        debug = False
         output = ''
 
         if self.check_config_mode():
             output = self.send_command(exit_config, strip_prompt=False, strip_command=False)
             if "Uncommitted changes found" in output:
                 output += self.send_command('no\n', strip_prompt=False, strip_command=False)
-            if debug:
-                print(output)
             if self.check_config_mode():
                 raise ValueError("Failed to exit configuration mode")
 
