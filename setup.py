418ec5557f744f55208ed3c7cbfc4c1e65190c26
Adam Burns <adam@yourteamneedsadam.com>
2015-07-09 16:06:20 -0700

    Find version without importing dependencies.

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
+import os
+import re
+
 from distutils.core import setup
 
-import netmiko
+
+def find_version(*file_paths):
+    """
+    This pattern was modeled on a method from the Python Packaging User Guide:
+        https://packaging.python.org/en/latest/single_source_version.html
+
+    We read instead of importing so we don't get import errors if our code
+    imports from dependencies listed in install_requires.
+    """
+    base_module_file = os.path.join(*file_paths)
+    with open(base_module_file) as f:
+        base_module_data = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              base_module_data, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='netmiko',
-    version=netmiko.__version__,
+    version=find_version('netmiko', '__init__.py'),
     url='https://github.com/ktbyers/netmiko',
     license='MIT',
     author='Kirk Byers',
@@ -19,4 +39,3 @@ setup(
               'netmiko/brocade',
               'netmiko/huawei',],
     )
-
