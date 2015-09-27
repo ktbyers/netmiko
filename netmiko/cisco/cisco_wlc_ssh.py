5ecb23823d85e1d81d701c1792096371359b3bb2
Kirk Byers <ktbyers@twb-tech.com>
2015-09-27 16:39:41 -0700

    New WLC method to handle show run-config command

--- netmiko/cisco/cisco_wlc_ssh.py
+++ netmiko/cisco/cisco_wlc_ssh.py
@@ -40,6 +40,56 @@ class CiscoWlcSSH(BaseSSHConnection):
             i += 1
 
 
+    def send_command_w_enter(self, *args, **kwargs):
+        '''
+        For 'show run-config' Cisco WLC adds a 'Press Enter to continue...' message
+        Even though pagination is disabled
+
+        show run-config also has excessive delays in the output which requires special
+        handling.
+
+        Arguments are the same as send_command() method
+        '''
+
+        output = self.send_command(*args, **kwargs)
+        if 'Press Enter to' in output:
+            new_args = list(args)
+            if len(args) >= 1:
+                new_args[0] = '\n'
+            else:
+                kwargs['command_string'] = '\n'
+
+            if not kwargs.get('delay_factor') and not len(args) >= 2:
+                kwargs['delay_factor'] = 3
+            if not kwargs.get('max_loops') and not len(args) >= 3:
+                kwargs['max_loops'] = 150
+
+            # Send an 'enter'
+            output = self.send_command(*new_args, **kwargs)
+
+            # WLC has excessive delay after this appears on screen
+            if '802.11b Advanced Configuration' in output:
+
+                # Defaults to 30 seconds
+                time.sleep(kwargs['delay_factor'] * 10)
+                not_done = True
+                i = 1
+                while (not_done) and (i <= 150):
+                    time.sleep(kwargs['delay_factor'])
+                    i += 1
+                    if self.remote_conn.recv_ready():
+                        # Unicode error occurred in output (errors=ignore strips this out).
+                        output += self.remote_conn.recv(MAX_BUFFER).decode('utf-8',
+                                                                           errors='ignore')
+                    else:
+                        not_done = False
+
+        strip_prompt = kwargs.get('strip_prompt', True)
+        if strip_prompt:
+            output = self.strip_prompt(output)
+        return output
+
+
     def session_preparation(self):
         '''
         Prepare the session after the connection has been established
