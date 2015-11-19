3cee354d66402c8302df3ff4be5bae9e4d7c255b
Kirk Byers <ktbyers@twb-tech.com>
2015-11-19 13:09:19 -0800

    Minor PEP8 cleanup

--- netmiko/ovs/ovs_linux_ssh.py
+++ netmiko/ovs/ovs_linux_ssh.py
@@ -3,18 +3,15 @@ from netmiko.ssh_connection import SSHConnection
 
 class OvsLinuxSSH(SSHConnection):
 
-
     def check_config_mode(self, check_string='#'):
         return super(SSHConnection, self).check_config_mode(check_string=check_string)
 
-
     def set_base_prompt(self, pri_prompt_terminator='$',
-                                    alt_prompt_terminator='#', delay_factor=.5):
+                        alt_prompt_terminator='#', delay_factor=.5):
         return super(SSHConnection, self).set_base_prompt(
-                pri_prompt_terminator=pri_prompt_terminator,
-                alt_prompt_terminator=alt_prompt_terminator,
-                delay_factor=delay_factor)
-
+            pri_prompt_terminator=pri_prompt_terminator,
+            alt_prompt_terminator=alt_prompt_terminator,
+            delay_factor=delay_factor)
 
     def config_mode(self, config_command='sudo su'):
         return super(SSHConnection, self).config_mode(config_command=config_command)
