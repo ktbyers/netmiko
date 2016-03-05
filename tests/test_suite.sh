11a79d3bb5dc1965f1602b6b5c8e6ae9f868a560
Kirk Byers <ktbyers@twb-tech.com>
2016-03-05 12:53:00 -0800

    Fix bug with echo

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -4,24 +4,24 @@ RETURN_CODE=0
 
 # Exit on the first test failure and set RETURN_CODE = 1
 echo "Starting tests...good luck:" \
-echo "Cisco IOS" \
+&& echo "Cisco IOS" \
 && py.test -v test_netmiko_show.py --test_device cisco881 \
 && py.test -v test_netmiko_config.py --test_device cisco881 \
 \
-echo "Arista" \
+&& echo "Arista" \
 && py.test -v test_netmiko_show.py --test_device arista_sw4 \
 && py.test -v test_netmiko_config.py --test_device arista_sw4 \
 \
-echo "HP ProCurve" \
+&& echo "HP ProCurve" \
 && py.test -v test_netmiko_show.py --test_device hp_procurve \
 && py.test -v test_netmiko_config.py --test_device hp_procurve \
 \
-echo "Juniper" \
+&& echo "Juniper" \
 && py.test -v test_netmiko_show.py --test_device juniper_srx \
 && py.test -v test_netmiko_config.py --test_device juniper_srx \
 && py.test -v test_netmiko_commit.py --test_device juniper_srx \
 \
-echo "Cisco IOS-XR" \
+&& echo "Cisco IOS-XR" \
 && py.test -v test_netmiko_show.py --test_device cisco_xrv \
 && py.test -v test_netmiko_config.py --test_device cisco_xrv \
 && py.test -v test_netmiko_commit.py --test_device cisco_xrv \
