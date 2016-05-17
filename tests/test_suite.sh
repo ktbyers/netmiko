cfd6c13683971f24064949365d2c204ae6b5a7fb
Kirk Byers <ktbyers@twb-tech.com>
2016-05-17 09:40:02 -0700

    Remove HP from Travis Tests

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -12,10 +12,6 @@ echo "Starting tests...good luck:" \
 && py.test -v test_netmiko_show.py --test_device arista_sw4 \
 && py.test -v test_netmiko_config.py --test_device arista_sw4 \
 \
-&& echo "HP ProCurve" \
-&& py.test -v test_netmiko_show.py --test_device hp_procurve \
-&& py.test -v test_netmiko_config.py --test_device hp_procurve \
-\
 && echo "Juniper" \
 && py.test -v test_netmiko_show.py --test_device juniper_srx \
 && py.test -v test_netmiko_config.py --test_device juniper_srx \
@@ -26,9 +22,5 @@ echo "Starting tests...good luck:" \
 && py.test -v test_netmiko_config.py --test_device cisco_xrv \
 && py.test -v test_netmiko_commit.py --test_device cisco_xrv \
 || RETURN_CODE=1
-
-#\
-#&& py.test -v test_netmiko_show.py --test_device cisco_asa \
-#&& py.test -v test_netmiko_config.py --test_device cisco_asa \
 
 exit $RETURN_CODE
