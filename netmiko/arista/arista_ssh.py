bff839edbca6fb46bbe186bc8e8b4aa1dbbc2b42
Robert Ehlers <rob@oldwestmail.com>
2016-06-16 09:00:41 -0700

    Fix for configure mode on Arista 7500 models
    Need to look for config), as Arista 7500 has supervisor # (s1) / (s2) in prompt.
    This confuses the default check_string, which only looks for ')#'.
    Arista 7500 output:
    loc1-core01(s1)#configure terminal 
    loc1-core01(s1)(config)#

--- netmiko/arista/arista_ssh.py
+++ netmiko/arista/arista_ssh.py
@@ -2,4 +2,6 @@ from netmiko.ssh_connection import SSHConnection
 
 
 class AristaSSH(SSHConnection):
-    pass
+    def check_config_mode(self, check_string='config)'):
+    	"""Checks if the device is in configuration mode or not."""
+        return super(SSHConnection, self).check_config_mode(check_string=check_string)
