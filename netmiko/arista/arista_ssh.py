0b1813bef37819209ed9fb5b06eb7495d0e0e1fb
Kirk Byers <ktbyers@twb-tech.com>
2016-07-25 17:54:25 -0700

    Improve Arista reliability on slow login

--- netmiko/arista/arista_ssh.py
+++ netmiko/arista/arista_ssh.py
@@ -1,5 +1,11 @@
+import time
 from netmiko.ssh_connection import SSHConnection
 
 
 class AristaSSH(SSHConnection):
-    pass
+    def special_login_handler(self, delay_factor=1):
+        """
+        Arista adds a "Last login: " message that doesn't always have sufficient time to be handled
+        """
+        time.sleep(3 * delay_factor)
+        self.clear_buffer()
