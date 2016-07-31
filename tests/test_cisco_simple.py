44466acd0bd9ee7b04750514d277d92892a39850
Kirk Byers <ktbyers@twb-tech.com>
2016-07-31 08:41:47 -0700

    Test of telnet

--- tests/test_cisco_simple.py
+++ tests/test_cisco_simple.py
@@ -2,17 +2,34 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
-ip_addr = raw_input("Enter IP Address: ")
+#ip_addr = raw_input("Enter IP Address: ")
+pwd = getpass()
+ip_addr = '184.105.247.70'
 
-device = {
+telnet_device = {
     'device_type': 'cisco_ios_telnet',
     'ip': ip_addr,
     'username': 'pyclass',
-    'password': getpass(),
+    'password': pwd,
     'port': 23,
 } 
 
-net_connect = ConnectHandler(**device)
+ssh_device = {
+    'device_type': 'cisco_ios_ssh',
+    'ip': ip_addr,
+    'username': 'pyclass',
+    'password': pwd,
+    'port': 22,
+} 
+
+print "telnet"
+net_connect1 = ConnectHandler(**telnet_device)
+print net_connect1.find_prompt()
+
+print "SSH"
+net_connect2 = ConnectHandler(**ssh_device)
+print net_connect2.find_prompt()
+
 #output = net_connect.send_command_expect("show version")
 
 #print
