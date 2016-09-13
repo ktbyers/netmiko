c09bf70bc7bc303ba22215b8c4eb24f14ee03842
Kirk Byers <ktbyers@twb-tech.com>
2016-09-13 10:28:18 -0700

    fix flake8 issue

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
               'netmiko/quanta'],
     install_requires=['paramiko>=1.13.0', 'scp>=0.10.0', 'pyyaml'],
     extras_require={
-        'test': ['pytest>=2.6.0',]
+        'test': ['pytest>=2.6.0', ]
     },
 )
