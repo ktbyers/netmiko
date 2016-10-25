bd244103cefe8e54622c2ccb666546da252057cc
Kirk Byers <ktbyers@twb-tech.com>
2016-10-25 11:04:30 -0700

    Add a test for NXOS

---
+++ tests/test_nxos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+RETURN_CODE=0
+
+echo "Starting tests...good luck:" \
+&& echo "Cisco NXOS" \
+&& py.test -v test_netmiko_show.py --test_device nxos1 \
+&& py.test -v test_netmiko_config.py --test_device nxos1 \
+|| RETURN_CODE=1
+
+exit $RETURN_CODE
