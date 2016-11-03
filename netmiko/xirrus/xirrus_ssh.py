0299d499632d696afde2392df9daf3a275a5593a
Alvin Morris <skriptmonkey@gmail.com>
2016-11-03 12:40:44 -0400

    Testing in progress. Not all tests are passing.

--- netmiko/xirrus/xirrus_ssh.py
+++ netmiko/xirrus/xirrus_ssh.py
@@ -1,4 +1,5 @@
 from netmiko.base_connection import BaseConnection
+import time
 
 class XirrusSSH(BaseConnection):
     """
@@ -16,9 +17,8 @@ class XirrusSSH(BaseConnection):
         Exit config mode.
         """
 
-        self.config_mode()
-        self.disable_paging(command="no more\n")
-#        self.exit_config_mode()
+        self.disable_paging(command="no more")
+        self.set_base_prompt()
 
     def check_enable_mode(self, *args, **kwargs):
         """No enable mode on Xirrus APs."""
@@ -40,3 +40,44 @@ class XirrusSSH(BaseConnection):
         """Exit configuration mode."""
         return super(XirrusSSH, self).exit_config_mode(exit_config=exit_config)
 
+    def set_base_prompt(self, pri_prompt_terminator='#',
+                        alt_prompt_terminator='>', delay_factor=1):
+        """Sets self.base_prompt"""
+        #self.read_until_prompt()
+        prompt = self.find_prompt(delay_factor=delay_factor)
+        print(prompt)
+        if not prompt[-1] in (pri_prompt_terminator, alt_prompt_terminator):
+            raise ValueError("AP Prompt not found: {0}".format(prompt))
+        # Strip off trailing terminator
+        self.base_prompt = prompt[:-1]
+        return self.base_prompt
+
+    def disable_paging(self, command="no more", delay_factor=1):
+        """Disable paging default to the xirrus method."""
+        debug = True
+        delay_factor = self.select_delay_factor(delay_factor)
+        time.sleep(delay_factor * .1)
+        self.clear_buffer()
+        config = self.normalize_cmd("configure")
+        command = self.normalize_cmd(command)
+        exit_config = self.normalize_cmd("end")
+
+        if debug:
+            print("In disable_paging")
+            print("command: {}", format(command))
+
+        self.read_until_prompt()
+        self.write_channel(config)
+        output = self.read_until_prompt()
+        self.write_channel(command)
+        output += self.read_until_prompt()
+        self.write_channel(exit_config)
+        output += self.read_until_prompt()
+
+        if self.ansi_escape_codes:
+            output = self.strip_ansi_escape_codes(output)
+        if debug:
+            print(output)
+            print("Exiting disable_paging")
+
+        return output
