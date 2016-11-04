06900ec80255d049e84c8596076298851dc7b167
Kirk Byers <ktbyers@twb-tech.com>
2016-11-04 09:15:41 -0700

    Fixing fortinet vdom bug

--- netmiko/fortinet/fortinet_ssh.py
+++ netmiko/fortinet/fortinet_ssh.py
@@ -15,8 +15,7 @@ class FortinetSSH(CiscoSSHConnection):
         self.allow_disable_global = True
         self.vdoms = False
 
-        # According with http://www.gossamer-threads.com/lists/rancid/users/6729
-        if output.find("Virtual domain configuration: enable"):
+        if "Virtual domain configuration: enable" in output:
             self.vdoms = True
             vdom_additional_command = "config global"
             output = self.send_command_timing(vdom_additional_command)
