cd6521b6f8e517854fb5b100ccbfa075a082f6ac
Tim Raphael <raphael.timothy@gmail.com>
2016-11-16 20:51:58 +0800

    Cleaning up Ubiquiti driver and adding comments.

--- netmiko/ubiquiti/edgeswitch_ssh.py
+++ netmiko/ubiquiti/edgeswitch_ssh.py
@@ -1,17 +1,11 @@
-from __future__ import unicode_literals
-
-import re
-import time
-
-#from netmiko.base_connection import BaseConnection
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
 class UbiquitiEdgeSwitchSSH(CiscoSSHConnection):
     """
     Implements support for Ubiquity EdgeSwitch devices.
-
-    Passes on anything "enable" related as UBNT doesn't have this mode.
+    
+    Mostly conforms to Cisco IOS style syntax with a few minor changes.
     """
 
     def check_config_mode(self, check_string=')#'):
@@ -27,15 +21,5 @@ class UbiquitiEdgeSwitchSSH(CiscoSSHConnection):
        	return super(UbiquitiEdgeSwitchSSH, self).exit_config_mode(exit_config=exit_config)
 
     def exit_enable_mode(self, exit_command='exit'):
+        """Exit enable mode."""
         return super(UbiquitiEdgeSwitchSSH, self).exit_enable_mode(exit_command=exit_command)
-
-
-    def commit(self, confirm=False, confirm_delay=None, check=False, comment='',
-               and_quit=False, delay_factor=1):
-      
-		return super(UbiquitiEdgeSwitchSSH, self).commit(confirm=False, confirm_delay=None, check=False, comment='',
-               and_quit=False, delay_factor=1)
-
-    def strip_prompt(self, *args, **kwargs):
-        """Strip the trailing router prompt from the output."""
-        return super(UbiquitiEdgeSwitchSSH, self).strip_prompt(*args, **kwargs)
