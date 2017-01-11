ktbyers/netmiko

commit 649dbcd4e10eb497e285662e4410ae45038fd15a

Fixing some issues with SSH proxy support

netmiko/base_connection.py
@@ -88,11 +88,14 @@ class BaseSSHConnection(object):
             ssh_config_instance = paramiko.SSHConfig()
             with open(full_path) as f:
                 ssh_config_instance.parse(f)
-                source = ssh_config_instance.lookup(self.ip)
+                host_specifier = "{0}:{1}".format(self.ip, self.port)
+                source = ssh_config_instance.lookup(host_specifier)
         else:
             source = {}
 
         if source.get('proxycommand'):
+            proxy = paramiko.ProxyCommand(source['proxycommand'])
+        elif source.get('ProxyCommand'):
             proxy = paramiko.ProxyCommand(source['proxycommand'])
         else:
             proxy = None
@@ -100,7 +103,7 @@ class BaseSSHConnection(object):
         # Only update 'hostname', 'sock', 'port', and 'username'
         # For 'port' and 'username' only update if using object defaults
         if connect_dict['port'] == 22:
-            connect_dict['port'] = source.get('port', self.port)
+            connect_dict['port'] = int(source.get('port', self.port))
         if connect_dict['username'] == '':
             connect_dict['username'] = source.get('username', self.username)
         if proxy:
@@ -153,6 +156,7 @@ class BaseSSHConnection(object):
 
         # initiate SSH connection
         try:
+            print(ssh_connect_params)
             self.remote_conn_pre.connect(**ssh_connect_params)
 
         except socket.error:

commit 91450d152d08f655727b1ef485d71d3959a43163

Fix proxycommand support
Prior to this, the port was appended to the hostname parameter when
given to paramiko.SSHConfig, this causes a config such as:

  ProxyCommand ssh jumphost nc %h %p

to be expanded to something like:

  ssh jumphost nc 10.0.0.1:22 22

Which isn't correct according to ssh_configs doc's on the usage of
the replacements:

> In the command string, any occurrence of ‘%h’ will be substituted by the host name to connect, ‘%p’ by the port, and ‘%r’ by the remote user name.

This bug was introduced in 649dbcd4e1

netmiko/base_connection.py
@@ -394,8 +394,7 @@ class BaseConnection(object):
             ssh_config_instance = paramiko.SSHConfig()
             with open(full_path) as f:
                 ssh_config_instance.parse(f)
-                host_specifier = "{0}:{1}".format(self.host, self.port)
-                source = ssh_config_instance.lookup(host_specifier)
+                source = ssh_config_instance.lookup(self.host)
         else:
             source = {}
 