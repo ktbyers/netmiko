d327cdbe851c0c865c60f5ecc5f75af2ab92c7ff
Kirk Byers <ktbyers@twb-tech.com>
2017-01-14 09:16:26 -0800

    Rolling version to 1.2.5

--- netmiko/__init__.py
+++ netmiko/__init__.py
@@ -17,7 +17,7 @@ from netmiko.ssh_exception import NetMikoAuthenticationException
 NetmikoTimeoutError = NetMikoTimeoutException
 NetmikoAuthError = NetMikoAuthenticationException
 
-__version__ = '1.2.4'
+__version__ = '1.2.5'
 
 __all__ = ('ConnectHandler', 'ssh_dispatcher', 'platforms', 'SCPConn', 'FileTransfer',
            'NetMikoTimeoutException', 'NetMikoAuthenticationException',
