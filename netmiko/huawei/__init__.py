fa361a7e29f9737b71024ef1cf73ca52a6294536
plejon <per@lejon.org>
2017-01-19 11:26:20 +0100

    fixing __init__

--- netmiko/huawei/__init__.py
+++ netmiko/huawei/__init__.py
@@ -1,4 +1,4 @@
 from netmiko.huawei.huawei_ssh import HuaweiSSH
-from netmiko.huawei.huawei_ssh import HuaweiSSH_MT5600T
+from netmiko.huawei.huawei_ssh_mt5600t import HuaweiSSH_MT5600T
 
 __all__ = ['HuaweiSSH', 'HuaweiSSH_MT5600T']
