ff44813397e221124bd08f84480846219a13f777
Kirk Byers <ktbyers@twb-tech.com>
2017-02-10 12:04:09 -0800

    Make file read operations consistent for PY3 support

--- netmiko/scp_handler.py
+++ netmiko/scp_handler.py
@@ -280,7 +280,7 @@ class InLineTransfer(FileTransfer):
 
     @staticmethod
     def _read_file(file_name):
-        with open(file_name, "rt") as f:
+        with io.open(file_name, "rt", encoding='utf-8') as f:
             return f.read()
 
     @staticmethod
