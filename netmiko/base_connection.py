f818cce48c472fe676db0f939e4040f1050cfd09
cctired <cctired@ull.edu.es>
2017-02-16 08:34:02 +0000

    Added three ansi escape codes.
    I stumbled upon these while upgrading the firmware of HP ProCurve/Aruba switches (2530).
    
    The codes I found were:
    
     - [?6l -> Reset mode screen with option 6: 640 x 200 monochrome (graphics). The number specifies the width of the screen, it can go from 0 to 19, except 7 that resets line wrapping.
     - [?7l -> Reset line wrapping.
     - [2J -> Erase display.
    
    I added to the code the whole range for resetting the width of the screen, the reset of the line wrapping and the erase display. The reset of the line wrapping is a bit redundant but that way it's specified that it is indeed removed.

--- netmiko/base_connection.py
+++ netmiko/base_connection.py
@@ -831,12 +831,9 @@ class BaseConnection(object):
     def strip_ansi_escape_codes(string_buffer):
         """
         Remove any ANSI (VT100) ESC codes from the output
-
         http://en.wikipedia.org/wiki/ANSI_escape_code
-
         Note: this does not capture ALL possible ANSI Escape Codes only the ones
         I have encountered
-
         Current codes that are filtered:
         ESC = '\x1b' or chr(27)
         ESC = is the escape character [^ in hex ('\x1b')
@@ -845,6 +842,9 @@ class BaseConnection(object):
         ESC[E        Next line (HP does ESC-E)
         ESC[2K       Erase line
         ESC[1;24r    Enable scrolling from start to row end
+        ESC[?6l      Reset mode screen with options 640 x 200 monochrome (graphics)
+        ESC[?7l      Disable line wrapping
+        ESC[2J       Code erase display
 
         HP ProCurve's, Cisco SG300, and F5 LTM's require this (possible others)
         """
@@ -859,9 +859,15 @@ class BaseConnection(object):
         code_erase_line = chr(27) + r'\[2K'
         code_erase_start_line = chr(27) + r'\[K'
         code_enable_scroll = chr(27) + r'\[\d+;\d+r'
+        code_form_feed = chr(27) + r'\[1L'
+        code_carriage_return = chr(27) + r'\[1M'
+        code_disable_line_wrapping = chr(27) + r'\[\?7l'
+        code_reset_mode_screen_options = chr(27) + r'\[\?\d+l'
+        code_erase_display = chr(27) + r'\[2J'
 
         code_set = [code_position_cursor, code_show_cursor, code_erase_line, code_enable_scroll,
-                    code_erase_start_line]
+                    code_erase_start_line, code_form_feed, code_carriage_return, code_disable_line_wrapping,
+                    code_reset_mode_screen_options, code_erase_display]
 
         output = string_buffer
         for ansi_esc_code in code_set:
