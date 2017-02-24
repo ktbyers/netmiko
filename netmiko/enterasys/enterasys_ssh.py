505cc15bb04167ab27936fc5d51bd9d00dc945b4
Kirk Byers <ktbyers@twb-tech.com>
2017-02-24 09:43:42 -0800

    Enterasys SecureStack enable fix.

--- netmiko/enterasys/enterasys_ssh.py
+++ netmiko/enterasys/enterasys_ssh.py
@@ -10,3 +10,12 @@ class EnterasysSSH(CiscoSSHConnection):
         self.enable()
         self.set_base_prompt()
         self.disable_paging(command="set length 0\n")
+
+    def enable(self, *args, **kwargs):
+        """Enterasys SecureStack must be in 'router' mode to have enable."""
+        try:
+            super(EnterasysSSH, self).enable(*args, **kwargs)
+        except ValueError:
+            prompt = self.find_prompt()
+            if 'router' in prompt:
+                raise
