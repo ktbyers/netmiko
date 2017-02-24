1eea61884e30daaa98cc49e525d91c1aaa9d733a
Kirk Byers <ktbyers@twb-tech.com>
2017-02-24 10:03:48 -0800

    Better fix for Enterasys enable issue.

--- netmiko/enterasys/enterasys_ssh.py
+++ netmiko/enterasys/enterasys_ssh.py
@@ -1,5 +1,6 @@
 """Enterasys support."""
 from __future__ import unicode_literals
+import re
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
@@ -11,11 +12,21 @@ class EnterasysSSH(CiscoSSHConnection):
         self.set_base_prompt()
         self.disable_paging(command="set length 0\n")
 
-    def enable(self, *args, **kwargs):
-        """Enterasys SecureStack must be in 'router' mode to have enable."""
-        try:
-            super(EnterasysSSH, self).enable(*args, **kwargs)
-        except ValueError:
-            prompt = self.find_prompt()
-            if 'router' in prompt:
-                raise
+    def enable(self, cmd='enable', pattern='password', re_flags=re.IGNORECASE):
+        """Enterasys SecureStack must be in 'router' mode to have enable.
+
+        This message is generated '% Invalid input detected at '^' marker.' if enable is
+        not supported.
+        """
+        output = ""
+        if not self.check_enable_mode():
+            self.write_channel(self.normalize_cmd(cmd))
+            output += self.read_until_prompt_or_pattern(pattern=pattern, re_flags=re_flags)
+            # Check if enable command is not known (SecureStack not in 'router' mode)
+            if '% Invalid input detected at' in output:
+                return output
+            self.write_channel(self.normalize_cmd(self.secret))
+            output += self.read_until_prompt()
+            if not self.check_enable_mode():
+                raise ValueError("Failed to enter enable mode.")
+        return output
