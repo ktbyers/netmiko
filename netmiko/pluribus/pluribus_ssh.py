89dc0859ff47d3a250110816ea301f8bf3895809
Mircea Ulinic <mirucha@cloudflare.com>
2017-03-13 13:37:40 +0000

    Add disable_paging and session_preparation more specific methods

--- netmiko/pluribus/pluribus_ssh.py
+++ netmiko/pluribus/pluribus_ssh.py
@@ -2,4 +2,16 @@
 from netmiko.base_connection import BaseConnection
 
 class PluribusSSH(BaseConnection):
-    pass
+    '''Common methods for Pluribus.'''
+    def disable_paging(self):
+        '''Make sure pagins is disabled.'''
+        return super(PluribusSSH, self).disable_paging('pager off')
+    def session_preparation(self):
+        '''
+        Prepare the netmiko session:
+
+        - find the prompt
+        - disable paging
+        '''
+        self.set_base_prompt()
+        self.disable_paging()
