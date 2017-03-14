1cacb11dbbd95a5c2df9bf14b97e9a0fbeb49cb5
Mircea Ulinic <mirucha@cloudflare.com>
2017-03-14 14:36:53 +0000

    Control config mode

--- netmiko/pluribus/pluribus_ssh.py
+++ netmiko/pluribus/pluribus_ssh.py
@@ -5,6 +5,10 @@ from netmiko.base_connection import BaseConnection
 class PluribusSSH(BaseConnection):
     '''Common methods for Pluribus.'''
 
+    def __init__(self, *args, **kwargs):
+        super(PluribusSSH, self).__init__(*args, **kwargs)
+        self.__config_mode = False
+
     def disable_paging(self):
         '''Make sure paging is disabled.'''
         return super(PluribusSSH, self).disable_paging('pager off')
@@ -24,16 +28,18 @@ class PluribusSSH(BaseConnection):
         Pluribus devices don't have a special config mode.
         Therefore it can be considered as always in config mode.
         '''
-        return True
+        return self.__config_mode
 
     def config_mode(self):
         '''
         No special actions to enter in config mode.
         '''
+        self.__config_mode = True
         return ''
 
     def exit_config_mode(self):
         '''
         No special actions to exit config mode.
         '''
+        self.__config_mode = False
         return ''
