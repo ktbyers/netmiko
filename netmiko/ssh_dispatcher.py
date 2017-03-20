ebf57546422f61fb1db414671823dc4322a84b06
Alex Rodenberg <arodenberg@danmagi.com>
2017-03-20 12:21:47 +0100

    zonedirector

--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -17,6 +17,7 @@ from netmiko.brocade import BrocadeVdxSSH
 from netmiko.a10 import A10SSH
 from netmiko.avaya import AvayaVspSSH
 from netmiko.avaya import AvayaErsSSH
+from netmiko.ruckus import zdSSH
 
 # The keys of this dictionary are the supported device_types
 CLASS_MAPPER = {
@@ -37,6 +38,7 @@ CLASS_MAPPER = {
     'a10'           : A10SSH,	
     'avaya_vsp'     : AvayaVspSSH,
     'avaya_ers'     : AvayaErsSSH,
+    'ruckus_zd'     : zdSSH,
 }
 
 platforms = list(CLASS_MAPPER.keys())
