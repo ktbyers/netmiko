f7e083ed1ea602ed3ad05434938c7f3336b8f599
Skrevin <kevin.haelterman@hotmail.com>
2017-04-06 15:40:10 +0200

    Update __init__.py
    added support for USG v5 devices

--- netmiko/huawei/__init__.py
+++ netmiko/huawei/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
 from netmiko.huawei.huawei_ssh import HuaweiSSH
+from netmiko.huawei.huawei_usgv5_ssh import Huawei_usgv5_SSH
 
-__all__ = ['HuaweiSSH']
+__all__ = ['HuaweiSSH','Huawei_usgv5_SSH']
