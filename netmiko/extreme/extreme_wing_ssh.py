605d9cb3c11aed1f6a7d5c167383b5942730085c
watanabe takanobu <rreedd555@gmail.com>
2017-05-07 00:07:42 +0800

    Update extreme_wing_ssh.py
    Add delay_factor value 2 for more stable, tuned with test_netmiko_show/config.py

--- netmiko/extreme/extreme_wing_ssh.py
+++ netmiko/extreme/extreme_wing_ssh.py
@@ -7,6 +7,7 @@ class ExtremeWingSSH(CiscoSSHConnection):
     """Extreme WiNG support."""
     def session_preparation(self):
         self.set_base_prompt(pri_prompt_terminator='>',
-                             alt_prompt_terminator='#')
+                             alt_prompt_terminator='#',
+                             delay_factor=2)
         self.disable_paging(command="no page\n")
         self.set_terminal_width(command='terminal width 512')
