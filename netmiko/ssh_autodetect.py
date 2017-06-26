bf052752523c6cbb3ab12605969a737e56cf0884
bacada105 <bacada105@gmail.com>
2017-06-26 15:02:30 -0600

    Update ssh_autodetect.py

--- netmiko/ssh_autodetect.py
+++ netmiko/ssh_autodetect.py
@@ -49,6 +49,12 @@ from netmiko.base_connection import BaseConnection
 # 'dispatch' key is the SSHDetect method to call. dispatch key will be popped off dictionary
 # remaining keys indicate kwargs that will be passed to dispatch method.
 SSH_MAPPER_BASE = {
+     'alcatel_aos': {
+        "cmd": "show system",
+        "search_patterns": ["Alcatel-Lucent"],
+        "priority": 99,
+        "dispatch": "_autodetect_std",
+    },
     'arista_eos': {
         "cmd": "show version | inc rist",
         "search_patterns": ["Arista"],
