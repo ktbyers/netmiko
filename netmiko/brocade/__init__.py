ee0b6151f9706b5dd3f9efdb6e3902167981fa40
nmusser <nmuss11@gmail.com>
2017-07-06 13:56:11 -0500

    Added Fastiron and Netiron telnet imports

--- netmiko/brocade/__init__.py
+++ netmiko/brocade/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from netmiko.brocade.brocade_nos_ssh import BrocadeNosSSH
 from netmiko.brocade.brocade_fastiron_ssh import BrocadeFastironSSH
+from netmiko.brocade.brocade_fastiron_telnet import BrocadeFastironTelnet
 from netmiko.brocade.brocade_netiron_ssh import BrocadeNetironSSH
+from netmiko.brocade.brocade_netiron_telnet import BrocadeNetironTelnet
 
-__all__ = ['BrocadeNosSSH', 'BrocadeFastironSSH', 'BrocadeNetironSSH']
+__all__ = ['BrocadeNosSSH', 'BrocadeFastironSSH', 'BrocadeFastironTelnet', 'BrocadeNetironSSH', 'BrocadeNetironTelnet']
