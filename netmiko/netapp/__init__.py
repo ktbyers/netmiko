fee72a76b19841c88cfce96b9664c42aad297a4e
Koichi Miyata <kou.miyata59@gmail.com>
2017-07-20 15:14:48 +0900

    add netapp device support

---
+++ netmiko/netapp/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import unicode_literals
+from netmiko.netapp.netapp_cdot_ssh import NetAppcDotSSH
+
+__all__ = ['NetAppcDotSSH']
--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -36,6 +36,7 @@ from netmiko.juniper import JuniperSSH
 from netmiko.linux import LinuxSSH
 from netmiko.mellanox import MellanoxSSH
 from netmiko.mrv import MrvOptiswitchSSH
+from netmiko.netapp import NetAppcDotSSH
 from netmiko.ovs import OvsLinuxSSH
 from netmiko.paloalto import PaloAltoPanosSSH
 from netmiko.pluribus import PluribusSSH
@@ -88,6 +89,7 @@ CLASS_MAPPER_BASE = {
     'linux': LinuxSSH,
     'mellanox_ssh': MellanoxSSH,
     'mrv_optiswitch': MrvOptiswitchSSH,
+    'netapp_cdot': NetAppcDotSSH,
     'ovs_linux': OvsLinuxSSH,
     'paloalto_panos': PaloAltoPanosSSH,
     'pluribus': PluribusSSH,
