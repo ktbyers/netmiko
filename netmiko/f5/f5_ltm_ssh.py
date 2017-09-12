cff697e568ba54a9eee85b8b13060710ea79965b
Zhecho Zhechev <zhechev.zhecho@gmail.com>
2017-09-12 13:52:59 +0300

    Disable paging cli command depends on version
    Hello all,
    
    I think that session_preparation() should send various "disable paging CLI commands" depends on the F5 firmware version 
    I couldn't find command "set length 0" as previous cli command was set... but as we spoke I decided to leave it as well , so for version 11,12,13 it'll execute "modify cli preference pager disabled". I think that this command will work on ver.10.x.x as well but I couldn't find working v.10 for test it 
    
    Best Regards!

--- netmiko/f5/f5_ltm_ssh.py
+++ netmiko/f5/f5_ltm_ssh.py
@@ -13,10 +13,24 @@ class F5LtmSSH(BaseConnection):
         delay_factor = self.select_delay_factor(delay_factor=0)
         self._test_channel_read()
         self.set_base_prompt()
-        self.disable_paging(command="\nset length 0\n")
+        # Send disable paging cmd depends on F5 cli version
+        cmd = self.dis_paging_cmd()
+        self.disable_paging(command = cmd)
         time.sleep(1 * delay_factor)
-        self.tmsh_mode()
-        self.set_base_prompt()
+
+    def dis_paging_cmd(self):
+        """ return F5 disable_paging cli cmd """
+        try:
+            cli_cmd = 'show sys version | grep  -i version\n'
+            version_res = self.send_command(str(cli_cmd))
+            version = re.search(r'^\s+[Vv]ersion\s+(\d+).(\d+).(\d)',version_res,re.I|re.M)
+            if version.group(1) in ['11', '12', '13']:
+                return str("modify cli preference pager disabled\n")
+            else: # version smaller then 10 send old cmd
+                return str('set length 0\n')
+        except Exception as e:
+            raise ValueError("Can't find disable paging cli command")
+            raise e
 
     def tmsh_mode(self, delay_factor=1):
         """tmsh command is equivalent to config command on F5."""
