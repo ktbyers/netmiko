7ab4abb0776eeed32e7bf2c25e4acd96a428fb6a
Kirk Byers <ktbyers@twb-tech.com>
2017-10-04 19:08:33 -0700

    Restructure SCP code to support class hierarchy

--- netmiko/__init__.py
+++ netmiko/__init__.py
@@ -9,9 +9,9 @@ from netmiko.ssh_dispatcher import ConnectHandler
 from netmiko.ssh_dispatcher import ssh_dispatcher
 from netmiko.ssh_dispatcher import redispatch
 from netmiko.ssh_dispatcher import platforms
+from netmiko.ssh_dispatcher import FileTransfer
 from netmiko.scp_handler import SCPConn
-from netmiko.scp_handler import FileTransfer
-from netmiko.scp_handler import InLineTransfer
+from netmiko.cisco.cisco_ios import InLineTransfer
 from netmiko.ssh_exception import NetMikoTimeoutException
 from netmiko.ssh_exception import NetMikoAuthenticationException
 from netmiko.ssh_autodetect import SSHDetect
--- netmiko/cisco/__init__.py
+++ netmiko/cisco/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
-from netmiko.cisco.cisco_ios import CiscoIosBase, CiscoIosSSH, CiscoIosTelnet
+from netmiko.cisco.cisco_ios import CiscoIosBase, CiscoIosSSH, CiscoIosTelnet, CiscoIosFileTransfer
+from netmiko.cisco.cisco_ios import InLineTransfer
 from netmiko.cisco.cisco_asa_ssh import CiscoAsaSSH
 from netmiko.cisco.cisco_nxos_ssh import CiscoNxosSSH
 from netmiko.cisco.cisco_xr_ssh import CiscoXrSSH
@@ -8,4 +9,5 @@ from netmiko.cisco.cisco_s300 import CiscoS300SSH
 from netmiko.cisco.cisco_tp_tcce import CiscoTpTcCeSSH
 
 __all__ = ['CiscoIosSSH', 'CiscoIosTelnet', 'CiscoAsaSSH', 'CiscoNxosSSH', 'CiscoXrSSH',
-           'CiscoWlcSSH', 'CiscoS300SSH', 'CiscoTpTcCeSSH', 'CiscoIosBase']
+           'CiscoWlcSSH', 'CiscoS300SSH', 'CiscoTpTcCeSSH', 'CiscoIosBase',
+           'CiscoIosFileTransfer', 'InLineTransfer']
