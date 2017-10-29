b8f73c862c2189b9051894cc5b15b723bcbb83f3
Kirk Byers <ktbyers@twb-tech.com>
2017-10-29 12:09:16 -0700

    Adding NX-OS SCP support

--- netmiko/cisco/__init__.py
+++ netmiko/cisco/__init__.py
@@ -14,20 +14,7 @@ from netmiko.cisco.cisco_wlc_ssh import CiscoWlcSSH
 from netmiko.cisco.cisco_s300 import CiscoS300SSH
 from netmiko.cisco.cisco_tp_tcce import CiscoTpTcCeSSH
 
-__all__ = [
-    "CiscoIosSSH",
-    "CiscoIosTelnet",
-    "CiscoAsaSSH",
-    "CiscoNxosSSH",
-    "CiscoXrSSH",
-    "CiscoWlcSSH",
-    "CiscoS300SSH",
-    "CiscoTpTcCeSSH",
-    "CiscoIosBase",
-    "CiscoIosFileTransfer",
-    "InLineTransfer",
-    "CiscoAsaFileTransfer",
-    "CiscoNxosFileTransfer",
-    "CiscoIosSerial",
-    "CiscoXrFileTransfer",
-]
+__all__ = ['CiscoIosSSH', 'CiscoIosTelnet', 'CiscoAsaSSH', 'CiscoNxosSSH', 'CiscoXrSSH',
+           'CiscoWlcSSH', 'CiscoS300SSH', 'CiscoTpTcCeSSH', 'CiscoIosBase',
+           'CiscoIosFileTransfer', 'InLineTransfer', 'CiscoAsaFileTransfer',
+            'CiscoNxosFileTransfer']
--- netmiko/cisco/cisco_nxos_ssh.py
+++ netmiko/cisco/cisco_nxos_ssh.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 import re
 import time
 from netmiko.cisco_base_connection import CiscoSSHConnection
+from netmiko.cisco_base_connection import CiscoFileTransfer
+from netmiko.scp_handler import SCPConn
 
 
 class CiscoNxosSSH(CiscoSSHConnection):
@@ -21,3 +23,77 @@ class CiscoNxosSSH(CiscoSSHConnection):
         """Convert '\r\n' or '\r\r\n' to '\n, and remove extra '\r's in the text."""
         newline = re.compile(r'(\r\r\n|\r\n)')
         return newline.sub(self.RESPONSE_RETURN, a_string).replace('\r', '')
+
+
+class CiscoNxosFileTransfer(CiscoFileTransfer):
+    """Cisco NXOS SCP File Transfer driver."""
+    def __init__(self, ssh_conn, source_file, dest_file, file_system=None, direction='put'):
+        self.ssh_ctl_chan = ssh_conn
+        self.source_file = source_file
+        self.dest_file = dest_file
+        self.direction = direction
+
+        if file_system:
+            self.file_system = file_system
+        else:
+            raise ValueError("Destination file system must be specified for NX-OS")
+
+#        if direction == 'put':
+#            self.source_md5 = self.file_md5(source_file)
+#            self.file_size = os.stat(source_file).st_size
+#        elif direction == 'get':
+#            self.source_md5 = self.remote_md5(remote_file=source_file)
+#            self.file_size = self.remote_file_size(remote_file=source_file)
+#        else:
+#            raise ValueError("Invalid direction specified")
+
+    def remote_space_available(self, search_pattern=r"bytes total \((.*) bytes free\)"):
+        """Return space available on remote device."""
+        raise NotImplementedError
+
+    def verify_space_available(self, search_pattern=r"bytes total \((.*) bytes free\)"):
+        """Verify sufficient space is available on destination file system (return boolean)."""
+        raise NotImplementedError
+
+    def check_file_exists(self, remote_cmd=""):
+        """Check if the dest_file already exists on the file system (return boolean)."""
+        raise NotImplementedError
+
+    def remote_file_size(self, remote_cmd="", remote_file=None):
+        """Get the file size of the remote file."""
+        raise NotImplementedError
+
+    @staticmethod
+    def process_md5(md5_output, pattern=r"= (.*)"):
+        """
+        Process the string to retrieve the MD5 hash
+
+        Output from Cisco IOS (ASA is similar)
+        .MD5 of flash:file_name Done!
+        verify /md5 (flash:file_name) = 410db2a7015eaa42b1fe71f1bf3d59a2
+        """
+        raise NotImplementedError
+
+    def compare_md5(self, base_cmd='verify /md5'):
+        """Compare md5 of file on network device to md5 of local file"""
+        raise NotImplementedError
+
+    def remote_md5(self, base_cmd='verify /md5', remote_file=None):
+        raise NotImplementedError
+
+    def transfer_file(self):
+        """SCP transfer file."""
+        if self.direction == 'put':
+            self.put_file()
+        elif self.direction == 'get':
+            self.get_file()
+
+    def verify_file(self):
+        """Verify the file has been transferred correctly."""
+        raise NotImplementedError
+
+    def enable_scp(self, cmd=None):
+        raise NotImplementedError
+
+    def disable_scp(self, cmd=None):
+        raise NotImplementedError
