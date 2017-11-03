a9b7aa7ac99dfd96396e7a8d128aa99630503915
Kirk Byers <ktbyers@twb-tech.com>
2017-11-02 20:30:42 -0700

    First pass at Arista SCP transfer

--- netmiko/arista/arista_ssh.py
+++ netmiko/arista/arista_ssh.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import time
 from netmiko.cisco_base_connection import CiscoSSHConnection
+from netmiko.cisco_base_connection import CiscoFileTransfer
 from netmiko import log
 
 
@@ -32,3 +33,94 @@ class AristaSSH(CiscoSSHConnection):
         output = output.replace("(s2)", "")
         log.debug("check_config_mode: {0}".format(repr(output)))
         return check_string in output
+
+
+class AristaFileTransfer(CiscoFileTransfer):
+    """Cisco NXOS SCP File Transfer driver."""
+    def __init__(self, ssh_conn, source_file, dest_file, file_system=None, direction='put'):
+        self.ssh_ctl_chan = ssh_conn
+        self.source_file = source_file
+        self.dest_file = dest_file
+        self.direction = direction
+
+        if file_system:
+            self.file_system = file_system
+        else:
+            raise ValueError("Destination file system must be specified for NX-OS")
+
+#        if direction == 'put':
+#            self.source_md5 = self.file_md5(source_file)
+#            self.file_size = os.stat(source_file).st_size
+#        elif direction == 'get':
+#            self.source_md5 = self.remote_md5(remote_file=source_file)
+#            self.file_size = self.remote_file_size(remote_file=source_file)
+#        else:
+#            raise ValueError("Invalid direction specified")
+
+    def put_file(self):
+        """SCP copy the file from the local system to the remote device."""
+        destination = "{}/{}".format(self.file_system, self.dest_file)
+        self.scp_conn.scp_transfer_file(self.source_file, destination)
+        # Must close the SCP connection to get the file written (flush)
+        self.scp_conn.close()
+
+    def remote_space_available(self, search_pattern=r"(\d+) bytes free"):
+        """Return space available on remote device."""
+        return super(CiscoNxosFileTransfer, self).remote_space_available(
+            search_pattern=search_pattern
+        )
+
+    def verify_space_available(self, search_pattern=r"(\d+) bytes free"):
+        """Verify sufficient space is available on destination file system (return boolean)."""
+        return super(CiscoNxosFileTransfer, self).verify_space_available(
+            search_pattern=search_pattern
+        )
+
+    def check_file_exists(self, remote_cmd=""):
+        """Check if the dest_file already exists on the file system (return boolean)."""
+        raise NotImplementedError
+
+    def remote_file_size(self, remote_cmd="", remote_file=None):
+        """Get the file size of the remote file."""
+        if remote_file is None:
+            if self.direction == 'put':
+                remote_file = self.dest_file
+            elif self.direction == 'get':
+                remote_file = self.source_file
+
+        if not remote_cmd:
+            remote_cmd = "dir {}/{}".format(self.file_system, remote_file)
+
+        remote_out = self.ssh_ctl_chan.send_command(remote_cmd)
+        # Match line containing file name
+        escape_file_name = re.escape(remote_file)
+        pattern = r".*({}).*".format(escape_file_name)
+        match = re.search(pattern, remote_out)
+        if match:
+            file_size = match.group(0)
+            file_size = file_size.split()[0]
+
+        if 'No such file or directory' in remote_out:
+            raise IOError("Unable to find file on remote system")
+        else:
+            return int(file_size)
+
+    @staticmethod
+    def process_md5(md5_output, pattern=r"= (.*)"):
+        """Not needed on NX-OS."""
+        raise NotImplementedError
+
+    def remote_md5(self, base_cmd='show file', remote_file=None):
+        if remote_file is None:
+            if self.direction == 'put':
+                remote_file = self.dest_file
+            elif self.direction == 'get':
+                remote_file = self.source_file
+        remote_md5_cmd = "{} {}{} md5sum".format(base_cmd, self.file_system, remote_file)
+        return self.ssh_ctl_chan.send_command(remote_md5_cmd, delay_factor=3.0)
+
+    def enable_scp(self, cmd=None):
+        raise NotImplementedError
+
+    def disable_scp(self, cmd=None):
+        raise NotImplementedError
