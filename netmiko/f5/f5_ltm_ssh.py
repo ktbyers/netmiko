ac4d676c66f037a850efc452bfd8d6562ae64947
Kirk Byers <ktbyers@twb-tech.com>
2017-11-13 10:59:45 -0800

    Fixing F5 paging and terminal width

--- netmiko/f5/f5_ltm_ssh.py
+++ netmiko/f5/f5_ltm_ssh.py
@@ -7,17 +7,14 @@ class F5LtmSSH(BaseConnection):
 
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
-        delay_factor = self.select_delay_factor(delay_factor=0)
         self._test_channel_read()
         self.set_base_prompt()
-        command = "{}set length 0{}".format(self.RETURN, self.RETURN)
-        self.disable_paging(command=command)
-        time.sleep(1 * delay_factor)
         self.tmsh_mode()
         self.set_base_prompt()
-        # Clear the read buffer
-        time.sleep(.3 * self.global_delay_factor)
+        self.disable_paging(command="modify cli preference pager disabled")
         self.clear_buffer()
+        cmd = 'run /util bash -c "stty cols 255"'
+        self.set_terminal_width(command=cmd)
 
     def tmsh_mode(self, delay_factor=1):
         """tmsh command is equivalent to config command on F5."""
