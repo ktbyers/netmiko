edda0566c69d45d51bea80706e1d83a34a53d814
Kirk Byers <ktbyers@twb-tech.com>
2017-11-22 09:48:59 -0800

    Temporary test suite

--- tests/test_suite_tmp.sh
+++ tests/test_suite_tmp.sh
@@ -32,10 +32,6 @@ echo "Starting tests...good luck:" \
 && py.test -v test_netmiko_show.py --test_device hp_procurve \
 && py.test -v test_netmiko_config.py --test_device hp_procurve \
 \
-&& echo "HP Comware7" \
-&& py.test -v test_netmiko_show.py --test_device hp_comware \
-&& py.test -v test_netmiko_config.py --test_device hp_comware \
-\
 && echo "Juniper" \
 && py.test -v test_netmiko_show.py --test_device juniper_srx \
 && py.test -v test_netmiko_config.py --test_device juniper_srx \
