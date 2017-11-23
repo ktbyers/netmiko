6cc0c0f6060476cf531b45549b48b709c2e9a6fb
Kirk Byers <ktbyers@twb-tech.com>
2017-11-22 20:29:07 -0800

    Brocade to ruckus rename, cleanup

--- netmiko/ruckus/__init__.py
+++ netmiko/ruckus/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-from netmiko.ruckus.ruckus_fastiron_ssh import RuckusFastironSSH
+from netmiko.ruckus.ruckus_fastiron import RuckusFastironSSH
+from netmiko.ruckus.ruckus_fastiron import RuckusFastironTelnet
 
-
-__all__ = ['RuckusFastironSSH']
+__all__ = ['RuckusFastironSSH', 'RuckusFastironTelnet']
