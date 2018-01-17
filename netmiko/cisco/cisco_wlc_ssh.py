2569982924b08e2ebd9a6b304a01beaa2a94d1aa
Jay Crotts <jacrotts@cisco.com>
2018-01-17 14:59:29 -0800

    add save_config to cisco_wlc_ssh.py

--- netmiko/cisco/cisco_wlc_ssh.py
+++ netmiko/cisco/cisco_wlc_ssh.py
@@ -158,3 +158,9 @@ class CiscoWlcSSH(BaseConnection):
         output = self._sanitize_output(output)
         log.debug("{}".format(output))
         return output
+
+    def save_config(self):
+        """ Saves config """
+        self.exit_config_mode()  # need to be in root prompt to save config on WLC
+        self.send_command(command_string='save config', expect_string='(y/n')
+        self.send_command(command_string='y')
