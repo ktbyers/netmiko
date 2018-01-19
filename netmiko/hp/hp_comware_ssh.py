8f0bd49c73885a2dc070a49224fca7d59d8256b7
Zhecho <zhechev.zhecho@gmail.com>
2018-01-19 16:18:57 +0200

    - Adding new property enable_cmd in HPComwareSSH class   it depends on Comware Version 5/7. - add get_comware_version() - remove priviledge_escalation() / modified enable() method
    	modified:   netmiko/hp/hp_comware_ssh.py

--- netmiko/hp/hp_comware_ssh.py
+++ netmiko/hp/hp_comware_ssh.py
@@ -7,6 +7,12 @@ import re
 
 class HPComwareSSH(CiscoSSHConnection):
 
+    def __init__(self, *args, enable_cmd=None, **kwargs):
+        """ add another parameter enable_cmd ot HPcomwareSSH that depends on
+        comware version """
+        super().__init__(*args, **kwargs)
+        self.enable_cmd = enable_cmd
+
     def session_preparation(self):
         """
         Prepare the session after the connection has been established.
@@ -14,38 +20,32 @@ class HPComwareSSH(CiscoSSHConnection):
         """
         self._test_channel_read(pattern=r'[>\]]')
         self.set_base_prompt()
-        self.priv_escalation()
-        self.disable_paging(command="\nscreen-length disable\n")
+        # Comware version matters ...
+        self.com_version = self.get_comware_version()
+        if self.com_version in '5':
+            self.enable_cmd = 'super'
+        elif self.com_version in '7':
+            self.enable_cmd = 'super level-15'
+        # check if screen-length disable is permitted by some TACACS/RADIUS
+        # config and enable it after priviledge escalation "enable() method"
+        if 'denied' in self.send_command_timing("screen-length disable"):
+            self.enable()
+        self.disable_paging(command="screen-length disable\n")
 
-    def priv_escalation(self):
-        """ Privilege Escalation before entering config mode """
+    def get_comware_version(self):
+        """ send display version and grep Comware exact version """
         try:
             out_version = self.send_command_timing("display version")
-            regex_v = re.compile(r"""Comware\s+Software,\s+Version\s+(.*),
-            \sRelease\s+""", re.X)
-            match_v = regex_v.search(out_version, re.I | re.M)
+            match_v = re.search(r'Comware\s+Software,\s+Version\s+(.*),\sRelease\s+', out_version, re.I | re.M)
             self.send_command_timing('e')
-
             # Comware version 5.xx
             if match_v and match_v.group(1).startswith('5'):
-                priv_escalation_cmd = 'super'
-                self.send_command(
-                        priv_escalation_cmd,
-                        expect_string='Password:', auto_find_prompt=False)
-                self.send_command(
-                        self.secret,
-                        expect_string='>', auto_find_prompt=False)
+                return '5'
             # Comware version 7.xx
             elif match_v and match_v.group(1).startswith('7'):
-                priv_escalation_cmd = 'super level-15'
-                self.send_command(
-                        priv_escalation_cmd,
-                        expect_string='Username:', auto_find_prompt=False)
-                self.send_command(
-                        self.username,
-                        expect_string='Password:', auto_find_prompt=False)
-                self.send_command(
-                        self.secret, expect_string='>', auto_find_prompt=False)
+                return '7'
+            else:
+                raise Exception
         except Exception as e:
             raise e
 
@@ -54,7 +54,7 @@ class HPComwareSSH(CiscoSSHConnection):
         return super(
                 HPComwareSSH, self).config_mode(config_command=config_command)
 
-    def exit_config_mode(self, exit_config='return'):
+    def exit_config_mode(self, exit_config='quit'):
         """Exit config mode."""
         return super(HPComwareSSH, self).exit_config_mode(exit_config=exit_config)
 
@@ -86,11 +86,36 @@ class HPComwareSSH(CiscoSSHConnection):
         self.base_prompt = prompt
         return self.base_prompt
 
-    def enable(self, cmd='system-view'):
-        """enable mode on Comware is system-view."""
-        return self.config_mode(config_command=cmd)
+    def enable(self):
+        """enable mode on Comware depends on version 5/7."""
+        try:
+            # Comware version 5.xx
+            if self.com_version == '5':
+                self.send_command(
+                        self.enable_cmd,
+                        expect_string='Password:', auto_find_prompt=False)
+                self.send_command(
+                        self.secret,
+                        expect_string='>', auto_find_prompt=False)
+                return
+            # Comware version 7.xx
+            elif self.com_version == '7':
+                self.send_command(
+                        self.enable_cmd,
+                        expect_string='Username:', auto_find_prompt=False)
+                self.send_command(
+                        self.username,
+                        expect_string='Password:', auto_find_prompt=False)
+                self.send_command(
+                        self.secret, expect_string='>', auto_find_prompt=False)
+                return
+            else:
+                raise("Unrecognised Comware Version")
+        except Exception as e:
+            raise e
+        # return self.config_mode(config_command=self.enable_cmd)
 
-    def exit_enable_mode(self, exit_command='return'):
+    def exit_enable_mode(self, exit_command='quit'):
         """enable mode on Comware is system-view."""
         return self.exit_config_mode(exit_config=exit_command)
 
