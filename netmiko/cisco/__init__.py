ee8dfdd4b0d9b2209acfbd00be4dc66aadc61a52
Elisa Jasinska <elisa@jasinska.de>
2018-01-22 11:08:49 -0400

    adding extreme telnet class

--- netmiko/cisco/__init__.py
+++ netmiko/cisco/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
-from netmiko.cisco.cisco_ios import CiscoIosBase, CiscoIosSSH, CiscoIosTelnet, CiscoIosSerial
-from netmiko.cisco.cisco_ios import CiscoIosFileTransfer
+from netmiko.cisco.cisco_ios import CiscoIosBase, CiscoIosSSH, CiscoIosTelnet, CiscoIosFileTransfer, CiscoIosSerial
 from netmiko.cisco.cisco_ios import InLineTransfer
 from netmiko.cisco.cisco_asa_ssh import CiscoAsaSSH, CiscoAsaFileTransfer
 from netmiko.cisco.cisco_nxos_ssh import CiscoNxosSSH, CiscoNxosFileTransfer
