2c50bca1681ada6f49aa5386a18da442e4512fcc
Jay Crotts <jacrotts@cisco.com>
2018-01-22 15:26:53 -0800

    Add save_config for brocade VDX

--- netmiko/brocade/brocade_nos_ssh.py
+++ netmiko/brocade/brocade_nos_ssh.py
@@ -19,3 +19,8 @@ class BrocadeNosSSH(CiscoSSHConnection):
         delay_factor = self.select_delay_factor(delay_factor)
         self.write_channel(self.RETURN)
         time.sleep(1 * delay_factor)
+
+    def save_config(self):
+        """Save Config for Brocade VDX."""
+        self.send_command('copy running-config startup-config', '[Y/N]')
+        self.send_command('y')
