b3768c1ba09253fbf04e03fd1eb7bde7cca245e3
Jay Crotts <jacrotts@cisco.com>
2018-01-22 16:40:00 -0800

    Add save_config for dell devices

--- netmiko/dell/dell_force10_ssh.py
+++ netmiko/dell/dell_force10_ssh.py
@@ -5,4 +5,7 @@ from netmiko.cisco_base_connection import CiscoSSHConnection
 
 class DellForce10SSH(CiscoSSHConnection):
     """Dell Force10 Driver - supports DNOS9."""
-    pass
+
+    def save_config(self):
+        """Save Config on DellForce10SSH"""
+        self.send_command('copy running-configuration startup-configuration')
