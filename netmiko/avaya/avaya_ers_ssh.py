0e02ad68d956844ff328b50ad2c2be1b0f4ad297
Craig Gruelle <cgruelle@gmail.com>
2018-01-25 11:25:00 -0500

    Avaya ERS devices can have a prompt longer than 20 chars in config mode.

--- netmiko/avaya/avaya_ers_ssh.py
+++ netmiko/avaya/avaya_ers_ssh.py
@@ -1,7 +1,10 @@
 """Netmiko support for Avaya Ethernet Routing Switch."""
 from __future__ import print_function
 from __future__ import unicode_literals
+
+import re
 import time
+
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 # Avaya presents Enter Ctrl-Y to begin.
@@ -10,15 +13,42 @@ CTRL_Y = '\x19'
 
 class AvayaErsSSH(CiscoSSHConnection):
     """Netmiko support for Avaya Ethernet Routing Switch."""
+
+    def config_mode(self, config_command='config term', pattern=''):
+        """
+
+        Enter into configuration mode on remote device.
+
+        Avaya ERS devices can have a prompt longer than 20 characters in
+        config mode.
+        """
+        if not pattern:
+            pattern = re.escape(self.base_prompt)
+        return super(CiscoSSHConnection, self).config_mode(config_command=config_command,
+                                                            pattern=pattern)    
+    def check_config_mode(self, check_string='#', pattern=''):
+            """
+
+            Checks if the device is in configuration mode or not.
+            
+            Avaya ERS devices can have a prompt longer than 20 characters in
+            config mode.
+            """
+            if not pattern:
+                pattern = re.escape(self.base_prompt)
+            return super(CiscoSSHConnection, self).check_config_mode(check_string=check_string,
+                                                                      pattern=pattern)    
     def special_login_handler(self, delay_factor=1):
         """
         Avaya ERS presents the following as part of the login process:
 
         Enter Ctrl-Y to begin.
+
+        Some Avaya ERS devices present a menu interface upon login
         """
         delay_factor = self.select_delay_factor(delay_factor)
 
-        # Handle 'Enter Ctrl-Y to begin'
+        # Handle 'Enter Ctrl-Y to begin' and Menu if present
         output = ""
         i = 0
         while i <= 6:
