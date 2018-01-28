d98a0653b000564ad22e1f9806b5676e2f13cbae
Kirk Byers <ktbyers@twb-tech.com>
2018-01-28 10:01:03 -0800

    Fix linter errors

--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -5,7 +5,8 @@ from netmiko.a10 import A10SSH
 from netmiko.accedian import AccedianSSH
 from netmiko.alcatel import AlcatelAosSSH
 from netmiko.alcatel import AlcatelSrosSSH
-from netmiko.arista import AristaSSH, AristaFileTransfer
+from netmiko.arista import AristaSSH
+# from netmiko.arista import AristaFileTransfer
 from netmiko.aruba import ArubaSSH
 from netmiko.avaya import AvayaErsSSH
 from netmiko.avaya import AvayaVspSSH
@@ -35,7 +36,8 @@ from netmiko.f5 import F5LtmSSH
 from netmiko.fortinet import FortinetSSH
 from netmiko.hp import HPProcurveSSH, HPComwareSSH
 from netmiko.huawei import HuaweiSSH, HuaweiVrpv8SSH
-from netmiko.juniper import JuniperSSH, JuniperFileTransfer
+from netmiko.juniper import JuniperSSH
+# from netmiko.juniper import JuniperFileTransfer
 from netmiko.linux import LinuxSSH
 from netmiko.mellanox import MellanoxSSH
 from netmiko.mrv import MrvOptiswitchSSH
@@ -110,12 +112,12 @@ CLASS_MAPPER_BASE = {
 }
 
 FILE_TRANSFER_MAP = {
-#    'arista_eos': AristaFileTransfer,
+    # 'arista_eos': AristaFileTransfer,
     'cisco_asa': CiscoAsaFileTransfer,
     'cisco_ios': CiscoIosFileTransfer,
     'cisco_xe': CiscoIosFileTransfer,
     'cisco_nxos': CiscoNxosFileTransfer,
-#    'juniper_junos': JuniperFileTransfer,
+    # 'juniper_junos': JuniperFileTransfer,
 }
 
 # Also support keys that end in _ssh
