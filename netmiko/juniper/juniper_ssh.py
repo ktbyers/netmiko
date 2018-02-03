632e95cb68dc5d94bd7e2849896c1db4a709376a
Kirk Byers <ktbyers@twb-tech.com>
2018-02-03 09:12:35 -0800

    Fixing linter

--- netmiko/juniper/juniper_ssh.py
+++ netmiko/juniper/juniper_ssh.py
@@ -186,10 +186,9 @@ class JuniperSSH(BaseConnection):
                 return self.RESPONSE_RETURN.join(response_list[:-1])
         return a_string
 
-     def save_config(self, cmd='', confirm=True, confirm_response=''):
-         """Not Implemented"""
-         raise NotImplementedError
-
+    def save_config(self, cmd='', confirm=True, confirm_response=''):
+        """Not Implemented"""
+        raise NotImplementedError
 
 
 class JuniperFileTransfer(BaseFileTransfer):
