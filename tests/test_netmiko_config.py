5bedf1ea434ce8b28202a93a8f5cc539ce3148fe
jcrotts <crotts.jay@gmail.com>
2018-02-10 21:03:19 -0800

    Add test cases for save_config method and add example commands for tests

--- tests/test_netmiko_config.py
+++ tests/test_netmiko_config.py
@@ -10,6 +10,7 @@ test_config_mode: verify enter config mode
 test_exit_config_mode: verify exit config mode
 test_command_set: verify sending a set of config commands
 test_commands_from_file: verify sending a set of config commands from a file
+test_save_config: verify save config with default values
 test_disconnect: cleanly disconnect the SSH session
 
 """
@@ -100,6 +101,69 @@ def test_commands_from_file(net_connect, commands, expected_responses):
     else:
         print("Skipping test (no file specified)...",)
 
+def test_save_base(net_connect, commands, expected_responses):
+    '''
+    Test save config with no options.
+    '''
+    save_verify = expected_responses['save_config']
+
+    cmd_response = net_connect.save_config()
+    assert save_verify in cmd_response
+
+def test_save_confirm(net_connect, commands, expected_responses):
+    '''
+    Test save config with the confirm parameter.
+    '''
+    confirm = commands['save_config_confirm']
+    save_verify = expected_responses['save_config']
+
+    cmd_response = net_connect.save_config(confirm)
+    assert save_verify in cmd_response
+
+def test_save_response(net_connect, commands, expected_responses):
+    '''
+    Test save config with the confirm response parameter.
+    '''
+    confirm_response = commands['save_config_response']
+    save_verify = expected_responses['save_config']
+
+    cmd_response = net_connect.save_config(confirm_response=confirm_response)
+    assert save_verify in cmd_response
+
+def test_save_cmd(net_connect, commands, expected_responses):
+    '''
+    Test save config with cmd parameter.
+    '''
+    cmd = commands['save_config_cmd']
+    save_verify = expected_responses['save_config']
+
+    cmd_response = net_connect.save_config(cmd=cmd)
+    assert save_verify in cmd_response
+
+def test_save_confirm_response(net_connect, commands, expected_responses):
+    '''
+    Test save config with confirm and confirm response parameters
+    '''
+    confirm = commands['save_config_confirm']
+    confirm_response = commands['save_config_response']
+    save_verify = expected_responses['save_config']
+
+    cmd_response = net_connect.save_config(confirm=confirm, 
+                                          confirm_response=confirm_response)
+    assert save_verify in cmd_response    
+
+def test_save_all(net_connect, commands, expected_responses):
+    '''
+    Test the save config method with all additional parameters.
+    '''
+    cmd = commands['save_config_cmd'] 
+    confirm = commands['save_config_confirm']
+    confirm_response = commands['save_config_response']
+    save_verify = expected_responses['save_config']
+
+    cmd_response = net_connect.save_config(cmd=cmd, confirm=confirm, 
+                                           confirm_response=confirm_response)
+    assert save_verify in cmd_response
 
 def test_disconnect(net_connect, commands, expected_responses):
     '''
