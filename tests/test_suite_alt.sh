8d7506e0e9f346cb5abd55c85b5fe9b1dd1dcd05
Kirk Byers <ktbyers@twb-tech.com>
2018-02-24 19:44:44 -0800

    Updating test suite

--- tests/test_suite_alt.sh
+++ tests/test_suite_alt.sh
@@ -41,6 +41,7 @@ echo "Starting tests...good luck:" \
 && py.test -v test_netmiko_config.py --test_device hp_comware \
 \
 && echo "Juniper" \
+&& py.test -v test_netmiko_scp.py --test_device juniper_srx \
 && py.test -v test_netmiko_show.py --test_device juniper_srx \
 && py.test -v test_netmiko_config.py --test_device juniper_srx \
 && py.test -v test_netmiko_commit.py --test_device juniper_srx \
