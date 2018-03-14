33484025ca530ec9896235ad83f7a09397e0b669
thomasbo <thomasbo@microsoft.com>
2018-03-13 21:35:36 -0700

    cleaning up

--- netmiko/citrix/netscaler_ssh.py
+++ netmiko/citrix/netscaler_ssh.py
@@ -1,4 +1,7 @@
+import time
+
 from netmiko.base_connection import BaseConnection
+
 
 class NetscalerSSH(BaseConnection):
     """ Netscaler SSH class. """
@@ -13,18 +16,13 @@ class NetscalerSSH(BaseConnection):
         self.disable_paging(command="\nset cli mode -page OFF\n")
         time.sleep(1 * delay_factor)
         self.set_base_prompt()
+        time.sleep(.3 * self.global_delay_factor)
+        self.clear_buffer()
 
     def send_config_set(self, config_commands=None, exit_config_mode=True, **kwargs):
         """ Nothing to exit"""
         return super(NetscalerSSH, self).send_config_set(config_commands=config_commands,
                                                          exit_config_mode=False, **kwargs)
-
-    @staticmethod
-    def normalize_linefeeds(a_string):
-        """Convert '\r\n' or '\r\r\n' to '\n, and remove '\r's in the text."""
-        # Done appears post-commands at times, blow it away.
-        newline = re.compile(r'(\r\n|\r\n\r\n|\r\r\n|\n\r|\r)')
-        return newline.sub('\n', a_string)
 
     def strip_prompt(self, a_string):
         """ Strip 'Done' from command output """
