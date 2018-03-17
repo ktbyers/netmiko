32961cab7df64bae8e4e951d2131456991983875
Kirk Byers <ktbyers@twb-tech.com>
2018-03-17 09:52:49 -0700

    Calix B6 return character

--- netmiko/calix/calix_b6.py
+++ netmiko/calix/calix_b6.py
@@ -10,7 +10,7 @@ from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
 class SSHClient_noauth(SSHClient):
-    """Override _auth method for noauth"""
+    """Set noauth when manually handling SSH authentication."""
     def _auth(self, username, *args):
         self._transport.auth_none(username)
         return
@@ -18,6 +18,11 @@ class SSHClient_noauth(SSHClient):
 
 class CalixB6Base(CiscoSSHConnection):
     """Common methods for Calix B6, both SSH and Telnet."""
+    def __init__(self, *args, **kwargs):
+        default_enter = kwargs.get('default_enter')
+        kwargs['default_enter'] = '\r\n' if default_enter is None else default_enter
+        super(CalixB6SSH, self).__init__(*args, **kwargs)
+
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
         self.ansi_escape_codes = True
@@ -48,8 +53,10 @@ class CalixB6Base(CiscoSSHConnection):
                 elif 'Password:' in output:
                     self.write_channel(self.password + self.RETURN)
                     break
+                time.sleep(delay_factor * 0.5)
             else:
                 self.write_channel(self.RETURN)
+                time.sleep(delay_factor * 1)
             i += 1
 
     def check_config_mode(self, check_string=')#', pattern=''):
@@ -60,19 +67,23 @@ class CalixB6Base(CiscoSSHConnection):
     def save_config(self, cmd='copy run start', confirm=False):
         return super(CalixB6Base, self).save_config(cmd=cmd, confirm=confirm)
 
+    def exit_config_mode(self, exit_config=None, pattern=''):
+        """Exit from configuration mode."""
+        if exit_config is None:
+            exit_config = 'exit{}{}end'.format(self.RETURN, self.RETURN)
+        return super(CalixB6SSH, self).exit_config_mode(exit_config=exit_config, pattern=pattern)
+
 
 class CalixB6SSH(CalixB6Base):
-    """
-    Calix B6 SSH Driver.
-    To make it work, we have to override the SSHClient _auth method.
-    If we use login/password, the ssh server use the (none) auth mechanism.
+    """Calix B6 SSH Driver.
+
+    To make it work, we have to override the SSHClient _auth method and manually handle
+    the username/password.
     """
     def _build_ssh_client(self):
-        """Prepare for Paramiko SSH connection.
-        See base_connection.py file for any updates.
-        """
+        """Prepare for Paramiko SSH connection."""
         # Create instance of SSHClient object
-        # If username is cli, we use noauth
+        # If not using SSH keys, we use noauth
         if not self.use_keys:
             remote_conn_pre = SSHClient_noauth()
         else:
@@ -84,8 +95,7 @@ class CalixB6SSH(CalixB6Base):
         if self.alt_host_keys and path.isfile(self.alt_key_file):
             remote_conn_pre.load_host_keys(self.alt_key_file)
 
-        # Default is to automatically add untrusted hosts (make sure
-        # appropriate for your env)
+        # Default is to automatically add untrusted hosts (make sure appropriate for your env)
         remote_conn_pre.set_missing_host_key_policy(self.key_policy)
         return remote_conn_pre
 
