3ba8af66d0c6ebbdd4a3799b819a72292da2c6c5
Kirk Byers <ktbyers@twb-tech.com>
2018-03-17 09:26:10 -0700

    Make return character an attribute

--- netmiko/citrix/netscaler_ssh.py
+++ netmiko/citrix/netscaler_ssh.py
@@ -13,22 +13,23 @@ class NetscalerSSH(BaseConnection):
         delay_factor = self.select_delay_factor(delay_factor=0)
         self._test_channel_read()
         self.set_base_prompt()
-        self.disable_paging(command="\nset cli mode -page OFF\n")
+        cmd = "{}set cli mode -page OFF{}".format(self.RETURN, self.RETURN)
+        self.disable_paging(command=cmd)
         time.sleep(1 * delay_factor)
         self.set_base_prompt()
-        time.sleep(.3 * self.global_delay_factor)
+        time.sleep(.3 * delay_factor)
         self.clear_buffer()
 
     def send_config_set(self, config_commands=None, exit_config_mode=True, **kwargs):
-        """ Nothing to exit"""
+        """Nothing to exit"""
         return super(NetscalerSSH, self).send_config_set(config_commands=config_commands,
                                                          exit_config_mode=False, **kwargs)
 
     def strip_prompt(self, a_string):
         """ Strip 'Done' from command output """
         output = super(NetscalerSSH, self).strip_prompt(a_string)
-        lines = output.split('\n')
+        lines = output.split(self.RESPONSE_RETURN)
         if "Done" in lines[-1]:
-            return '\n'.join(lines[:-1])
+            return 'self.RESPONSE_RETURN'.join(lines[:-1])
         else:
             return output
