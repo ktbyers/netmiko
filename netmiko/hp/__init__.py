4b7cfe279e7c9d5c8100b693621b541e4bdebf2c
gininfra <36296911+gininfra@users.noreply.github.com>
2018-03-23 23:51:23 +0700

    Update netmiko/hp/__init__.py
    from netmiko.hp.hp_procurvejinhua_ssh import HPProcurveJinhuaSSH
    from netmiko.hp.hp_procurve512900_ssh import HPProcurve512900SSH
    __all__ = ['HPProcurveSSH', 'HPComwareSSH', 'HPProcurveJinhuaSSH', 'HPProcurve512900SSH']

--- netmiko/hp/__init__.py
+++ netmiko/hp/__init__.py
@@ -1,5 +1,8 @@
+__init__.py
 from __future__ import unicode_literals
 from netmiko.hp.hp_procurve_ssh import HPProcurveSSH
+from netmiko.hp.hp_procurvejinhua_ssh import HPProcurveJinhuaSSH
+from netmiko.hp.hp_procurve512900_ssh import HPProcurve512900SSH
 from netmiko.hp.hp_comware_ssh import HPComwareSSH
-
-__all__ = ['HPProcurveSSH', 'HPComwareSSH']
+ 
+__all__ = ['HPProcurveSSH', 'HPComwareSSH', 'HPProcurveJinhuaSSH', 'HPProcurve512900SSH']
