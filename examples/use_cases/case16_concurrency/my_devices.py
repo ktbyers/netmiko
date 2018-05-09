2794b145161b10d5b86a0fe784a766bcd6b7d874
Kirk Byers <ktbyers@twb-tech.com>
2018-05-09 14:54:09 -0700

    Updating ip addresses

--- examples/use_cases/case16_concurrency/my_devices.py
+++ examples/use_cases/case16_concurrency/my_devices.py
@@ -4,49 +4,49 @@ std_pwd = getpass("Enter standard password: ")
 
 pynet_rtr1 = {
     'device_type': 'cisco_ios',
-    'ip': '184.105.247.70',
+    'ip': '10.10.247.70',
     'username': 'pyclass',
     'password': std_pwd,
 }
 
 pynet_rtr2 = {
     'device_type': 'cisco_ios',
-    'ip': '184.105.247.71',
+    'ip': '10.10.247.71',
     'username': 'pyclass',
     'password': std_pwd,
 }
 
 pynet_sw1 = {
     'device_type': 'arista_eos',
-    'ip': '184.105.247.72',
+    'ip': '10.10.247.72',
     'username': 'pyclass',
     'password': std_pwd,
 }
 
 pynet_sw2 = {
     'device_type': 'arista_eos',
-    'ip': '184.105.247.73',
+    'ip': '10.10.247.73',
     'username': 'pyclass',
     'password': std_pwd,
 }
 
 pynet_sw3 = {
     'device_type': 'arista_eos',
-    'ip': '184.105.247.74',
+    'ip': '10.10.247.74',
     'username': 'pyclass',
     'password': std_pwd,
 }
 
 pynet_sw4 = {
     'device_type': 'arista_eos',
-    'ip': '184.105.247.75',
+    'ip': '10.10.247.75',
     'username': 'pyclass',
     'password': std_pwd,
 }
 
 juniper_srx = {
     'device_type': 'juniper_junos',
-    'ip': '184.105.247.76',
+    'ip': '10.10.247.76',
     'username': 'pyclass',
     'password': std_pwd,
 }
