916ae0c0e414d23bb325ea64f6a755cafb46bb13
Stephen Kiely <stephen@snjnetworks.com>
2018-07-11 10:44:33 -0500

    Fix pylint errors found with Pylama

--- netmiko/rad/rad_etx.py
+++ netmiko/rad/rad_etx.py
@@ -1,15 +1,15 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 import time
-import re
 from netmiko.base_connection import BaseConnection
+
 
 class RadETXBase(BaseConnection):
     """RAD ETX Support, Tested on RAD 203AX, 205A and 220A"""
     def session_preparation(self):
         self._test_channel_read()
         self.set_base_prompt()
-        self.disable_paging(command="config term length 0")
+        self.disable_paging(command='config term length 0')
         # Clear the read buffer
         time.sleep(.3 * self.global_delay_factor)
         self.clear_buffer()
@@ -27,7 +27,7 @@ class RadETXBase(BaseConnection):
             # Some devices are slow so match on trailing-prompt if you can
             output = self.send_command(command_string=cmd)
         return output
-    
+
     def enable(self, *args, **kwargs):
         """The Rad ETX software does not have an enable"""
         pass
@@ -39,7 +39,8 @@ class RadETXBase(BaseConnection):
         This needs some extended testing.
         """
         return super(RadETXBase, self).config_mode(config_command=config_command,
-                                                            pattern=pattern)
+                                                   pattern=pattern)
+
     def check_config_mode(self, check_string='>config', pattern=''):
         """
         Checks if the device is in configuration mode or not.
@@ -47,16 +48,21 @@ class RadETXBase(BaseConnection):
         Rad config starts with baseprompt>config
         """
         return super(RadETXBase, self).check_config_mode(check_string=check_string,
-                                                                  pattern=pattern)
+                                                         pattern=pattern)
 
     def exit_config_mode(self, exit_config='exit all', pattern='#'):
         """Exit from configuration mode."""
         return super(RadETXBase, self).exit_config_mode(exit_config=exit_config,
-                                                                 pattern=pattern)
+                                                        pattern=pattern)
+
 
 class RadETXSSH(RadETXBase):
-    """RAD ETX SSH Support"""
+    """RAD ETX SSH Support.
+
+    Found that a global_delay_factor of 2 is needed at minimum for SSH to the Rad ETX.
+    """
     pass
+
 
 class RadETXTelnet(RadETXBase):
     """RAD ETX Telnet Support"""
