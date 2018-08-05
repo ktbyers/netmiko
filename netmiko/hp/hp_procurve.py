9e5966a0de70f1389fb71d51aaeb805638345106
Connor Quick <connor_quick@harvard.edu>
2018-08-05 15:53:52 -0400

    Allows passwordless SSH to hp_procurve devices
    If user is does not specify a password or enables SSH keys, try to set
    noauth to login without a password.
    
    squash: fixes path import and pep8 compliance

--- netmiko/hp/hp_procurve.py
+++ netmiko/hp/hp_procurve.py
@@ -3,8 +3,17 @@ from __future__ import unicode_literals
 import re
 import time
 import socket
+from os import path
+from paramiko import SSHClient
 from netmiko.cisco_base_connection import CiscoSSHConnection
 from netmiko import log
+
+
+class SSHClient_noauth(SSHClient):
+    """Set noauth when manually handling SSH authentication."""
+    def _auth(self, username, *args):
+        self._transport.auth_none(username)
+        return
 
 
 class HPProcurveBase(CiscoSSHConnection):
@@ -84,7 +93,30 @@ class HPProcurveBase(CiscoSSHConnection):
 
 
 class HPProcurveSSH(HPProcurveBase):
-    pass
+    """HP Procurve SSH Driver.
+
+    To make it work, we have to override the SSHClient _auth method to invoke
+    the noauth function if there is no password and no SSH keys.
+    """
+    def _build_ssh_client(self):
+        """Prepare for Paramiko SSH connection."""
+        # Create instance of SSHClient object
+        # If not using SSH keys and no password
+        # specified, we use noauth
+        if not self.use_keys and not self.password:
+            remote_conn_pre = SSHClient_noauth()
+        else:
+            remote_conn_pre = SSHClient()
+
+        # Load host_keys for better SSH security
+        if self.system_host_keys:
+            remote_conn_pre.load_system_host_keys()
+        if self.alt_host_keys and path.isfile(self.alt_key_file):
+            remote_conn_pre.load_host_keys(self.alt_key_file)
+
+        # Default is to automatically add untrusted hosts (make sure appropriate for your env)
+        remote_conn_pre.set_missing_host_key_policy(self.key_policy)
+        return remote_conn_pre
 
 
 class HPProcurveTelnet(HPProcurveBase):
