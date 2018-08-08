2640137d564be207d5bbf9713b8a3949b7c5ced9
Kirk Byers <ktbyers@twb-tech.com>
2018-08-08 15:47:27 -0700

    IPInfusion enable + enter issue

--- netmiko/ipinfusion/ipinfusion_ocnos.py
+++ netmiko/ipinfusion/ipinfusion_ocnos.py
@@ -7,9 +7,8 @@ from netmiko.cisco_base_connection import CiscoBaseConnection
 class IpInfusionOcNOSBase(CiscoBaseConnection):
     """Common Methods for IP Infusion OcNOS support."""
     def __init__(self, **kwargs):
-        if kwargs.get('default_enter') is None and kwargs.get('response_return') is None:
-            kwargs['default_enter'] = '\r\n'
-            kwargs['response_return'] = '\r\n'
+        if kwargs.get('default_enter') is None:
+            kwargs['default_enter'] = '\r'
         return super(IpInfusionOcNOSBase, self).__init__(**kwargs)
 
     def session_preparation(self):
