6abb0cc83f955d4542d812c90f0b886898fead3b
labulaka521 <labulakalia@gmail.com>
2018-08-15 14:58:26 +0800

    fix chinese become empty

--- netmiko/utilities.py
+++ netmiko/utilities.py
@@ -162,12 +162,12 @@ def write_bytes(out_data):
     """Write Python2 and Python3 compatible byte stream."""
     if sys.version_info[0] >= 3:
         if isinstance(out_data, type(u'')):
-            return out_data.encode('ascii', 'ignore')
+            return out_data.encode('utf-8')
         elif isinstance(out_data, type(b'')):
             return out_data
     else:
         if isinstance(out_data, type(u'')):
-            return out_data.encode('ascii', 'ignore')
+            return out_data.encode('utf-8')
         elif isinstance(out_data, type(str(''))):
             return out_data
     msg = "Invalid value for out_data neither unicode nor byte string: {0}".format(out_data)
