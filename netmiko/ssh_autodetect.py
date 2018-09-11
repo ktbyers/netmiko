33ee393ecc5f1dc6563556f6c7f2c9ea440f017a
TomCos <tom.cosmos@oracle.com>
2018-09-11 15:28:35 -0500

    add support to detect an F5 system the the user logs into the tmsh shell directly

--- netmiko/ssh_autodetect.py
+++ netmiko/ssh_autodetect.py
@@ -135,6 +135,12 @@ SSH_MAPPER_BASE = {
         "priority": 99,
         "dispatch": "_autodetect_std",
     },
+    'f5_ltm': {
+        "cmd": "show sys version",
+        "search_patterns": [r"BIG-IP"],
+        "priority": 99,
+        "dispatch": "_autodetect_std",
+    }
 }
 
 
@@ -275,6 +281,8 @@ class SSHDetect(object):
             r'syntax error, expecting',
             r'Error: Unrecognized command',
             r'%Error'
+            r'command not found'
+            r'Syntax Error: unexpected argument'
         ]
         if not cmd or not search_patterns:
             return 0
