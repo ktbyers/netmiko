e5c243b22e1eb01bba4bf5af106c33025febb0d8
Kirk Byers <ktbyers@twb-tech.com>
2018-10-09 20:30:10 -0700

    Removing obsolete development status

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
     author_email='ktbyers@twb-tech.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
