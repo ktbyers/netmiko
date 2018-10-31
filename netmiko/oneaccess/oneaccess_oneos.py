0acbc46ccc3629e141417d2543798b5a86e6ebf3
Maarten Wallraf <maarten.wallraf@orange.com>
2018-10-31 21:38:45 +0100

    Update tests for Oneaccss OneOS telnet + ssh;  Update Oneaccess OneOS telnet method

--- netmiko/oneaccess/oneaccess_oneos.py
+++ netmiko/oneaccess/oneaccess_oneos.py
@@ -1,17 +1,34 @@
-"""OneAccess ONEOS support."""
+"""Netmiko driver for OneAccess ONEOS"""
+
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from netmiko.cisco_base_connection import CiscoSSHConnection
-from netmiko.base_connection import BaseConnection
+from netmiko.cisco_base_connection import CiscoBaseConnection
+import time
 
 
-class OneaccessOneosSSH(CiscoSSHConnection):
+class OneaccessOneos(CiscoBaseConnection):
     def __init__(self, *args, **kwargs):
+        """Init connection - similar as Cisco"""
         default_enter = kwargs.get("default_enter")
         kwargs["default_enter"] = "\r\n" if default_enter is None else default_enter
-        super(OneaccessOneosSSH, self).__init__(*args, **kwargs)
+        super(OneaccessOneos, self).__init__(*args, **kwargs)
 
-class OneaccessOneosTelnet(BaseConnection):
+    def session_preparation(self):
+        """Prepare connection - disable paging"""
+        self._test_channel_read()
+        self.set_base_prompt()
+        self.disable_paging(command="term len 0")
+        # Clear the read buffer
+        time.sleep(0.3 * self.global_delay_factor)
+        self.clear_buffer()
+
+    def save_config(self, cmd="write mem", confirm=False):
+        """Save config: write mem"""
+        return super(OneaccessOneos, self).save_config(cmd=cmd, confirm=confirm)
+
+class OneaccessOneosSSH(OneaccessOneos):
     pass
 
+class OneaccessOneosTelnet(OneaccessOneos):
+    pass
--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -69,7 +69,7 @@ from netmiko.terminal_server import TerminalServerSSH
 from netmiko.terminal_server import TerminalServerTelnet
 from netmiko.ubiquiti import UbiquitiEdgeSSH
 from netmiko.vyos import VyOSSSH
-from netmiko.oneaccess import OneaccessOneosSSH
+from netmiko.oneaccess import OneaccessOneosTelnet, OneaccessOneosSSH
 
 
 # The keys of this dictionary are the supported device_types
@@ -195,6 +195,7 @@ CLASS_MAPPER["juniper_junos_telnet"] = JuniperTelnet
 CLASS_MAPPER["paloalto_panos_telnet"] = PaloAltoPanosTelnet
 CLASS_MAPPER["rad_etx_telnet"] = RadETXTelnet
 CLASS_MAPPER["ruckus_fastiron_telnet"] = RuckusFastironTelnet
+CLASS_MAPPER["oneaccess_oneos_telnet"] = OneaccessOneosTelnet
 
 # Add serial drivers
 CLASS_MAPPER["cisco_ios_serial"] = CiscoIosSerial
