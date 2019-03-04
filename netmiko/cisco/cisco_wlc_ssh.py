b062c2ce66befa8dfe085312321ea10f5fc103f1
Jeremy White <jeremy.lee.w@me.com>
2019-03-05 01:54:25 +0300

    Changing raise to use NetMikoAuthenticationException instead of valueError

--- netmiko/cisco/cisco_wlc_ssh.py
+++ netmiko/cisco/cisco_wlc_ssh.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import time
 import re
 
+from netmiko.ssh_exception import NetMikoAuthenticationException
 from netmiko.base_connection import BaseConnection
 from netmiko.py23_compat import string_types
 from netmiko import log
@@ -104,7 +105,8 @@ class CiscoWlcSSH(BaseConnection):
         try:
             self.set_base_prompt()
         except ValueError:
-            raise ValueError("Incorrect username and/or password.")
+            msg = "Authentication failed: {}".format(self.host)
+            raise NetMikoAuthenticationException(msg)
 
         self.disable_paging(command="config paging disable")
         # Clear the read buffer
