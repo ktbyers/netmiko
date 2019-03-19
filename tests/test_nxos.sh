c0633278f5ec90281b2f620a2b9b2bc2e711a1bf
Kirk Byers <ktbyers@twb-tech.com>
2019-03-19 08:49:19 -0700

    Fixing MD5 / carriage return issue on NXOS 9K (#1140)

--- tests/test_nxos.sh
+++ tests/test_nxos.sh
@@ -4,6 +4,7 @@ RETURN_CODE=0
 
 echo "Starting tests...good luck:" \
 && echo "Cisco NXOS" \
+&& py.test -v test_netmiko_scp.py --test_device nxos1 \
 && py.test -v test_netmiko_show.py --test_device nxos1 \
 && py.test -v test_netmiko_config.py --test_device nxos1 \
 || RETURN_CODE=1
