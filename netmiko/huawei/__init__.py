ba7d9e6687c9a6fa2609c14601508dfbc86b3efb
Kirk Byers <ktbyers@twb-tech.com>
2019-04-22 13:01:13 -0700

    Update huawei telnet driver support

--- netmiko/huawei/__init__.py
+++ netmiko/huawei/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-from netmiko.huawei.huawei_ssh import HuaweiSSH, HuaweiVrpv8SSH
-from netmiko.huawei.huawei_telnet import HuaweiTelnet
+from netmiko.huawei.huawei import HuaweiSSH, HuaweiVrpv8SSH
+from netmiko.huawei.huawei import HuaweiTelnet
 
 __all__ = ["HuaweiSSH", "HuaweiVrpv8SSH", "HuaweiTelnet"]
