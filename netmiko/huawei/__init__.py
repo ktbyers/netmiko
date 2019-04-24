338f745f3b58bcf642634a0334d3da60822fd479
Kirk Byers <ktbyers@twb-tech.com>
2019-04-24 12:54:00 -0700

    Updated Huawei telnet support PR (#1176)

--- netmiko/huawei/__init__.py
+++ netmiko/huawei/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
-from netmiko.huawei.huawei_ssh import HuaweiSSH, HuaweiVrpv8SSH
+from netmiko.huawei.huawei import HuaweiSSH, HuaweiVrpv8SSH
+from netmiko.huawei.huawei import HuaweiTelnet
 
-__all__ = ["HuaweiSSH", "HuaweiVrpv8SSH"]
+__all__ = ["HuaweiSSH", "HuaweiVrpv8SSH", "HuaweiTelnet"]
