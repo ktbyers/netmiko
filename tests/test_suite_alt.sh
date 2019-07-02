7965d27570c479d5a7a16fdd7b784aef35180980
Kirk Byers <ktbyers@twb-tech.com>
2019-07-01 17:03:50 -0700

    Minor test updates (#1270)

--- tests/test_suite_alt.sh
+++ tests/test_suite_alt.sh
@@ -4,21 +4,23 @@ RETURN_CODE=0
 
 # Exit on the first test failure and set RETURN_CODE = 1
 echo "Starting tests...good luck:" \
+&& echo "Cisco IOS-XE SSH (including SCP)" \
+&& py.test -v test_netmiko_scp.py --test_device cisco3 \
+&& py.test -v test_netmiko_show.py --test_device cisco3 \
+&& py.test -v test_netmiko_config.py --test_device cisco3 \
+\
 && echo "Cisco IOS SSH (including SCP) using key auth" \
-&& py.test -v test_netmiko_scp.py --test_device cisco881_key \
 && py.test -v test_netmiko_tcl.py --test_device cisco881_key \
 && py.test -v test_netmiko_show.py --test_device cisco881_key \
 && py.test -v test_netmiko_config.py --test_device cisco881_key \
 \
 && echo "Cisco IOS SSH (including SCP)" \
-&& py.test -v test_netmiko_scp.py --test_device cisco881 \
 && py.test -v test_netmiko_tcl.py --test_device cisco881 \
 && py.test -v test_netmiko_show.py --test_device cisco881 \
 && py.test -v test_netmiko_config.py --test_device cisco881 \
 && py.test -v test_netmiko_sesssion_log.py --test_device cisco881_slog \
 \
 && echo "Cisco IOS SSH fast_cli (including SCP)" \
-&& py.test -v test_netmiko_scp.py --test_device cisco881_fast \
 && py.test -v test_netmiko_tcl.py --test_device cisco881_fast \
 && py.test -v test_netmiko_show.py --test_device cisco881_fast \
 && py.test -v test_netmiko_config.py --test_device cisco881_fast \
@@ -39,10 +41,6 @@ echo "Starting tests...good luck:" \
 && py.test -v test_netmiko_scp.py --test_device arista_sw \
 && py.test -v test_netmiko_show.py --test_device arista_sw \
 && py.test -v test_netmiko_config.py --test_device arista_sw \
-\
-&& echo "HP ProCurve" \
-&& py.test -v test_netmiko_show.py --test_device hp_procurve \
-&& py.test -v test_netmiko_config.py --test_device hp_procurve \
 \
 && echo "Juniper" \
 && py.test -v test_netmiko_scp.py --test_device juniper_srx \
@@ -78,6 +76,10 @@ echo "Starting tests...good luck:" \
 && py.test -s -v test_netmiko_autodetect.py --test_device cisco_asa \
 && py.test -s -v test_netmiko_autodetect.py --test_device cisco_xrv \
 \
+&& echo "HP ProCurve" \
+&& py.test -v test_netmiko_show.py --test_device hp_procurve \
+&& py.test -v test_netmiko_config.py --test_device hp_procurve \
+\
 || RETURN_CODE=1
 
 exit $RETURN_CODE
@@ -86,3 +88,7 @@ exit $RETURN_CODE
 # && py.test -v test_netmiko_show.py --test_device hp_comware \
 # && py.test -v test_netmiko_config.py --test_device hp_comware \
 # \
+# Can't get Cisco IOS and SCP get to run reliably--IOS bug?
+# && py.test -v test_netmiko_scp.py --test_device cisco881_key \
+# && py.test -v test_netmiko_scp.py --test_device cisco881 \
+# && py.test -v test_netmiko_scp.py --test_device cisco881_fast \
