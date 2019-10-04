9b8164e36771f123d3a446ad5833b44f6c8f5912
user01 <lacrosse100@gmail.com>
2019-10-05 01:39:03 +0900

    added support for Yamaha routers and firewalls

--- netmiko/yamaha/__init__.py
+++ netmiko/yamaha/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
 from netmiko.yamaha.yamaha import YamahaSSH, YamahaTelnet
-#from netmiko.yamaha.yamaha_ssh import YamahaSSH
 
 __all__ = ["YamahaSSH", "YamahaTelnet"]
