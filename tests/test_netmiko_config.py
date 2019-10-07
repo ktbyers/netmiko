6afb391c2499e26bf19ee32e20eaabc8f6482424
carl <carl.r.montanari@gmail.com>
2019-10-07 13:13:34 -0700

    add large acl to test command buffering

--- tests/test_netmiko_config.py
+++ tests/test_netmiko_config.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import ipaddress
 
 
 def test_ssh_connect(net_connect, commands, expected_responses):
@@ -20,7 +21,7 @@ def test_enable_mode(net_connect, commands, expected_responses):
         enable_prompt = net_connect.find_prompt()
         assert enable_prompt == expected_responses["enable_prompt"]
     except AttributeError:
-        assert True == True
+        assert True is True
 
 
 def test_config_mode(net_connect, commands, expected_responses):
@@ -89,4 +90,21 @@ def test_disconnect(net_connect, commands, expected_responses):
     """
     Terminate the SSH session
     """
+    net_connect.disconnect()
+
+
+def test_large_acl(net_connect, acl_entries=500):
+    """
+    Test creating an ACL with tons of lines
+    """
+    if net_connect.device_type not in ["cisco_xe", "cisco_ios"]:
+        return
+    net_connect.send_config_set(["no ip access-list extended netmiko_test_large_acl"])
+    cfg_lines = ["ip access-list extended netmiko_test_large_acl"]
+    for i in range(1, acl_entries + 1):
+        cfg_lines.append(f"permit ip host {ipaddress.ip_address('192.168.0.0') + i} any")
+    net_connect.send_config_set(cfg_lines)
+    result = net_connect.send_command("show ip access-lists netmiko_test_large_acl")
+    assert len(result.splitlines()) == len(cfg_lines)
+    net_connect.send_config_set(["no ip access-list extended netmiko_test_large_acl"])
     net_connect.disconnect()
