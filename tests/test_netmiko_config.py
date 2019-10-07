60ff59bc85c81a07877755e38bff00d2dec14ae1
carl <carl.r.montanari@gmail.com>
2019-10-07 14:10:40 -0700

    add comments and a second assert

--- tests/test_netmiko_config.py
+++ tests/test_netmiko_config.py
@@ -105,8 +105,12 @@ def test_large_acl(net_connect, acl_entries=1000):
         cfg_lines.append(
             f"permit ip host {ipaddress.ip_address('192.168.0.0') + i} any"
         )
-    net_connect.send_config_set(cfg_lines)
-    result = net_connect.send_command("show ip access-lists netmiko_test_large_acl")
-    assert len(result.splitlines()) == len(cfg_lines)
+    result = net_connect.send_config_set(cfg_lines)
+    # check that the result of send_config_set returns same num lines + four lines for getting
+    # into and out of config mode
+    assert len(result.splitlines()) == len(cfg_lines) + 4
+    verify = net_connect.send_command("show ip access-lists netmiko_test_large_acl")
+    # check that length of lines in show of the acl matches lines configured
+    assert len(verify.splitlines()) == len(cfg_lines)
     net_connect.send_config_set(["no ip access-list extended netmiko_test_large_acl"])
     net_connect.disconnect()
