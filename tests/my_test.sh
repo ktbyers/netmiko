6a760412d5fb3f59a94e9948fd2dd3d9f8b007a2
Kirk Byers <ktbyers@twb-tech.com>
2019-10-31 18:19:16 -0700

    Test improvements (#1441)

--- tests/my_test.sh
+++ tests/my_test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 for i in `seq 5` ; do 
+    py.test -s -v test_netmiko_scp.py --test_device cisco3 --pdb
     py.test -s -v test_netmiko_show.py --test_device cisco3 --pdb
     py.test -s -v test_netmiko_config.py --test_device cisco3 --pdb
     py.test -s -v test_netmiko_config_acl.py --test_device cisco3 --pdb
