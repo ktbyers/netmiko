ed66bb5e0d5da62e88c0e2101f356aa67f3a9720
Kirk Byers <ktbyers@twb-tech.com>
2019-12-02 20:09:01 -0800

    Minor updates to screenos driver

--- netmiko/juniper/juniper_screenos.py
+++ netmiko/juniper/juniper_screenos.py
@@ -2,7 +2,7 @@ import time
 from netmiko.base_connection import BaseConnection
 
 
-class JuniperScreenOsBase(BaseConnection):
+class JuniperScreenOsSSH(BaseConnection):
     """
     Implement methods for interacting with Juniper ScreenOS devices.
     """
@@ -23,30 +23,28 @@ class JuniperScreenOsBase(BaseConnection):
 
     def check_enable_mode(self, *args, **kwargs):
         """No enable mode on Juniper ScreenOS."""
-        pass
+        return True
 
     def enable(self, *args, **kwargs):
         """No enable mode on Juniper ScreenOS."""
-        pass
+        return ""
 
-    def check_config_mode(self, check_string=""):
-        """Checks whether in configuration mode. Returns a boolean."""
-        return self._in_config_mode
+    def exit_enable_mode(self, *args, **kwargs):
+        """No enable mode on Juniper ScreenOS."""
+        return ""
+
+    def check_config_mode(self, *args, **kwargs):
+        """No configuration mode on Juniper ScreenOS."""
+        return False
+
+    def config_mode(self, *args, **kwargs):
+        """No configuration mode on Juniper ScreenOS."""
+        return ""
+
+    def exit_config_mode(self, *args, **kwargs):
+        """No configuration mode on Juniper ScreenOS."""
+        return ""
 
     def save_config(self, cmd="save config", confirm=False, confirm_response=""):
         """Save Config."""
         return self.send_command(command_string=cmd)
-
-    def config_mode(self):
-        """No configuration mode on Juniper ScreenOS."""
-        self._in_config_mode = True
-        return ""
-
-    def exit_config_mode(self, exit_config="-"):
-        """No configuration mode on Juniper ScreenOS."""
-        self._in_config_mode = False
-        return ""
-
-
-class JuniperScreenOsSSH(JuniperScreenOsBase):
-    pass
