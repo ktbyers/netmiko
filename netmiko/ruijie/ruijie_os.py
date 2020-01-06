3daa140b13a76cc0fb21b0cd6a05f225aaeb78d6
Kirk Byers <ktbyers@twb-tech.com>
2020-01-05 19:47:09 -0800

    removing unneeded method

--- netmiko/ruijie/ruijie_os.py
+++ netmiko/ruijie/ruijie_os.py
@@ -16,29 +16,6 @@ class RuijieOSBase(CiscoBaseConnection):
         time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
 
-    def special_login_handler(self, delay_factor=1):
-        """
-        Username: ****
-        Password: ****
-        """
-        delay_factor = self.select_delay_factor(delay_factor)
-        i = 0
-        time.sleep(delay_factor * 0.5)
-        output = ""
-        while i <= 12:
-            output = self.read_channel()
-            if output:
-                if "username:" in output.lower():
-                    self.write_channel(self.username + self.RETURN)
-                elif "password:" in output.lower():
-                    self.write_channel(self.password + self.RETURN)
-                    break
-                time.sleep(delay_factor * 1)
-            else:
-                self.write_channel(self.RETURN)
-                time.sleep(delay_factor * 1.5)
-            i += 1
-
     def save_config(self, cmd="write", confirm=False, confirm_response=""):
         """Save config: write"""
         return super().save_config(
