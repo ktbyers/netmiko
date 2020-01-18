dc24e85ae105d5b8e79abfb94ed9b9ffd86c06b7
Paul Parsons <parsonsp@localhost.localdomain>
2020-01-18 18:55:59 +0000

    Initial implementation to support Netgear ProSafe devices

--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -65,6 +65,7 @@ from netmiko.mrv import MrvLxSSH
 from netmiko.mrv import MrvOptiswitchSSH
 from netmiko.netapp import NetAppcDotSSH
 from netmiko.nokia import NokiaSrosSSH, NokiaSrosFileTransfer
+from netmiko.netgear import NetgearProSafeSSH
 from netmiko.oneaccess import OneaccessOneOSTelnet, OneaccessOneOSSSH
 from netmiko.ovs import OvsLinuxSSH
 from netmiko.paloalto import PaloAltoPanosSSH
@@ -173,6 +174,7 @@ CLASS_MAPPER_BASE = {
     "mrv_lx": MrvLxSSH,
     "mrv_optiswitch": MrvOptiswitchSSH,
     "netapp_cdot": NetAppcDotSSH,
+    "netgear_prosafe": NetgearProSafeSSH,
     "netscaler": NetscalerSSH,
     "nokia_sros": NokiaSrosSSH,
     "oneaccess_oneos": OneaccessOneOSSSH,
