05b55e0447f85cfd1df9d6afd7471d76e197c9e1
sandyw777 <sandywu5133@gmail.com>
2020-03-03 17:16:57 +0800

    Added support for Supermicro Blade/ToR switches.

---
+++ netmiko/supermicro/__init__.py
@@ -0,0 +1,5 @@
+from netmiko.supermicro.smci_switch_usp import (
+    SmciSwitchUspSerial,
+    SmciSwitchUspTelnet,
+    SmciSwitchUspSSH
+)
---
+++ netmiko/supermicro/smci_switch_usp.py
@@ -0,0 +1,13 @@
+from netmiko.supermicro.smci_base_connection import SmciBaseConnection
+
+
+class SmciSwitchUspSSH(SmciBaseConnection):
+    pass
+
+
+class SmciSwitchUspTelnet(SmciBaseConnection):
+    pass
+
+
+class SmciSwitchUspSerial(SmciBaseConnection):
+    pass
