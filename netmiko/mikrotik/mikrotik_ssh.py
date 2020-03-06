901075b3c57ce402b3d09b4d54b943ce8b25d088
Kirk Byers <ktbyers@twb-tech.com>
2020-03-06 11:30:09 -0800

    Mikrotik fix strip_prompt

--- netmiko/mikrotik/mikrotik_ssh.py
+++ netmiko/mikrotik/mikrotik_ssh.py
@@ -73,7 +73,11 @@ class MikrotikBase(CiscoSSHConnection):
         :type a_string: str
         """
         response_list = a_string.split(self.RESPONSE_RETURN)
-        last_line = response_list[-2]
+        try:
+            last_line = response_list[-2]
+        except IndexError:
+            return a_string
+
         if self.base_prompt in last_line:
             return self.RESPONSE_RETURN.join(response_list[:-2])
         else:
