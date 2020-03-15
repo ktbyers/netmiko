dc1e11297154ca6d704fa375f9d528f100eb3dd1
Kirk Byers <ktbyers@twb-tech.com>
2020-03-15 12:04:33 -0700

    Working on global_cmd_verify attribute

--- tests/test_netmiko_show.py
+++ tests/test_netmiko_show.py
@@ -17,11 +17,13 @@ test_disconnect: cleanly disconnect the SSH session
 import pytest
 import time
 from datetime import datetime
+from netmiko.utilities import select_cmd_verify
 
-# import logging
 
-# logging.basicConfig(filename="test.log", level=logging.DEBUG)
-# logger = logging.getLogger("netmiko")
+@select_cmd_verify
+def bogus_func(obj, *args, **kwargs):
+    """Function that just returns the arguments modified by the decorator."""
+    return (obj, args, kwargs)
 
 
 def test_disable_paging(net_connect, commands, expected_responses):
@@ -58,11 +60,52 @@ def test_send_command_timing(net_connect, commands, expected_responses):
 
 def test_send_command(net_connect, commands, expected_responses):
     """Verify a command can be sent down the channel successfully using send_command method."""
-    time.sleep(1)
     net_connect.clear_buffer()
     show_ip_alt = net_connect.send_command(commands["basic"])
     assert expected_responses["interface_ip"] in show_ip_alt
     show_ip_alt = net_connect.send_command(commands["basic"], cmd_verify=False)
+    assert expected_responses["interface_ip"] in show_ip_alt
+
+
+def test_cmd_verify_decorator(net_connect_cmd_verify):
+    obj = net_connect_cmd_verify
+    # Global False should have precedence
+    assert obj.global_cmd_verify is False
+    (obj, args, kwargs) = bogus_func(net_connect_cmd_verify, cmd_verify=True)
+    assert kwargs["cmd_verify"] is False
+    (obj, args, kwargs) = bogus_func(net_connect_cmd_verify, cmd_verify=False)
+    assert kwargs["cmd_verify"] is False
+
+    # Global True should have precedence
+    obj.global_cmd_verify = True
+    assert obj.global_cmd_verify is True
+    (obj, args, kwargs) = bogus_func(net_connect_cmd_verify, cmd_verify=True)
+    assert kwargs["cmd_verify"] is True
+    (obj, args, kwargs) = bogus_func(net_connect_cmd_verify, cmd_verify=False)
+    assert kwargs["cmd_verify"] is True
+
+    # None should track the local argument
+    obj.global_cmd_verify = None
+    assert obj.global_cmd_verify is None
+    (obj, args, kwargs) = bogus_func(net_connect_cmd_verify, cmd_verify=True)
+    assert kwargs["cmd_verify"] is True
+    (obj, args, kwargs) = bogus_func(net_connect_cmd_verify, cmd_verify=False)
+    assert kwargs["cmd_verify"] is False
+
+
+def test_send_command_global_cmd_verify(
+    net_connect_cmd_verify, commands, expected_responses
+):
+    """
+    Verify a command can be sent down the channel successfully using send_command method.
+
+    Disable cmd_verify globally.
+    """
+    net_connect = net_connect_cmd_verify
+    net_connect.clear_buffer()
+    # cmd_verify should be disabled globally at this point
+    assert net_connect.global_cmd_verify is False
+    show_ip_alt = net_connect.send_command(commands["basic"])
     assert expected_responses["interface_ip"] in show_ip_alt
 
 
