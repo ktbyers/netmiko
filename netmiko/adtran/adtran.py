3c80755ccbcb1dd57ce9a1a0586f4807c7580ee5
Brandon Spendlove <bspendlove@automate.thn.lon.network.as201838.net>
2020-03-22 12:09:21 +0000

    Testing Adtran SSH

--- netmiko/adtran/adtran.py
+++ netmiko/adtran/adtran.py
@@ -15,26 +15,5 @@ class AdtranOSBase(CiscoBaseConnection):
         time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
 
-    def strip_ansi_escape_codes(self, string_buffer):
-        """
-        Huawei does a strange thing where they add a space and then add ESC[1D
-        to move the cursor to the left one.
-
-                Adtran also encounters this same issue like Huawei when executing long commands...
-                This function works as intended with Adtran also (Taken from Huawei)
-
-        The extra space is problematic.
-        """
-        code_cursor_left = chr(27) + r"\[\d+D"
-        output = string_buffer
-        pattern = rf" {code_cursor_left}"
-        output = re.sub(pattern, "", output)
-
-        log.debug("Stripping ANSI escape codes")
-        log.debug(f"new_output = {output}")
-        log.debug(f"repr = {repr(output)}")
-        return super().strip_ansi_escape_codes(output)
-
-
 class AdtranOSSSH(AdtranOSBase):
     pass
