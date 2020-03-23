2c7dfd5bcf32a7fad7b321068892093cd41c1a76
TanY0Y0 <443608814@qq.com>
2020-03-23 15:54:29 +0800

    format 2

--- netmiko/yotc/yotc.py
+++ netmiko/yotc/yotc.py
@@ -82,7 +82,7 @@ class YotcBase(CiscoBaseConnection):
         # Clear the read buffer
         time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
-    
+
     def check_config_mode(self, check_string=")#", pattern="#"):
         """
         Checks if the device is in configuration mode or not.
