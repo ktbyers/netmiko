3a66f1cab80377d4e7e9312758eddac45c3b035b
Чобанов Николай Степанович <nikolay.chobanov@wifire.ru>
2020-03-25 03:22:51 +0300

    add inherit telnet_login

--- netmiko/huawei/huawei.py
+++ netmiko/huawei/huawei.py
@@ -237,6 +237,8 @@ class HuaweiVrpv8SSH(HuaweiSSH):
 
 
 class HuaweiTelnetOLT8000(CiscoBaseConnection):
+    telnet_login = HuaweiTelnet.telnet_login
+
     def disable_paging(self, command="scroll"):
         return super().disable_paging(command=command)
 
@@ -261,78 +263,6 @@ class HuaweiTelnetOLT8000(CiscoBaseConnection):
         time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
 
-    def telnet_login(
-        self,
-        pri_prompt_terminator=r"]\s*$",
-        alt_prompt_terminator=r">\s*$",
-        username_pattern=r"(?:user:|username|login|user name)",
-        pwd_pattern=r"assword",
-        delay_factor=1,
-        max_loops=20,
-    ):
-        """Telnet login for Huawei Devices"""
-
-        delay_factor = self.select_delay_factor(delay_factor)
-        password_change_prompt = r"(Change now|Please choose 'YES' or 'NO').+"
-        combined_pattern = r"({}|{}|{})".format(
-            pri_prompt_terminator, alt_prompt_terminator, password_change_prompt
-        )
-
-        output = ""
-        return_msg = ""
-        i = 1
-        while i <= max_loops:
-            try:
-                # Search for username pattern / send username
-                output = self.read_until_pattern(
-                    pattern=username_pattern, re_flags=re.I
-                )
-                return_msg += output
-                self.write_channel(self.username + self.TELNET_RETURN)
-
-                # Search for password pattern / send password
-                output = self.read_until_pattern(pattern=pwd_pattern, re_flags=re.I)
-                return_msg += output
-                self.write_channel(self.password + self.TELNET_RETURN)
-
-                # Waiting for combined output
-                output = self.read_until_pattern(pattern=combined_pattern)
-                return_msg += output
-
-                # Search for password change prompt, send "N"
-                if re.search(password_change_prompt, output):
-                    self.write_channel("N" + self.TELNET_RETURN)
-                    output = self.read_until_pattern(pattern=combined_pattern)
-                    return_msg += output
-
-                # Check if proper data received
-                if re.search(pri_prompt_terminator, output, flags=re.M) or re.search(
-                    alt_prompt_terminator, output, flags=re.M
-                ):
-                    return return_msg
-
-                self.write_channel(self.TELNET_RETURN)
-                time.sleep(0.5 * delay_factor)
-                i += 1
-
-            except EOFError:
-                self.remote_conn.close()
-                msg = f"Login failed: {self.host}"
-                raise NetmikoAuthenticationException(msg)
-
-        # Last try to see if we already logged in
-        self.write_channel(self.TELNET_RETURN)
-        time.sleep(0.5 * delay_factor)
-        output = self.read_channel()
-        return_msg += output
-        if re.search(pri_prompt_terminator, output, flags=re.M) or re.search(
-            alt_prompt_terminator, output, flags=re.M
-        ):
-            return return_msg
-
-        self.remote_conn.close()
-        msg = f"Login failed: {self.host}"
-        raise NetmikoAuthenticationException(msg)
 
     def cleanup(self):
         """Return paging before disconnect"""
