3832f588f7ef4ca341f57633d24ad182a44c69f4
Patryk Szulczewski <patryk@szulczewski.eu>
2020-05-11 17:37:45 +0200

    SCP nokia_sros: Fix to check_file_exists() to support sub-directories (#1724)

--- netmiko/nokia/nokia_sros_ssh.py
+++ netmiko/nokia/nokia_sros_ssh.py
@@ -222,10 +222,11 @@ class NokiaSrosFileTransfer(BaseFileTransfer):
                 remote_cmd = self._file_cmd_prefix() + "file dir {}/{}".format(
                     self.file_system, self.dest_file
                 )
+            dest_file_name = self.dest_file.replace("\\", "/").split("/")[-1]
             remote_out = self.ssh_ctl_chan.send_command(remote_cmd)
             if "File Not Found" in remote_out:
                 return False
-            elif self.dest_file in remote_out:
+            elif dest_file_name in remote_out:
                 return True
             else:
                 raise ValueError("Unexpected output from check_file_exists")
