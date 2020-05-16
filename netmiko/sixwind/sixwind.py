ad5b85e34ddb133e404bfea2b714f80b08c623aa
Pau Nadeu Rabat <pau.nadeu@everywan.com>
2020-05-17 01:28:56 +0200

    I want solve LINT test.

--- netmiko/sixwind/sixwind.py
+++ netmiko/sixwind/sixwind.py
@@ -5,11 +5,13 @@ from netmiko.cisco_base_connection import CiscoBaseConnection
 class SixwindBase(CiscoBaseConnection):
 
     def disable_paging(self, *args, **kwargs):
-        """6WIND requires | no-pager at the end of command, not implemented at this time."""
-        return ""
+        """6WIND requires no-pager at the end of command, not implemented at this time."""
+
+    pass
 
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
+
         self.ansi_escape_codes = True
         self._test_channel_read()
         self.set_base_prompt()
@@ -19,6 +21,7 @@ class SixwindBase(CiscoBaseConnection):
 
     def set_base_prompt(self, pri_prompt_terminator=">", alt_prompt_terminator="#", delay_factor=1):
         """Sets self.base_prompt: used as delimiter for stripping of trailing prompt in output."""
+
         prompt = super().set_base_prompt(pri_prompt_terminator=pri_prompt_terminator,
                                          alt_prompt_terminator=alt_prompt_terminator,
                                          delay_factor=delay_factor)
@@ -28,6 +31,7 @@ class SixwindBase(CiscoBaseConnection):
 
     def config_mode(self, config_command="edit running"):
         """Enter configuration mode."""
+
         return super().config_mode(config_command=config_command)
 
     def commit(self, comment="", delay_factor=1):
@@ -38,6 +42,7 @@ class SixwindBase(CiscoBaseConnection):
         default:
            command_string = commit
         """
+
         delay_factor = self.select_delay_factor(delay_factor)
         error_marker = "Failed to generate committed config"
         command_string = "commit"
@@ -53,26 +58,32 @@ class SixwindBase(CiscoBaseConnection):
 
     def exit_config_mode(self, exit_config="exit", pattern=r">"):
         """Exit configuration mode."""
+
         return super().exit_config_mode(exit_config=exit_config, pattern=pattern)
 
     def check_config_mode(self, check_string="#"):
         """Checks whether in configuration mode. Returns a boolean."""
+
         return super().check_config_mode(check_string=check_string)
 
     def save_config(self, cmd="copy running startup", confirm=True, confirm_response="y"):
         """ Save Config for 6WIND"""
+
         return super().save_config(cmd=cmd, confirm=confirm, confirm_response=confirm_response)
 
     def check_enable_mode(self, *args, **kwargs):
         """6WIND has no enable mode."""
+
         pass
 
     def enable(self, *args, **kwargs):
         """6WIND has no enable mode."""
+
         pass
 
     def exit_enable_mode(self, *args, **kwargs):
         """6WIND has no enable mode."""
+
         pass
 
 
