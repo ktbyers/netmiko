dd3616f41ac64fddc09cb6c6898ab434401e6ee7
Andrew Riachi <ariachi@ku.edu>
2020-06-24 14:16:21 -0500

    discover that ArubaOS-Switch is just Procurve OS

--- netmiko/aruba/__init__.py
+++ netmiko/aruba/__init__.py
@@ -1,3 +1,4 @@
 from netmiko.aruba.aruba_ssh import ArubaSSH
+from netmiko.aruba.aruba_switch_ssh import ArubaSwitchSSH
 
-__all__ = ["ArubaSSH"]
+__all__ = ["ArubaSSH", "ArubaSwitchSSH"]
---
+++ netmiko/aruba/aruba_switch_ssh.py
@@ -0,0 +1,5 @@
+from netmiko.hp.hp_procurve import HPProcurveBase
+
+class ArubaSwitchSSH(HPProcurveBase):
+    """ArubaOS Switch is basically a rebranding of the Procurve OS."""
+    pass
