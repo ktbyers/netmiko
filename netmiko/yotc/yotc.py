52af87337fd9a10a8b034ffa049a4a2c6a50f144
Kirk Byers <ktbyers@twb-tech.com>
2020-08-04 21:22:42 -0700

    Updates to YOTC driver

--- netmiko/yotc/yotc.py
+++ netmiko/yotc/yotc.py
@@ -1,41 +1,54 @@
 from netmiko.cisco_base_connection import CiscoBaseConnection
 import re
 import time
-from netmiko import log
 from telnetlib import IAC, DO, DONT, WILL, WONT, SB, SE, ECHO, SGA, NAWS
 from netmiko.utilities import get_structured_data, get_structured_data_genie
 
 
 class YotcBase(CiscoBaseConnection):
-    def send_command_more(
+    def session_preparation(self):
+        """Prepare the session after the connection has been established."""
+        self._test_channel_read(pattern=r"[>#]")
+        self.set_base_prompt()
+        self.enable()
+        self.config_mode()
+        self.disable_paging("command max-lines 1000")
+        self.exit_config_mode()
+        # Clear the read buffer
+        time.sleep(0.3 * self.global_delay_factor)
+        self.clear_buffer()
+
+    def _send_command_more(
         self,
         command_string,
         use_textfsm=False,
         use_genie=False,
         textfsm_template=None,
-        cmd_echo=True,
+        cmd_verify=False,
+        strip_prompt=True,
+        strip_command=True,
         **kwargs,
     ):
         """
         yotc has no disable_paging command, need to handle it in show command.
         cisco_wlc_ssh.py has similar function: send_command_w_enter().
         """
-        self.disable_paging("command max-lines 1000")
         prompt = self.find_prompt()
-        more_str_re = f" --More-- \b\b\b\b\b\b\b\b\b\b          \
-            \b\b\b\b\b\b\b\b\b\b\n?|(\n?{re.escape(prompt)} ?\n?)+"
-        buffer = self.send_command_timing(
-            command_string, cmd_echo=cmd_echo, use_textfsm=False, **kwargs
-        )
-        output = buffer
-        log.debug(f"send_command_more first output: {output}")
-        while prompt not in buffer:
-            buffer = self.send_command_timing(
-                "", strip_command=False, strip_prompt=False, cmd_echo=cmd_echo
+        more_str_re = r".*--More--.*"
+        new_data = ""
+        output = ""
+        while prompt not in new_data:
+            new_data = self.send_command_timing(
+                " ", cmd_verify=False, use_textfsm=False, use_genie=False, **kwargs
             )
-            log.debug(f"send_command_more buffer: {buffer}")
-            output += buffer
+            output += new_data
         output = re.sub(more_str_re, "", output)
+        output = self._sanitize_output(
+            output,
+            strip_command=strip_command,
+            command_string=command_string,
+            strip_prompt=strip_prompt,
+        )
 
         if use_textfsm:
             structured_output = get_structured_data(
@@ -58,41 +71,19 @@ class YotcBase(CiscoBaseConnection):
 
         return output
 
-    def session_preparation(self):
-        """Prepare the session after the connection has been established."""
-
-        self._test_channel_read(pattern=r"[>#]")
-        self.set_base_prompt()
-        self.enable()
-        self.config_mode()
-        # self.disable_paging("command max-lines 1000")
-        # Clear the read buffer
-        time.sleep(0.3 * self.global_delay_factor)
-        self.clear_buffer()
-
     def check_config_mode(self, check_string=")#", pattern="#"):
-        """
-        Checks if the device is in configuration mode or not.
-        """
+        """Checks if the device is in configuration mode or not."""
         return super().check_config_mode(check_string=check_string, pattern=pattern)
 
-    def save_config(self, cmd="wr", confirm_response="y"):
+    def save_config(self, cmd="wr", confirm=True, confirm_response="y"):
         """Saves Config."""
         self.exit_config_mode()
-        self.enable()
-        output = self.send_command_timing(
-            command_string=cmd, strip_prompt=False, strip_command=False
+        return super().save_config(
+            cmd=cmd, confirm=confirm, confirm_response=confirm_response
         )
-        output += self.send_command_timing(
-            confirm_response, strip_prompt=False, strip_command=False, normalize=False
-        )
-        return output
 
     def cleanup(self):
-        """
-        no use exit command to disconnect
-        :return:
-        """
+        """Don't use 'exit' to disconnect."""
         pass
 
 
@@ -122,9 +113,7 @@ class YotcSSH(YotcBase):
 class YotcTelnet(YotcBase):
     @staticmethod
     def _process_option(telnet_sock, cmd, opt):
-        """
-        enable ECHO, SGA, set window size to [500, 50]
-        """
+        """enable ECHO, SGA, set window size to [500, 50]."""
         if cmd == WILL:
             if opt in [ECHO, SGA]:
                 # reply DO ECHO / DO SGA
