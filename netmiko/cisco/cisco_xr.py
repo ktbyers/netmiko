99099ff50ee2710e43b31b7faca378a8af9aa2b8
Taisuke Sasaki <taisasak@cisco.com>
2020-08-19 09:43:18 +0900

    CiscoXRBase: improved session preparation

--- netmiko/cisco/cisco_xr.py
+++ netmiko/cisco/cisco_xr.py
@@ -10,12 +10,9 @@ class CiscoXrBase(CiscoBaseConnection):
 
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
-        self._test_channel_read()
         self.set_base_prompt()
         self.disable_paging()
-        self.set_terminal_width(command="terminal width 511")
         # Clear the read buffer
-        time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
 
     def send_config_set(self, config_commands=None, exit_config_mode=False, **kwargs):
