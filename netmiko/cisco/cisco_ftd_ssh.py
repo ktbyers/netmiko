d119cb90d558f0635c7001adaa232033f41bf41f
Kirk Byers <ktbyers@twb-tech.com>
2020-09-02 12:34:31 -0700

    Updates on FTD driver

--- netmiko/cisco/cisco_ftd_ssh.py
+++ netmiko/cisco/cisco_ftd_ssh.py
@@ -1,32 +1,33 @@
 """Subclass specific to Cisco FTD."""
-import time
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
 class CiscoFtdSSH(CiscoSSHConnection):
     """Subclass specific to Cisco FTD."""
 
+    def __init__(self, *args, **kwargs):
+        # FTD defaults to fast_cli=True and legacy_mode=False
+        kwargs.setdefault("fast_cli", True)
+        kwargs.setdefault("_legacy_mode", False)
+        return super().__init__(*args, **kwargs)
+
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
         self._test_channel_read()
         self.set_base_prompt()
 
-        # Clear the read buffer
-        time.sleep(0.3 * self.global_delay_factor)
-        self.clear_buffer()
-
-    def send_config_set(self, config_commands=None):
+    def send_config_set(self, *args, **kwargs):
         """Canot change config on FTD via ssh"""
         raise NotImplementedError
 
-    def enable(self):
-        """no enable mode on firepower ssh"""
-        raise AttributeError
-
-    def config_mode(self, config_command="", pattern=""):
-        """No Config mode on firepower ssh"""
+    def enable(self, *args, **kwargs):
+        """No enable mode on firepower ssh"""
         return ""
 
-    def check_config_mode(self, check_string="", pattern=""):
-        """No Config mode on firepower ssh"""
+    def config_mode(self, *args, **kwargs):
+        """No config mode on firepower ssh"""
+        return ""
+
+    def check_config_mode(self, *args, **kwargs):
+        """No config mode on firepower ssh"""
         return False
