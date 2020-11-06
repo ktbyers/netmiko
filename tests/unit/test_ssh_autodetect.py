492c33b644838204813ace40e6761c5a90ded339
Kirk Byers <ktbyers@twb-tech.com>
2020-11-06 15:59:19 -0800

    Black

--- tests/unit/test_ssh_autodetect.py
+++ tests/unit/test_ssh_autodetect.py
@@ -3,5 +3,5 @@ from netmiko.ssh_autodetect import SSH_MAPPER_BASE
 
 
 def test_ssh_base_mapper_order():
-    "SSH_MAPPER_BASE should be sorted based on the most common command used."""
+    "SSH_MAPPER_BASE should be sorted based on the most common command used." ""
     assert SSH_MAPPER_BASE[0][1]["cmd"] == "show version"
