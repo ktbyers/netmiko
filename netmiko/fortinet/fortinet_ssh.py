af21f74be4420dac7c5b6848f46d0b4f00c7141e
Trasmontinho <53441739+Trasmontinho@users.noreply.github.com>
2021-02-12 11:29:34 +0100

    Update fortinet_ssh.py

--- netmiko/fortinet/fortinet_ssh.py
+++ netmiko/fortinet/fortinet_ssh.py
@@ -106,7 +106,7 @@ class FortinetSSH(CiscoSSHConnection):
 
     def config_mode(self, config_command="", pattern="", re_flags=0):
         """
-        #Enter into configuration mode on remote device. 
+        #Enter into configuration mode on remote device.
         #Function particularly usefull when VDOMs are configured on it
         """
 
