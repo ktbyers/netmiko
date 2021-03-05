a84367f7ce8b50a5b81ecc86645eb346b053ca49
Will George <wrgeorge1983@gmail.com>
2021-03-05 08:43:14 -0500

    mypy updates for cloudgenix_ion.py and coriant_ssh.py
    CloudGenixIonSSH.establish_connection() may not be necessary and/or might need to be replaced?

--- netmiko/cloudgenix/cloudgenix_ion.py
+++ netmiko/cloudgenix/cloudgenix_ion.py
@@ -1,49 +1,58 @@
+import io
+from typing import Any, Union, Iterable
+
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
 class CloudGenixIonSSH(CiscoSSHConnection):
-    def establish_connection(self):
-        super().establish_connection(width=100, height=1000)
+    def establish_connection(self) -> None:
+        self.channel.establish_connection(width=100, height=1000)
 
-    def session_preparation(self, *args, **kwargs):
+    def session_preparation(self, *args: Any, **kwargs: Any) -> None:
         """Prepare the session after the connection has been established."""
         self.ansi_escape_codes = True
         self._test_channel_read()
         self.write_channel(self.RETURN)
         self.set_base_prompt(delay_factor=5)
 
-    def disable_paging(self, *args, **kwargs):
+    def disable_paging(self, *args: Any, **kwargs: Any) -> str:
         """Cloud Genix ION sets terminal height in establish_connection"""
         return ""
 
-    def find_prompt(self, delay_factor=1):
+    def find_prompt(self, delay_factor: float = 1.0) -> str:
         prompt = super().find_prompt(delay_factor=delay_factor)
         prompt = self.strip_backspaces(prompt).strip()
         return prompt
 
-    def strip_command(self, command_string, output):
+    def strip_command(self, command_string: Any, output: Any) -> str:
         output = super().strip_command(command_string, output)
         # command_string gets repainted potentially multiple times (grab everything after last one)
         output = output.split(command_string)[-1]
         return output
 
-    def check_config_mode(self):
+    def check_config_mode(self, *args: Any, **kwargs: Any) -> bool:
         """Devices do not have a config mode."""
         return False
 
-    def config_mode(self):
+    def config_mode(self, *args: Any, **kwargs: Any) -> str:
         """Devices do not have a config mode."""
         return ""
 
-    def exit_config_mode(self):
+    def exit_config_mode(self, *args: Any, **kwargs: Any) -> str:
         """Devices do not have a config mode."""
         return ""
 
-    def save_config(self, *args, **kwargs):
+    def save_config(self, *args: Any, **kwargs: Any) -> str:
         """No save method on ION SSH"""
         pass
 
-    def send_config_set(self, config_commands=None, exit_config_mode=False, **kwargs):
+    def send_config_set(
+        self,
+        config_commands: Union[str, Iterable[str], io.TextIOWrapper, None] = None,
+        exit_config_mode: bool = False,
+        *args: Any,
+        **kwargs: Any
+    ) -> str:
         return super().send_config_set(
             config_commands=config_commands, exit_config_mode=exit_config_mode, **kwargs
         )
--- netmiko/coriant/coriant_ssh.py
+++ netmiko/coriant/coriant_ssh.py
@@ -1,35 +1,40 @@
+from typing import Any
+
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
 class CoriantSSH(CiscoSSHConnection):
-    def session_preparation(self):
+    def session_preparation(self) -> None:
         self._test_channel_read()
         self.set_base_prompt()
 
-    def check_enable_mode(self, *args, **kwargs):
+    def check_enable_mode(self, *args: Any, **kwargs: Any) -> bool:
         raise AttributeError("Coriant devices do not support enable mode!")
 
-    def enable(self, *args, **kwargs):
+    def enable(self, *args: Any, **kwargs: Any) -> str:
         raise AttributeError("Coriant devices do not support enable mode!")
 
-    def exit_enable_mode(self, *args, **kwargs):
+    def exit_enable_mode(self, *args: Any, **kwargs: Any) -> str:
         raise AttributeError("Coriant devices do not support enable mode!")
 
-    def check_config_mode(self):
+    def check_config_mode(self, *args: Any, **kwargs: Any) -> bool:
         """Coriant devices do not have a config mode."""
         return False
 
-    def config_mode(self):
+    def config_mode(self, *args: Any, **kwargs: Any) -> str:
         """Coriant devices do not have a config mode."""
         return ""
 
-    def exit_config_mode(self):
+    def exit_config_mode(self, *args: Any, **kwargs: Any) -> str:
         """Coriant devices do not have a config mode."""
         return ""
 
     def set_base_prompt(
-        self, pri_prompt_terminator=":", alt_prompt_terminator=">", delay_factor=2
-    ):
+        self,
+        pri_prompt_terminator: str = ":",
+        alt_prompt_terminator: str = ">",
+        delay_factor: float = 2.0,
+    ) -> str:
         """Sets self.base_prompt: used as delimiter for stripping of trailing prompt in output."""
         super().set_base_prompt(
             pri_prompt_terminator=pri_prompt_terminator,
@@ -38,6 +43,6 @@ class CoriantSSH(CiscoSSHConnection):
         )
         return self.base_prompt
 
-    def save_config(self, *args, **kwargs):
+    def save_config(self, *args: Any, **kwargs: Any) -> str:
         """Not Implemented"""
         raise NotImplementedError
