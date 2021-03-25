887c5576d179f9adce7cdd57331765c047b6c9e0
Евгений <evgesha@pop-os.localdomain>
2021-03-25 13:15:30 +0300

    update dlink_ds-driver and fix commands.yml.example

--- netmiko/dlink/dlink_ds.py
+++ netmiko/dlink/dlink_ds.py
@@ -9,6 +9,7 @@ class DlinkDSBase(CiscoSSHConnection):
         """Prepare the session after the connection has been established."""
         self.ansi_escape_codes = True
         self._test_channel_read()
+        self.enable()  # fix, disable clipaging - only to admin
         self.set_base_prompt()
         self.disable_paging()
         # Clear the read buffer
@@ -18,13 +19,17 @@ class DlinkDSBase(CiscoSSHConnection):
     def disable_paging(self, command="disable clipaging", delay_factor=1):
         return super().disable_paging(command=command, delay_factor=delay_factor)
 
-    def enable(self, *args, **kwargs):
-        """No implemented enable mode on D-Link yet"""
-        return ""
+    def enable(self, cmd="enable admin", pattern="ord:", enable_pattern='Success', **kwargs):
+        """Enter enable mode."""
+        if not self.check_enable_mode():
+            return super().enable(cmd=cmd, pattern=pattern, enable_pattern=enable_pattern, **kwargs)
+        return ''
 
     def check_enable_mode(self, *args, **kwargs):
-        """No implemented enable mode on D-Link yet"""
-        return True
+        """Check whether device is in enable mode. Return a boolean."""
+        self.write_channel(self.RETURN)
+        output = self._read_channel_timing()
+        return output.endswith((":4#", ":5#", ":admin#"))
 
     def exit_enable_mode(self, *args, **kwargs):
         """No implemented enable mode on D-Link yet"""
@@ -51,7 +56,7 @@ class DlinkDSBase(CiscoSSHConnection):
     def cleanup(self):
         """Return paging before disconnect"""
         self.send_command_timing("enable clipaging")
-        return super().cleanup()
+        return super().cleanup(command="logout")
 
 
 class DlinkDSSSH(DlinkDSBase):
@@ -63,3 +68,7 @@ class DlinkDSTelnet(DlinkDSBase):
         default_enter = kwargs.get("default_enter")
         kwargs["default_enter"] = "\r\n" if default_enter is None else default_enter
         super().__init__(*args, **kwargs)
+
+    def telnet_login(self, *args, **kwargs):
+        self.remote_conn.set_option_negotiation_callback(lambda *_: None)  # fix telnet bag to DES-3526, DES-3026
+        super().telnet_login(*args, **kwargs)
