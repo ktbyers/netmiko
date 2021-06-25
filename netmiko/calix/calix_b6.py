1178d34affd2695469e235792b58faf32c3b52f8
Kirk Byers <ktbyers@twb-tech.com>
2021-06-25 09:56:32 -0700

    Type hints for c* drivers

--- netmiko/calix/calix_b6.py
+++ netmiko/calix/calix_b6.py
@@ -1,4 +1,5 @@
 """Calix B6 SSH Driver for Netmiko"""
+from typing import Any
 import time
 from os import path
 
@@ -10,20 +11,20 @@ from netmiko.cisco_base_connection import CiscoSSHConnection
 class SSHClient_noauth(SSHClient):
     """Set noauth when manually handling SSH authentication."""
 
-    def _auth(self, username, *args):
-        self._transport.auth_none(username)
+    def _auth(self, username: str, *args: Any) -> None:
+        self._transport.auth_none(username)  # type: ignore
         return
 
 
 class CalixB6Base(CiscoSSHConnection):
     """Common methods for Calix B6, both SSH and Telnet."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         default_enter = kwargs.get("default_enter")
         kwargs["default_enter"] = "\r\n" if default_enter is None else default_enter
         super().__init__(*args, **kwargs)
 
-    def session_preparation(self):
+    def session_preparation(self) -> Any:
         """Prepare the session after the connection has been established."""
         self.ansi_escape_codes = True
         self._test_channel_read()
@@ -34,7 +35,7 @@ class CalixB6Base(CiscoSSHConnection):
         time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
 
-    def special_login_handler(self, delay_factor=1):
+    def special_login_handler(self, delay_factor: float = 1.0) -> None:
         """
         Calix B6 presents with the following on login:
 
@@ -49,8 +50,10 @@ class CalixB6Base(CiscoSSHConnection):
             output = self.read_channel()
             if output:
                 if "login as:" in output:
+                    assert isinstance(self.username, str)
                     self.write_channel(self.username + self.RETURN)
                 elif "Password:" in output:
+                    assert isinstance(self.password, str)
                     self.write_channel(self.password + self.RETURN)
                     break
                 time.sleep(delay_factor * 0.5)
@@ -59,11 +62,16 @@ class CalixB6Base(CiscoSSHConnection):
                 time.sleep(delay_factor * 1)
             i += 1
 
-    def check_config_mode(self, check_string=")#", pattern=""):
+    def check_config_mode(self, check_string: str = ")#", pattern: str = "") -> bool:
         """Checks if the device is in configuration mode"""
         return super().check_config_mode(check_string=check_string)
 
-    def save_config(self, cmd="copy run start", confirm=False, confirm_response=""):
+    def save_config(
+        self,
+        cmd: str = "copy run start",
+        confirm: bool = False,
+        confirm_response: str = "",
+    ) -> str:
         return super().save_config(
             cmd=cmd, confirm=confirm, confirm_response=confirm_response
         )
@@ -76,12 +84,12 @@ class CalixB6SSH(CalixB6Base):
     the username/password.
     """
 
-    def _build_ssh_client(self):
+    def _build_ssh_client(self) -> SSHClient:
         """Prepare for Paramiko SSH connection."""
         # Create instance of SSHClient object
         # If not using SSH keys, we use noauth
         if not self.use_keys:
-            remote_conn_pre = SSHClient_noauth()
+            remote_conn_pre: SSHClient = SSHClient_noauth()
         else:
             remote_conn_pre = SSHClient()
 
