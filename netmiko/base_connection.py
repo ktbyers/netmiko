798d93b6efd65e9b7d65ff30fb3e3772fcea5370
Kirk Byers <ktbyers@twb-tech.com>
2021-07-06 11:49:22 -0700

    Shouldn't be possible to get an index error here--even with a null string

--- netmiko/base_connection.py
+++ netmiko/base_connection.py
@@ -1408,11 +1408,8 @@ Device settings: {self.device_type} {self.host}:{self.port}
         :param a_string: Returned string from device
         :type a_string: str
         """
-        try:
-            response_list = a_string.split(self.RESPONSE_RETURN)
-            last_line = response_list[-1]
-        except IndexError:
-            return a_string
+        response_list = a_string.split(self.RESPONSE_RETURN)
+        last_line = response_list[-1]
 
         if self.base_prompt in last_line:
             return self.RESPONSE_RETURN.join(response_list[:-1])
