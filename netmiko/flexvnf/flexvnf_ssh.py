4dc4595fb113af0e6b2583437691525a22e6f988
Vera <vvegorova@gmail.com>
2021-07-17 10:13:35 +0200

    Use assert_str function in flexvnf_ssh module

--- netmiko/flexvnf/flexvnf_ssh.py
+++ netmiko/flexvnf/flexvnf_ssh.py
@@ -1,10 +1,11 @@
-from typing import Optional, Union, Any, List, Dict
+from typing import Optional, Any
 import re
 import time
 import warnings
 
 from netmiko.no_enable import NoEnable
 from netmiko.base_connection import BaseConnection, DELAY_FACTOR_DEPR_SIMPLE_MSG
+from netmiko.utilities import assert_str
 
 
 class FlexvnfSSH(NoEnable, BaseConnection):
@@ -71,18 +72,14 @@ class FlexvnfSSH(NoEnable, BaseConnection):
         """Exit configuration mode."""
         output = ""
         if self.check_config_mode():
-            new_output = self.send_command_timing(
+            output += assert_str(self.send_command_timing(
                 exit_config, strip_prompt=False, strip_command=False
-            )
-            assert isinstance(new_output, str)
-            output += new_output
+            ))
             # if 'Exit with uncommitted changes?' in output:
             if "uncommitted changes" in output:
-                new_output = self.send_command_timing(
+                output += assert_str(self.send_command_timing(
                     "yes", strip_prompt=False, strip_command=False
-                )
-                assert isinstance(new_output, str)
-                output += new_output
+                ))
             if self.check_config_mode():
                 raise ValueError("Failed to exit configuration mode")
         return output
@@ -161,24 +158,20 @@ class FlexvnfSSH(NoEnable, BaseConnection):
         # and_quit will get out of config mode on commit
         if and_quit:
             prompt = self.base_prompt
-            new_output = self.send_command(
+            output += assert_str(self.send_command(
                 command_string,
                 expect_string=prompt,
                 strip_prompt=True,
                 strip_command=True,
                 read_timeout=read_timeout,
-            )
-            assert isinstance(new_output, str)
-            output += new_output
+            ))
         else:
-            new_output = self.send_command(
+            output += assert_str(self.send_command(
                 command_string,
                 strip_prompt=True,
                 strip_command=True,
                 read_timeout=read_timeout,
-            )
-            assert isinstance(new_output, str)
-            output += new_output
+            ))
 
         if commit_marker not in output:
             raise ValueError(f"Commit failed with the following errors:\n\n{output}")
