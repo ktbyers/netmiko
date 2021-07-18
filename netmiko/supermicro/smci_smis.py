b06bbb42f2fdb1637d316c6a5d79b154bf7f6883
Vera <vvegorova@gmail.com>
2021-07-18 21:16:38 +0200

    Added type hints in smci_smis.py module

--- netmiko/supermicro/smci_smis.py
+++ netmiko/supermicro/smci_smis.py
@@ -4,7 +4,7 @@ import time
 
 
 class SmciSwitchSmisBase(NoEnable, CiscoBaseConnection):
-    def session_preparation(self):
+    def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
         self._test_channel_read(pattern=r"[>#]")
         self.set_base_prompt()
@@ -16,13 +16,16 @@ class SmciSwitchSmisBase(NoEnable, CiscoBaseConnection):
         time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
 
-    def check_enable_mode(self, check_string="#"):
+    def check_enable_mode(self, check_string: str = "#") -> bool:
         """Check if in enable mode. Return boolean."""
         return super().check_enable_mode(check_string=check_string)
 
     def save_config(
-        self, cmd="write startup-config", confirm=False, confirm_response=""
-    ):
+        self,
+        cmd: str = "write startup-config",
+        confirm: bool = False,
+        confirm_response: str = "",
+    ) -> str:
         """Save config"""
         return super().save_config(
             cmd=cmd, confirm=confirm, confirm_response=confirm_response
