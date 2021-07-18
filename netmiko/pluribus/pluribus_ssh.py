a5bcdfdadfc16e306415f08ead06120543e11c85
Vera <vvegorova@gmail.com>
2021-07-18 12:28:24 +0200

    Added type hints in oneaccess, paloalto, pluribus and quanta packages.

--- netmiko/pluribus/pluribus_ssh.py
+++ netmiko/pluribus/pluribus_ssh.py
@@ -1,4 +1,6 @@
 import time
+from typing import Any, Optional
+
 from netmiko.no_config import NoConfig
 from netmiko.base_connection import BaseConnection
 
@@ -6,15 +8,21 @@ from netmiko.base_connection import BaseConnection
 class PluribusSSH(NoConfig, BaseConnection):
     """Common methods for Pluribus."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self._config_mode = False
 
-    def disable_paging(self, command="pager off"):
+    def disable_paging(
+        self,
+        command: str = "terminal length 0",
+        delay_factor: Optional[float] = None,
+        cmd_verify: bool = True,
+        pattern: Optional[str] = None,
+    ) -> str:
         """Make sure paging is disabled."""
-        return super().disable_paging(command=command)
+        return super().disable_paging(command=command, delay_factor=delay_factor, cmd_verify=cmd_verify, pattern=pattern)
 
-    def session_preparation(self):
+    def session_preparation(self) -> None:
         """Prepare the netmiko session."""
         self._test_channel_read()
         self.set_base_prompt()
--- netmiko/quanta/quanta_mesh_ssh.py
+++ netmiko/quanta/quanta_mesh_ssh.py
@@ -1,10 +1,23 @@
+from typing import Optional
+
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
 class QuantaMeshSSH(CiscoSSHConnection):
-    def disable_paging(self, command="no pager"):
+    def disable_paging(
+        self,
+        command: str = "no pager",
+        delay_factor: Optional[float] = None,
+        cmd_verify: bool = True,
+        pattern: Optional[str] = None,
+    ) -> str:
         """Disable paging"""
-        return super().disable_paging(command=command)
+        return super().disable_paging(
+            command=command,
+            delay_factor=delay_factor,
+            cmd_verify=cmd_verify,
+            pattern=pattern,
+        )
 
     def config_mode(
         self, config_command: str = "configure", pattern: str = "", re_flags: int = 0
@@ -15,10 +28,10 @@ class QuantaMeshSSH(CiscoSSHConnection):
 
     def save_config(
         self,
-        cmd="copy running-config startup-config",
-        confirm=False,
-        confirm_response="",
-    ):
+        cmd: str = "copy running-config startup-config",
+        confirm: bool = False,
+        confirm_response: str = "",
+    ) -> str:
         """Saves Config"""
         return super().save_config(
             cmd=cmd, confirm=confirm, confirm_response=confirm_response
