a47b862920a08557dbf97a72ba658f1184ba9377
Kirk Byers <ktbyers@twb-tech.com>
2021-07-20 10:00:15 -0700

    Allied Telesis driver (#2397)

--- netmiko/allied_telesis/allied_telesis_awplus.py
+++ netmiko/allied_telesis/allied_telesis_awplus.py
@@ -20,31 +20,23 @@ class AlliedTelesisAwplusBase(CiscoBaseConnection):
 
     def _enter_shell(self) -> str:
         """Enter the Bourne Shell."""
-        output = self.send_command("start shell sh", expect_string=r"[\$#]")
-        assert isinstance(output, str)
-        return output
+        return self._send_command_str("start shell sh", expect_string=r"[\$#]")
 
     def _return_cli(self) -> str:
         """Return to the Awplus CLI."""
-        output = self.send_command("exit", expect_string=r"[#>]")
-        assert isinstance(output, str)
-        return output
+        return self._send_command_str("exit", expect_string=r"[#>]")
 
     def exit_config_mode(self, exit_config: str = "exit", pattern: str = "") -> str:
         """Exit configuration mode."""
         output = ""
         if self.check_config_mode():
-            new_output = self.send_command_timing(
+            output += self._send_command_timing_str(
                 exit_config, strip_prompt=False, strip_command=False
             )
-            assert isinstance(new_output, str)
-            output += new_output
             if "Exit with uncommitted changes?" in output:
-                new_output = self.send_command_timing(
+                output += self._send_command_timing_str(
                     "yes", strip_prompt=False, strip_command=False
                 )
-                assert isinstance(new_output, str)
-                output += new_output
             if self.check_config_mode():
                 raise ValueError("Failed to exit configuration mode")
         return output
