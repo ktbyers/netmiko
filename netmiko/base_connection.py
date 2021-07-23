bae47709337ea76bc0028883633bf8517bd82c26
Kirk Byers <ktbyers@twb-tech.com>
2021-07-23 11:10:19 -0700

    Add ANSI bold and other prompt styles (#2420)
    * Add ANSI bold and other prompt styles
    
    * Fixing reset_graphics_mode with double 00

--- netmiko/base_connection.py
+++ netmiko/base_connection.py
@@ -2146,7 +2146,8 @@ You can also look at the Netmiko session_log or debug log for more information.
         code_reset_graphics_mode = chr(27) + r"\[00m"
         code_erase_display = chr(27) + r"\[2J"
         code_erase_display_0 = chr(27) + r"\[J"
-        code_graphics_mode = chr(27) + r"\[\d\d;\d\dm"
+        code_graphics_mode = chr(27) + r"\[\dm"
+        code_graphics_mode1 = chr(27) + r"\[\d\d;\d\dm"
         code_graphics_mode2 = chr(27) + r"\[\d\d;\d\d;\d\dm"
         code_graphics_mode3 = chr(27) + r"\[(3|4)\dm"
         code_graphics_mode4 = chr(27) + r"\[(9|10)[0-7]m"
@@ -2174,6 +2175,7 @@ You can also look at the Netmiko session_log or debug log for more information.
             code_reset_graphics_mode,
             code_erase_display,
             code_graphics_mode,
+            code_graphics_mode1,
             code_graphics_mode2,
             code_graphics_mode3,
             code_graphics_mode4,
