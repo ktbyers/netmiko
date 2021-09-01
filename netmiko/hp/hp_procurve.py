310fe8984530021f5c1eb4abd6f3b0348f5087d6
Kirk Byers <ktbyers@twb-tech.com>
2021-08-31 18:00:22 -0700

    HP Procurve performance improvements

--- netmiko/hp/hp_procurve.py
+++ netmiko/hp/hp_procurve.py
@@ -18,14 +18,16 @@ class HPProcurveBase(CiscoSSHConnection):
         # HP output contains VT100 escape codes
         self.ansi_escape_codes = True
 
-        self._test_channel_read(pattern=r"[>#]")
+        # Procurve over SSH uses 'Press any key to continue'
+        data = self._test_channel_read(pattern=r"(any key to continue|[>#])")
+        if "any key to continue" in data:
+            self.write_channel(self.RETURN)
+            self._test_channel_read(pattern=r"[>#]")
+
         self.set_base_prompt()
-        command = self.RETURN + "no page"
         self.set_terminal_width(command="terminal width 511", pattern="terminal")
+        command = self.RETURN + "no page"
         self.disable_paging(command=command)
-        # Clear the read buffer
-        time.sleep(0.3 * self.global_delay_factor)
-        self.clear_buffer()
 
     def enable(
         self,
@@ -36,31 +38,40 @@ class HPProcurveBase(CiscoSSHConnection):
         default_username: str = "manager",
     ) -> str:
         """Enter enable mode"""
-        delay_factor = self.select_delay_factor(delay_factor=0)
+
         if self.check_enable_mode():
             return ""
 
         output = ""
-        i = 1
-        max_attempts = 5
-        while i <= max_attempts:
-            self.write_channel(cmd + self.RETURN)
-            time.sleep(0.3 * delay_factor)
-            new_output = self.read_channel()
-            username_pattern = r"(username|login|user name)"
-            if re.search(username_pattern, new_output, flags=re_flags):
-                output += new_output
-                new_output = self._send_command_timing_str(default_username)
-            if re.search(pattern, new_output, flags=re_flags):
-                output += new_output
-                self.write_channel(self.normalize_cmd(self.secret))
-                new_output = self.read_channel_timing()
-                if self.check_enable_mode():
-                    output += new_output
-                    return output
-            output += new_output
-            i += 1
+        username_pattern = r"(username|login|user name)"
+        pwd_pattern = pattern
+        prompt_pattern = r"[>#]"
+        full_pattern = rf"(username|login|user name|{pwd_pattern}|{prompt_pattern})"
 
+        # Send the enable command
+        self.write_channel(cmd + self.RETURN)
+        new_output = self.read_until_pattern(
+            full_pattern, read_timeout=15, re_flags=re_flags
+        )
+
+        # Send the username
+        if re.search(username_pattern, new_output, flags=re_flags):
+            output += new_output
+            self.write_channel(default_username + self.RETURN)
+            full_pattern = rf"{pwd_pattern}|{prompt_pattern})"
+            new_output = self.read_until_pattern(
+                full_pattern, read_timeout=15, re_flags=re_flags
+            )
+
+        # Send the password
+        if re.search(pwd_pattern, new_output, flags=re_flags):
+            output += new_output
+            self.write_channel(self.secret + self.RETURN)
+            new_output = self.read_until_pattern(
+                prompt_pattern, read_timeout=15, re_flags=re_flags
+            )
+
+        output += new_output
         log.debug(f"{output}")
         self.clear_buffer()
         msg = (
@@ -86,8 +97,8 @@ class HPProcurveBase(CiscoSSHConnection):
         self.write_channel(command + self.RETURN)
         count = 0
         output = ""
-        while count <= 5:
-            time.sleep(0.5)
+        while count <= 10:
+            time.sleep(0.3)
 
             # The connection might be dead here.
             try:
@@ -98,17 +109,14 @@ class HPProcurveBase(CiscoSSHConnection):
 
             if "Do you want to log out" in new_output:
                 self.write_channel("y" + self.RETURN)
-                time.sleep(0.5)
-                output += self.read_channel()
-
-            # Don't automatically save the config (user's responsibility)
-            if "Do you want to save the current" in output:
+            elif "Do you want to save the current" in new_output:
+                # Don't automatically save the config (user's responsibility)
                 self.write_channel("n" + self.RETURN)
-
-            try:
-                self.write_channel(self.RETURN)
-            except socket.error:
-                break
+            else:
+                try:
+                    self.write_channel(self.RETURN)
+                except socket.error:
+                    break
             count += 1
 
         # Set outside of loop
@@ -127,28 +135,6 @@ class HPProcurveBase(CiscoSSHConnection):
 
 
 class HPProcurveSSH(HPProcurveBase):
-    def session_preparation(self) -> None:
-        """
-        Prepare the session after the connection has been established.
-        """
-        # Procurve over SHH uses 'Press any key to continue'
-        delay_factor = self.select_delay_factor(delay_factor=0)
-        output = ""
-        count = 1
-        while count <= 30:
-            output += self.read_channel()
-            if "any key to continue" in output:
-                self.write_channel(self.RETURN)
-                break
-            else:
-                time.sleep(0.33 * delay_factor)
-            count += 1
-
-        # Try one last time to past "Press any key to continue
-        self.write_channel(self.RETURN)
-
-        super().session_preparation()
-
     def _build_ssh_client(self) -> SSHClient:
         """Allow passwordless authentication for HP devices being provisioned."""
 
