d712a0623c52869afc80cb21ce43c9100cfadc4a
Kirk Byers <ktbyers@twb-tech.com>
2021-09-09 17:56:25 -0700

    Huawei performance fixes (#2471)

--- netmiko/huawei/huawei_smartax.py
+++ netmiko/huawei/huawei_smartax.py
@@ -12,13 +12,10 @@ class HuaweiSmartAXSSH(CiscoBaseConnection):
     def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
         self.ansi_escape_codes = True
-        self._test_channel_read()
+        self._test_channel_read(pattern=r"[>#]")
         self.set_base_prompt()
         self._disable_smart_interaction()
         self.disable_paging()
-        # Clear the read buffer
-        time.sleep(0.3 * self.global_delay_factor)
-        self.clear_buffer()
 
     def strip_ansi_escape_codes(self, string_buffer: str) -> str:
         """
