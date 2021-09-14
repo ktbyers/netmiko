72ef6e2fe31d5c287505acda2241de056ebad691
Vera Ilina <vvegorova@gmail.com>
2021-09-14 22:06:36 +0200

    Script to test performance and generate graphs (#2470)

--- tests/performance/gen_graph.py
+++ tests/performance/gen_graph.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
 import pygal
 import csv
+import yaml
+import jinja2
 from pprint import pprint
+from typing import Dict
+
+from pathlib import Path
 
 
 def convert_time(time_str):
@@ -13,7 +19,7 @@ def convert_time(time_str):
 
 
 def read_csv(device):
-    csv_file = "netmiko_performance_releases.csv"
+    csv_file = "netmiko_performance.csv"
     entries = []
     with open(csv_file) as f:
         read_csv = csv.DictReader(f)
@@ -25,40 +31,30 @@ def read_csv(device):
     return entries
 
 
-if __name__ == "__main__":
+def filter_versions(entries):
+    patches = dict()
+    for entry in entries:
+        version = entry["netmiko_version"]
+        dot_pos = version.rfind(".")
+        minor_version = version[:dot_pos]
+        dot_pos += 1
+        patch_version = version[dot_pos:]
+        current_patch = patches.get(minor_version, "0")
+        patches[minor_version] = max(patch_version, current_patch)
 
-    cisco1 = {
-        "device": "cisco1",
-        "title": "Netmiko: Cisco IOS Performance (Cisco 881)",
-        "outfile": "netmiko_cisco_ios.svg",
-    }
-    cisco3 = {
-        "device": "cisco3",
-        "title": "Netmiko: Cisco IOS-XE Performance (Cisco C1111-4P)",
-        "outfile": "netmiko_cisco_xe.svg",
-    }
-    nxos1 = {
-        "device": "nxos1",
-        "title": "Netmiko: Cisco NX-OS Performance (nx9k virtual)",
-        "outfile": "netmiko_cisco_nxos.svg",
-    }
-    xr_azure = {
-        "device": "cisco_xr_azure",
-        "title": "Netmiko: Cisco IOS-XR Performance (cisco IOS-XRv 9000)",
-        "outfile": "netmiko_cisco_xr.svg",
-    }
-    arista1 = {
-        "device": "arista1",
-        "title": "Netmiko: Arista EOS Performance (vEOS)",
-        "outfile": "netmiko_arista_eos.svg",
-    }
+    last_versions = [f"{minor}.{patch}" for minor, patch in patches.items()]
+    entries = filter(lambda x: x["netmiko_version"] in last_versions, entries)
+    return sorted(entries, key=lambda x: x["netmiko_version"])
 
-    test_device = cisco1
 
-    device = test_device["device"]
-    title = test_device["title"]
-    outfile = test_device["outfile"]
+def generate_graph(device_name: str, params: Dict) -> None:
+    device = device_name
+    title = params["graph"]["title"]
+    device_dict = params["device"]
+    device_type = device_dict["device_type"]
+    outfile = f"netmiko_{device_type}.svg"
     entries = read_csv(device)
+    entries = filter_versions(entries)
 
     # Create relevant lists
     netmiko_versions = [v["netmiko_version"] for v in entries]
@@ -78,4 +74,34 @@ if __name__ == "__main__":
     line_chart.add("Show Command", send_command)
     line_chart.add("Simple Config", send_config)
     line_chart.add("Large ACL", send_config_acl)
-    line_chart.render_to_file(outfile)
+    dir_path = Path.cwd() / "graphs"
+    if not dir_path.exists():
+        dir_path.mkdir()
+    line_chart.render_to_file(str(dir_path / outfile))
+
+
+perf_report_template = """
+# Netmiko performance
+{%- for graph in graphs %}
+![]({{graph}})\n
+{%- endfor %}
+"""
+
+
+def generate_report():
+    template = jinja2.Template(perf_report_template)
+    graphs_path = Path.cwd() / "graphs"
+    graph_files = ["graphs/" + item.name for item in graphs_path.iterdir()]
+    report_file = Path.cwd() / "performance_report.md"
+    with report_file.open("w") as out_file:
+        out_file.writelines(template.render({"graphs": graph_files}))
+
+
+if __name__ == "__main__":
+    f_name = "test_devices.yml"
+    with open(f_name) as f:
+        devices = yaml.load(f)
+        for device_name, device in devices.items():
+            if "graph" in device:
+                generate_graph(device_name, device)
+    generate_report()
