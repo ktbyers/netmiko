159d78a399518dccb27918e44488ff27e3fe6dc4
Kirk Byers <ktbyers@twb-tech.com>
2022-04-12 11:18:22 -0700

    Fix issue you could get into where prompt might not be available.

--- netmiko/huawei/huawei.py
+++ netmiko/huawei/huawei.py
@@ -28,6 +28,9 @@ class HuaweiBase(NoEnable, CiscoBaseConnection):
     def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
         self.ansi_escape_codes = True
+        # The prompt might have already been read (in special_login_handler)
+        self.write_channel(self.RETURN)
+        self._test_channel_read(pattern=r"[>\]]")
         self.set_base_prompt()
         self.disable_paging(command="screen-length 0 temporary")
 
