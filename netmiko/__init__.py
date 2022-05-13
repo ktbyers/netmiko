b3c955ba91883d17205b9e5ac936d905165e4e5c
Kirk Byers <ktbyers@twb-tech.com>
2022-05-13 16:59:27 -0700

    Version check

--- netmiko/__init__.py
+++ netmiko/__init__.py
@@ -1,8 +1,33 @@
-import logging
+import sys
 
-# Logging configuration
-log = logging.getLogger(__name__)  # noqa
-log.addHandler(logging.NullHandler())  # noqa
+__version__ = "4.1.0"
+MIN_PYTHON_VER = "3.7"
+
+
+# Make sure user is using a valid Python version (for Netmiko)
+def check_python_version():
+    python_snake = "\U0001F40D"
+
+    # Use old-school .format() method in case someone tries to use Netmiko with very old Python
+    msg = """
+
+Netmiko Version {net_ver} requires Python Version {py_ver} or higher.
+
+""".format(
+        net_ver=__version__, py_ver=MIN_PYTHON_VER
+    )
+    if sys.version_info.major != 3:
+        raise ValueError(msg)
+    elif sys.version_info.minor < 7:
+        # Why not :-)
+        msg = msg.rstrip() + " {snake}\n\n".format(snake=python_snake)
+        raise ValueError(msg)
+
+
+check_python_version()
+
+
+import logging
 
 from netmiko.ssh_dispatcher import ConnectHandler
 from netmiko.ssh_dispatcher import ConnLogOnly
@@ -28,10 +53,13 @@ from netmiko.ssh_autodetect import SSHDetect
 from netmiko.base_connection import BaseConnection
 from netmiko.scp_functions import file_transfer, progress_bar
 
+# Logging configuration
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
 # Alternate naming
 Netmiko = ConnectHandler
 
-__version__ = "4.1.0"
 __all__ = (
     "ConnectHandler",
     "ConnLogOnly",
