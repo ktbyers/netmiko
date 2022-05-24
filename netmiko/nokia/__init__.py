89536f880743c4c4c0ffca5a89be58c5aad7ed57
Sebastien Guay <sebdug@hotmail.com>
2022-05-24 13:48:58 -0400

    Modifications after running the 'black' tool for code formatting.

--- netmiko/nokia/__init__.py
+++ netmiko/nokia/__init__.py
@@ -4,8 +4,6 @@ from netmiko.nokia.nokia_sros import (
     NokiaSrosFileTransfer,
 )
 
-from netmiko.nokia.nokia_srl import (
-    NokiaSrlSSH
-)
+from netmiko.nokia.nokia_srl import NokiaSrlSSH
 
 __all__ = ["NokiaSrosSSH", "NokiaSrosFileTransfer", "NokiaSrosTelnet", "NokiaSrlSSH"]
