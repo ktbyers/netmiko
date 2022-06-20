877c2fa03b715ec4339c13ef5d5d83173367ec2c
Kirk Byers <ktbyers@twb-tech.com>
2022-06-20 11:33:26 -0700

    Backing out pull-request #2788 due to breakage

--- netmiko/snmp_autodetect.py
+++ netmiko/snmp_autodetect.py
@@ -20,7 +20,8 @@ SNMPDetect class defaults to SNMPv3
 Note, pysnmp is a required dependency for SNMPDetect and is intentionally not included in
 netmiko requirements. So installation of pysnmp might be required.
 """
-from typing import Optional, Dict, Pattern
+from typing import Optional, Dict
+from typing.re import Pattern
 import re
 
 try:
@@ -333,7 +334,7 @@ class SNMPDetect(object):
         for entry in snmp_mapper_list:
             for device_type, v in entry.items():
                 oid: str = v["oid"]  # type: ignore
-                regex: Pattern[str] = v["expr"]  # type: ignore
+                regex: Pattern = v["expr"]
 
                 # Used cache data if we already queryied this OID
                 if self._response_cache.get(oid):
