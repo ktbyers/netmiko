9f99992343f258f532c51269e359e9b8ecf68970
Kirk Byers <ktbyers@twb-tech.com>
2022-07-25 20:20:41 -0700

    Working on type hints and audiocode driver

--- netmiko/audiocode/audiocode_ssh.py
+++ netmiko/audiocode/audiocode_ssh.py
@@ -1,7 +1,8 @@
 from typing import Any, Optional, Sequence, TextIO, Union, List
-from netmiko.base_connection import BaseConnection
 import time
 import re
+from netmiko.base_connection import BaseConnection
+from netmiko.no_enable import NoEnable
 
 
 class AudiocodeBaseSSH(BaseConnection):
@@ -236,49 +237,53 @@ class Audiocode66SSH(AudiocodeBaseSSH):
 
     def disable_paging(
         self,
-        disable_window_config=[
-            "config system",
-            "cli-terminal",
-            "set window-height 0",
-            "exit",
-        ],
-        delay_factor=0.5,
-    ):
-        """This is designed to disable window paging which prevents paged command
-        output from breaking the script.
+        command: str = "",
+        delay_factor: Optional[float] = 0.5,
+        cmd_verify: bool = True,
+        pattern: Optional[str] = None,
+    ) -> str:
 
-        :param disable_window_config: Command, or list of commands, to execute.
-        :type disable_window_config: str
+        if command:
+            return super().disable_paging(
+                command=command,
+                delay_factor=delay_factor,
+                cmd_verify=cmd_verify,
+                pattern=pattern,
+            )
+        else:
+            command_list: List[str] = [
+                "cli-terminal",
+                "set window-height 0",
+            ]
 
-        :param delay_factor: See __init__: global_delay_factor
-        :type delay_factor: int
-
-        """
-        return super().disable_paging(
-            disable_window_config=disable_window_config, delay_factor=delay_factor
-        )
+            self.enable()
+            assert isinstance(delay_factor, float)
+            delay_factor = self.select_delay_factor(delay_factor)
+            time.sleep(delay_factor * 0.1)
+            self.clear_buffer()
+            return self.send_config_set(
+                config_commands=command_list,
+                config_mode_command="config system",
+            )
 
     def _enable_paging(
         self,
-        enable_window_config=[
-            "config system",
+        delay_factor: Optional[float] = 0.5,
+    ) -> str:
+
+        command_list: List[str] = [
             "cli-terminal",
             "set window-height 100",
-            "exit",
-        ],
-        delay_factor=0.5,
-    ):
-        """This is designed to reenable window paging
+        ]
+        self.enable()
+        assert isinstance(delay_factor, float)
+        delay_factor = self.select_delay_factor(delay_factor)
+        time.sleep(delay_factor * 0.1)
+        self.clear_buffer()
 
-        :param enable_window_config: Command, or list of commands, to execute.
-        :type enable_window_config: str
-
-        :param delay_factor: See __init__: global_delay_factor
-        :type delay_factor: int
-
-        """
-        return super()._enable_paging(
-            enable_window_config=enable_window_config, delay_factor=delay_factor
+        return self.send_config_set(
+            config_commands=command_list,
+            config_mode_command="config system",
         )
 
 
@@ -288,10 +293,10 @@ class Audiocode66Telnet(Audiocode66SSH):
     pass
 
 
-class AudiocodeShellSSH(AudiocodeBaseSSH):
+class AudiocodeShellSSH(NoEnable, AudiocodeBaseSSH):
     """Audiocode this applies to 6.6 Audiocode Firmware versions that only use the Shell."""
 
-    def session_preparation(self):
+    def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
         self.write_channel(self.RETURN)
         self.write_channel(self.RETURN)
@@ -303,29 +308,11 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 
     def set_base_prompt(
         self,
-        pri_prompt_terminator=r"/>",
-        alt_prompt_terminator="",
-        delay_factor=1.0,
-        pattern=r"/>",
-    ):
-        """Sets self.base_prompt
-
-        Used as delimiter for stripping of trailing prompt in output.
-
-        Should be set to something that is general and applies in multiple contexts. For Cisco
-        devices this will be set to router hostname (i.e. prompt without > or #).
-
-        This will be set on entering user exec or privileged exec on Cisco, but not when
-        entering/exiting config mode.
-
-        :param pri_prompt_terminator: Primary trailing delimiter for identifying a device prompt
-
-        :param alt_prompt_terminator: Alternate trailing delimiter for identifying a device prompt
-
-        :param delay_factor: See __init__: global_delay_factor
-
-        :param pattern: Regular expression pattern to search for in find_prompt() call
-        """
+        pri_prompt_terminator: str = r"/>",
+        alt_prompt_terminator: str = "",
+        delay_factor: float = 1.0,
+        pattern: Optional[str] = r"/>",
+    ) -> str:
         prompt = self.find_prompt(delay_factor=delay_factor, pattern=pattern)
         pattern = pri_prompt_terminator
         if not re.search(pattern, prompt):
@@ -338,45 +325,21 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
     def find_prompt(
         self,
         delay_factor: float = 1.0,
-        pattern: str = r"/>",
+        pattern: Optional[str] = r"/>",
     ) -> str:
-        """Finds the current network device prompt, last line only.
-
-        :param delay_factor: See __init__: global_delay_factor
-        :type delay_factor: int
-
-        :param pattern: Regular expression pattern to determine whether prompt is valid
-
-        :param confirm_pattern: Regular expression pattern to confirm prompt was found by auto discovery.
-        """
         return super().find_prompt(
             delay_factor=delay_factor,
             pattern=pattern,
         )
 
-    def enable(self, cmd="", pattern="", re_flags=re.IGNORECASE):
-        """Not supported"""
-        pass
-
-    def check_enable_mode(self, check_string=r"/>"):
-        """Check if in enable mode. Return boolean.
-
-        :param check_string: Identification of privilege mode from device
-        :type check_string: str
-        """
-        return super().check_enable_mode(check_string=check_string)
-
-    def exit_enable_mode(self, exit_command=""):
-        """Not supported"""
-        pass
-
     def send_config_set(
         self,
-        config_commands=None,
+        config_commands: Union[str, Sequence[str], TextIO, None] = None,
+        *,
         exit_config_mode: bool = True,
-        read_timeout: float = None,
-        delay_factor: float = 1.0,
-        max_loops: int = 150,
+        read_timeout: Optional[float] = None,
+        delay_factor: Optional[float] = 1.0,
+        max_loops: Optional[int] = 150,
         strip_prompt: bool = False,
         strip_command: bool = False,
         config_mode_command: Optional[str] = None,
@@ -390,7 +353,7 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
         if config_mode_command is None:
             msg = """
 send_config_set() for the audiocode driver requires that you specify the
-config_mode_command. For example, config_mode_command="configure system" 
+config_mode_command. For example, config_mode_command="configure system"
 (or "configure voip" or "configure network" etc.)
             """
             raise ValueError(msg)
@@ -413,17 +376,6 @@ config_mode_command. For example, config_mode_command="configure system"
     def config_mode(
         self, config_command: str = "", pattern: str = r"/.*>", re_flags: int = 0
     ) -> str:
-        """Enter into config_mode.
-
-        :param config_command: Configuration command to send to the device
-        :type config_command: str
-
-        :param pattern: Pattern to terminate reading of channel
-        :type pattern: str
-
-        :param re_flags: Regular expression flags
-        :type re_flags: RegexFlag
-        """
         return super().config_mode(
             config_command=config_command, pattern=pattern, re_flags=re_flags
         )
@@ -434,124 +386,76 @@ config_mode_command. For example, config_mode_command="configure system"
         pattern: str = r"/.*>",
         force_regex: bool = True,
     ) -> bool:
-        """Checks if the device is in configuration mode or not.
-
-        :param check_string: Identification of configuration mode from the device
-        :type check_string: str
-
-        :param pattern: Pattern to terminate reading of channel
-        :type pattern: str
-        """
         return super().check_config_mode(
             check_string=check_string, pattern=pattern, force_regex=force_regex
         )
 
     def exit_config_mode(self, exit_config: str = "..", pattern: str = r"/>") -> str:
-        """Exit from configuration mode.
-
-        :param exit_config: Command to exit configuration mode
-        :type exit_config: str
-
-        :param pattern: Pattern to terminate reading of channel
-        :type pattern: str
-        """
         return super().exit_config_mode(exit_config=exit_config, pattern=pattern)
 
-    def disable_paging(self):
+    def disable_paging(
+        self,
+        command: str = "",
+        delay_factor: Optional[float] = 0.5,
+        cmd_verify: bool = True,
+        pattern: Optional[str] = None,
+    ) -> str:
         """Not supported"""
-        pass
+        return ""
 
-    def save_config(self, cmd="SaveConfiguration", confirm=False, confirm_response=""):
-        """Saves the running configuration.
-
-        :param cmd: Command to save configuration
-        :type cmd: str
-
-        :param confirm: Command if confirmation prompt is required
-        :type confirm: bool
-
-        :param confirm_response: Command if confirm response required to further script
-        :type confirm response: str
-
-        """
+    def save_config(
+        self,
+        cmd: str = "SaveConfiguration",
+        confirm: bool = False,
+        confirm_response: str = "",
+    ) -> str:
         return super().save_config(
             cmd=cmd, confirm=confirm, confirm_response=confirm_response
         )
 
-    def _reload_device(
+    # FIX
+    #    def _reload_device(
+    #        self,
+    #        reload_device=True,
+    #        reload_save=True,
+    #        cmd_save="SaveAndReset",
+    #        cmd_no_save="ReSetDevice",
+    #        reload_message="Resetting the board",
+    #    ):
+    #        """Reloads the device.
+    #
+    #        :param reload_message: This is the pattern by which the reload is detected.
+    #        :type reload_message: str
+    #
+    #        """
+    #        return super()._reload_device(
+    #            reload_device=reload_device,
+    #            reload_save=reload_save,
+    #            cmd_save=cmd_save,
+    #            cmd_no_save=cmd_no_save,
+    #            reload_message=reload_message,
+    #        )
+
+    def _enable_paging(
         self,
-        reload_device=True,
-        reload_save=True,
-        cmd_save="SaveAndReset",
-        cmd_no_save="ReSetDevice",
-        reload_message="Resetting the board",
-    ):
-        """Reloads the device.
-
-        :param reload_device: Boolean to determine if reload should occur.
-        :type reload_device: bool
-
-        :param reload_device: Boolean to determine if reload with saving first should occur.
-        :type reload_device: bool
-
-        :param cmd_save: Command to reload device with save.  Options are "reload now" and "reload if-needed".
-        :type cmd_save: str
-
-        :param cmd_no_save: Command to reload device.  Options are "reload without-saving", "reload without-saving in [minutes]".
-        :type cmd_no_save: str
-
-        :param reload_message: This is the pattern by which the reload is detected.
-        :type reload_message: str
-
-        """
-        return super()._reload_device(
-            reload_device=reload_device,
-            reload_save=reload_save,
-            cmd_save=cmd_save,
-            cmd_no_save=cmd_no_save,
-            reload_message=reload_message,
-        )
-
-    def _device_terminal_exit(self, command="exit"):
-        """This is for accessing devices via terminal. It first reenables window paging for
-        future use and exits the device before you send the disconnect method"""
-        return super()._device_terminal_exit(command=command)
-
-    def _enable_paging(self):
+        delay_factor: Optional[float] = 0.5,
+    ) -> str:
         """Not supported"""
         pass
 
     def strip_command(self, command_string: str, output: str) -> str:
-        """
-        Strip command_string from output string
-
-        Cisco IOS adds backspaces into output for long commands (i.e. for commands that line wrap)
-
-        :param command_string: The command string sent to the device
-        :type command_string: str
-
-        :param output: The returned output as a result of the command string sent to the device
-        :type output: str
-        """
         # Support for Audiocode_Shell.
         pattern = r"^SIP.*[\s\S]?PING.*>?.*[\s\S]?.*>?$"
         output = re.sub(pattern, "", output, flags=re.M)
 
         cmd = command_string.strip()
-        pattern = rf"{cmd}"
+        pattern = re.escape(cmd)
         output = re.sub(pattern, "", output, flags=re.M)
-
         return super().strip_command(command_string=command_string, output=output)
 
     def strip_prompt(self, a_string: str) -> str:
-        """Strip the trailing router prompt from the output.
-
-        :param a_string: Returned string from device
-        :type a_string: str
-        """
         pattern = r"^/>?"
         a_string = re.sub(pattern, "", a_string, flags=re.M)
-
         return super().strip_prompt(
             a_string=a_string,
         )
