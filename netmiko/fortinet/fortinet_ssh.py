d688f2c2e17df535e5b8da65dc6881f0c59f51cb
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-08-11 18:26:41 +0200

    Add support to FortiAnalyzer
    Add '>' character in pattern and update arguments for `set_base_prompt` method

--- netmiko/fortinet/fortinet_ssh.py
+++ netmiko/fortinet/fortinet_ssh.py
@@ -20,14 +20,14 @@ class FortinetSSH(NoConfig, CiscoSSHConnection):
     def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
 
-        data = self._test_channel_read(pattern="to accept|[#$]")
+        data = self._test_channel_read(pattern="to accept|[>#$]")
         # If "set post-login-banner enable" is set it will require you to press 'a'
         # to accept the banner before you login. This will accept if it occurs
         if "to accept" in data:
             self.write_channel("a\r")
-            self._test_channel_read(pattern=r"[#$]")
+            self._test_channel_read(pattern=r"[>#$]")
 
-        self.set_base_prompt(alt_prompt_terminator="$", alt_prompt_terminator2=">")
+        self.set_base_prompt()
         self.disable_paging()
 
     def disable_paging(
