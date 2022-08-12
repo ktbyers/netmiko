de659d755e22c173203dff0c7c63c67c62e0a035
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-08-12 10:52:55 +0200

    Add FortiAnalyzer Support
    Add '>' character to allow connection to FortiAnalyzer

--- netmiko/fortinet/fortinet_ssh.py
+++ netmiko/fortinet/fortinet_ssh.py
@@ -20,14 +20,14 @@ class FortinetSSH(NoConfig, CiscoSSHConnection):
     def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
 
-        data = self._test_channel_read(pattern="to accept|[#$]")
+        data = self._test_channel_read(pattern="to accept|[>#$]")
         # If "set post-login-banner enable" is set it will require you to press 'a'
         # to accept the banner before you login. This will accept if it occurs
         if "to accept" in data:
             self.write_channel("a\r")
-            self._test_channel_read(pattern=r"[#$]")
+            self._test_channel_read(pattern=r"[>#$]")
 
-        self.set_base_prompt(alt_prompt_terminator="$")
+        self.set_base_prompt(pattern=r"[>#$]")
         self.disable_paging()
 
     def disable_paging(
