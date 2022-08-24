f42ab003868993988cf8651d2d484629f1ab9b46
Vladislav Charikov <charickov@gmail.com>
2022-08-24 15:11:49 +0300

    Fix for black

--- netmiko/cisco/cisco_s200.py
+++ netmiko/cisco/cisco_s200.py
@@ -6,7 +6,6 @@ from netmiko.cisco_base_connection import CiscoSSHConnection
 from netmiko.exceptions import NetmikoAuthenticationException
 
 
-
 class CiscoS200Base(CiscoSSHConnection):
     """
     Support for Cisco SG200 series of devices.
@@ -76,15 +75,14 @@ output:
 """
                 raise NetmikoAuthenticationException(msg)
 
-
     def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
         self.ansi_escape_codes = True
         self._test_channel_read(pattern=r"[>#]")
         self.set_base_prompt()
         # Command not support in SG220
-        #self.set_terminal_width(command="terminal width 511", pattern="terminal")
-        #self.disable_paging(command="terminal datadump")
+        # self.set_terminal_width(command="terminal width 511", pattern="terminal")
+        # self.disable_paging(command="terminal datadump")
 
     def save_config(
         self,
