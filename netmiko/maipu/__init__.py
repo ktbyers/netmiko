de50be311fa2f8c3d9144cc96a9f95ddddc85b2b
alizee2020 <563864783@qq.com>
2022-09-17 20:21:08 +0800

    add maipu netmiko

---
+++ netmiko/maipu/__init__.py
@@ -0,0 +1,4 @@
+from netmiko.maipu.maipu import MaipuSSH
+from netmiko.maipu.maipu import MaipuTelnet
+
+__all__ = ["MaipuSSH", "MaipuTelnet"]
---
+++ netmiko/maipu/maipu.py
@@ -0,0 +1,54 @@
+import time
+from socket import socket
+from typing import Any
+
+from netmiko.cisco_base_connection import CiscoBaseConnection
+from telnetlib import IAC, DO, DONT, WILL, WONT, SB, SE, ECHO, SGA, NAWS, Telnet
+
+
+class MaipuBase(CiscoBaseConnection):
+
+    def session_preparation(self) -> None:
+        """Prepare the session after the connection has been established."""
+        self._test_channel_read(pattern=r"[>#]")
+        self.set_base_prompt()
+        self.enable()
+        self.disable_paging(command="more off")
+        # Clear the read buffer
+        time.sleep(0.3 * self.global_delay_factor)
+        self.clear_buffer()
+
+    def config_mode(
+            self,
+            config_command: str = "configure terminal",
+            pattern: str = "",
+            re_flags: int = 0,
+    ) -> str:
+        """Enter configuration mode."""
+        return super().config_mode(
+            config_command=config_command, pattern=pattern, re_flags=re_flags
+        )
+
+    def check_config_mode(
+        self, check_string: str = ")#", pattern: str = "#", force_regex: bool = False
+    ) -> bool:
+        """
+        Checks if the device is in configuration mode or not.
+        """
+        return super().check_config_mode(check_string=check_string, pattern=pattern)
+
+    def save_config(
+        self, cmd: str = "write", confirm: bool = False, confirm_response: str = ""
+    ) -> str:
+        """Saves Config Using Copy Run Start"""
+        return super().save_config(
+            cmd=cmd, confirm=confirm, confirm_response=confirm_response
+        )
+
+
+class MaipuSSH(MaipuBase):
+    pass
+
+
+class MaipuTelnet(MaipuBase):
+	pass
