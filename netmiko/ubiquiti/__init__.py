506fc904d1e7a22619b5dd6f96955a09f8a93f87
elavaud <edouard.lavaud@gmail.com>
2022-09-20 06:59:52 +0700

    Add support for scp on Ubiquiti edgerouter (#2950)
    Co-authored-by: Edouard Lavaud <e.lavaud@unyc.io>

--- netmiko/ubiquiti/__init__.py
+++ netmiko/ubiquiti/__init__.py
@@ -1,9 +1,13 @@
 from netmiko.ubiquiti.edge_ssh import UbiquitiEdgeSSH
-from netmiko.ubiquiti.edgerouter_ssh import UbiquitiEdgeRouterSSH
+from netmiko.ubiquiti.edgerouter_ssh import (
+    UbiquitiEdgeRouterSSH,
+    UbiquitiEdgeRouterFileTransfer,
+)
 from netmiko.ubiquiti.unifiswitch_ssh import UbiquitiUnifiSwitchSSH
 
 __all__ = [
     "UbiquitiEdgeRouterSSH",
+    "UbiquitiEdgeRouterFileTransfer",
     "UbiquitiEdgeSSH",
     "UbiquitiUnifiSwitchSSH",
 ]
