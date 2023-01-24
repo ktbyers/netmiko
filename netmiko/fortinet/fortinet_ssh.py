864976e51ca30b58dc5583179b5c691c1284a5c5
Kirk Byers <ktbyers@twb-tech.com>
2023-01-23 16:18:54 -0800

    Adding support for Fortinet V6 and earlier

--- netmiko/fortinet/fortinet_ssh.py
+++ netmiko/fortinet/fortinet_ssh.py
@@ -19,8 +19,8 @@ class FortinetSSH(NoConfig, CiscoSSHConnection):
             "diffie-hellman-group1-sha1",
         )
 
-	def _try_session_preparation(self, force_data: bool = False) -> None:
-		super()._try_session_preparation(force_data=force_data)
+    def _try_session_preparation(self, force_data: bool = False) -> None:
+        super()._try_session_preparation(force_data=force_data)
 
     def session_preparation(self) -> None:
         """Prepare the session after the connection has been established."""
@@ -32,11 +32,39 @@ class FortinetSSH(NoConfig, CiscoSSHConnection):
             self.write_channel("a\r")
             self._test_channel_read(pattern=self.prompt_pattern)
 
-        self.set_base_prompt(alt_prompt_terminator="$")
+        self.set_base_prompt()
+        self._vdoms = self._vdoms_enabled()
+        # Retain how the 'output mode' was original configured.
+        self._original_output_mode = self._get_output_mode()
+        self._output_mode = self._original_output_mode
+        self._os_version = self._determine_os_version()
         self.disable_paging()
 
-        self._vdoms = self._vdoms_enabled()
-        self._output_mode = "unknown"
+    def set_base_prompt(
+        self,
+        pri_prompt_terminator: str = r"#",
+        alt_prompt_terminator: str = r"$",
+        delay_factor: float = 1.0,
+        pattern: Optional[str] = None,
+    ) -> str:
+        if not pattern:
+            pattern = self.prompt_pattern
+        return super().set_base_prompt(
+            pri_prompt_terminator=pri_prompt_terminator,
+            alt_prompt_terminator=alt_prompt_terminator,
+            delay_factor=delay_factor,
+            pattern=pattern,
+        )
+
+    def find_prompt(
+        self, delay_factor: float = 1.0, pattern: Optional[str] = None
+    ) -> str:
+        if not pattern:
+            pattern = self.prompt_pattern
+        return super().find_prompt(
+            delay_factor=delay_factor,
+            pattern=pattern,
+        )
 
     def _vdoms_enabled(self) -> bool:
         """Determine whether virtual domains are enabled or not."""
@@ -48,6 +76,27 @@ class FortinetSSH(NoConfig, CiscoSSHConnection):
             re.search(r"Virtual domain configuration: (multiple|enable)", output)
         )
 
+    def _config_global(self) -> str:
+        """Enter 'config global' mode, raise a ValueError exception on failure."""
+        try:
+            return self._send_command_str(
+                "config global", expect_string=self.prompt_pattern
+            )
+        except Exception:
+            msg = """
+Netmiko may require 'config global' access to properly disable output paging.
+Alternatively you can try configuring 'configure system console -> set output standard'.
+"""
+            raise ValueError(msg)
+
+    def _exit_config_global(self) -> str:
+        """Exit 'config global' mode."""
+        try:
+            return self._send_command_str("exit", expect_string=self.prompt_pattern)
+        except Exception:
+            msg = "Unable to properly exit 'config global' mode."
+            raise ValueError(msg)
+
     def disable_paging(
         self,
         command: str = "terminal length 0",
@@ -57,70 +106,103 @@ class FortinetSSH(NoConfig, CiscoSSHConnection):
     ) -> str:
         """Disable paging is only available with specific roles so it may fail."""
 
-        if self._output_mode == "unknown":
-            # Save the state of output paging
-            self._retrieve_output_mode()
-
+        output = ""
         if self._output_mode == "standard":
             # Do nothing - already correct.
             return ""
 
         if self._vdoms:
-            vdom_additional_command = "config global"
-            try:
-                output = self._send_command_str(
-                    vdom_additional_command, expect_string=self.prompt_pattern
-                )
-            except Exception:
-                msg = """
-Netmiko requires 'config global' access to properly disable output paging
-(when VDOMs are enabled). Alternatively you can configure 'set output standard'
-and Netmiko should detect this.
-"""
-                raise ValueError(msg)
-
+            output += self._config_global()
         disable_paging_commands = [
             "config system console",
             "set output standard",
             "end",
         ]
-        # There is an extra 'end' required if in multi-vdoms are enabled
-        if self._vdoms:
-            disable_paging_commands.append("end")
         output += self.send_multiline(
             disable_paging_commands, expect_string=self.prompt_pattern
         )
+        self._output_mode = "standard"
+
+        if self._vdoms:
+            output += self._exit_config_global()
         return output
 
-    def _retrieve_output_mode(self) -> None:
-        """Save the state of the output mode so it can be reset at the end of the session."""
-        pattern = r"output\s+:\s+(?P<mode>.*)\s+\n"
+    def _determine_os_version(self) -> str:
+        check_command = "get system status | grep Version"
         output = self._send_command_str(
+            check_command, expect_string=self.prompt_pattern
+        )
+        if re.search("^Version: .* (v[78]\.).$", output, flags=re.M):
+            return "v7_or_later"
+        elif re.search("^Version: .* (v[654]\.).$", output, flags=re.M):
+            return "v6_or_earlier"
+        else:
+            raise ValueError("Unexpected FortiOS Version encountered.")
+
+    def _get_output_mode_v6(self) -> str:
+        """
+        FortiOS V6 and earlier.
+        Retrieve the current output mode.
+        """
+        if self._vdoms:
+            self._config_global()
+
+        self._send_command_str(
+            "config system console", expect_string=self.prompt_pattern
+        )
+        output = self._send_command_str(
+            "show full-configuration", expect_string=self.prompt_pattern
+        )
+        self._send_command_str("end", expect_string=self.prompt_pattern)
+
+        if self._vdoms:
+            self._exit_config_global()
+        return output
+
+    def _get_output_mode_v7(self) -> str:
+        """
+        FortiOS V7 and later.
+        Retrieve the current output mode.
+        """
+        return self._send_command_str(
             "get system console", expect_string=self.prompt_pattern
         )
+
+    def _get_output_mode(self) -> str:
+        """Save the state of the output mode so it can be reset at the end of the session."""
+        output = ""
+        # Fortios Version6 does not support 'get system console'
+        if "v6" in self._os_version:
+            output = self._get_output_mode_v6()
+        elif "v7" in self._os_version:
+            output = self._get_output_mode_v7()
+        else:
+            raise ValueError("Unexpected FortiOS Version encountered.")
+
+        # Search for 'output' pattern in results
+        pattern = r"output\s+:\s+(?P<mode>.*)\s+\n"
         result_mode_re = re.search(pattern, output)
         if result_mode_re:
             result_mode = result_mode_re.group("mode").strip()
             if result_mode in ["more", "standard"]:
-                self._output_mode = result_mode
-            else:
-                raise ValueError(
-                    "Unable to determine the output mode on the Fortinet device."
-                )
+                return result_mode
+
+        raise ValueError("Unable to determine the output mode on the Fortinet device.")
 
     def cleanup(self, command: str = "exit") -> None:
         """Re-enable paging globally."""
         output = ""
-        if self._output_mode == "more":
-            commands = []
+        if self._original_output_mode == "more":
             if self._vdoms:
-                commands = ["config global"]
-            commands += [
+                output += self._config_global()
+            commands = [
                 "config system console",
                 "set output more",
                 "end",
             ]
             output += self.send_multiline(commands, expect_string=self.prompt_pattern)
+            if self._vdoms:
+                self._exit_config_global()
         return super().cleanup(command=command)
 
     def save_config(
