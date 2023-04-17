702a0540ffe82de310056e1348065e6dfecfb1a7
Kirk Byers <ktbyers@twb-tech.com>
2023-04-17 16:57:23 -0700

    Adding type hints into PR. Making custom methods be private

--- netmiko/teldat/teldat.py
+++ netmiko/teldat/teldat.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Any, Union, Sequence, Iterator, TextIO
 from netmiko.base_connection import BaseConnection
 from netmiko.no_enable import NoEnable
 import time
@@ -49,18 +49,24 @@ class TeldatBase(NoEnable, BaseConnection):
         self.write_channel(command + self.RETURN)
         self.write_channel("yes" + self.RETURN)
 
-    def _check_monitor_mode(self, check_string: str="+", pattern: str = "") -> bool:
+    def _check_monitor_mode(self, check_string: str = "+", pattern: str = "") -> bool:
         return super().check_config_mode(check_string=check_string, pattern=pattern)
 
     def check_config_mode(
         self, check_string: str = ">", pattern: str = "", force_regex: bool = False
     ) -> bool:
-        return super().check_config_mode(check_string=check_string, pattern=pattern, force_regex=force_regex)
+        return super().check_config_mode(
+            check_string=check_string, pattern=pattern, force_regex=force_regex
+        )
 
-    def check_running_config_mode(self, check_string="$", pattern=None):
+    def _check_running_config_mode(
+        self, check_string: str = "$", pattern: str = ""
+    ) -> bool:
         return super().check_config_mode(check_string=check_string, pattern=pattern)
 
-    def monitor_mode(self, monitor_command="p 3", pattern=r"\+", re_flags=0):
+    def _monitor_mode(
+        self, monitor_command: str = "p 3", pattern: str = r"\+", re_flags: int = 0
+    ) -> str:
         """
         Enter into monitor_mode.
         On Teldat devices always go to base mode before entering other modes
@@ -82,17 +88,18 @@ class TeldatBase(NoEnable, BaseConnection):
             raise ValueError("Failed to enter monitor mode.")
         return output
 
-    def config_mode(self, config_command="p 4", pattern="onfig>", re_flags=0):
-        # TODO: consider reusing monitor_mode
+    def config_mode(
+        self, config_command: str = "p 4", pattern: str = "onfig>", re_flags: int = 0
+    ) -> str:
         self._base_mode()
         return super().config_mode(
             config_command=config_command, pattern=pattern, re_flags=re_flags
         )
 
-    def running_config_mode(self, config_command="p 5", pattern=r"onfig\$", re_flags=0):
-        """
-        Enter running config mode
-        """
+    def _running_config_mode(
+        self, config_command: str = "p 5", pattern: str = r"onfig\$", re_flags: int = 0
+    ) -> str:
+        """Enter running config mode."""
         self._base_mode()
 
         output = ""
@@ -102,42 +109,41 @@ class TeldatBase(NoEnable, BaseConnection):
             output += self.read_until_pattern(pattern=re.escape(config_command.strip()))
         if not re.search(pattern, output, flags=re_flags):
             output += self.read_until_pattern(pattern=pattern, re_flags=re_flags)
-        if not self.check_running_config_mode():
+        if not self._check_running_config_mode():
             raise ValueError("Failed to enter running config mode.")
         return output
 
     def send_config_set(
         self,
-        config_commands=None,
-        enter_config_mode=True,
-        config_mode_command=None,
-        exit_config_mode=False,
-        **kwargs,
-    ):
+        config_commands: Union[str, Sequence[str], Iterator[str], TextIO, None] = None,
+        *,
+        exit_config_mode: bool = False,
+        **kwargs: Any,
+    ) -> str:
         """
-        For Teldat devices allways enter config mode
+        For Teldat devices always enter config mode
         """
         return super().send_config_set(
             config_commands=config_commands,
             exit_config_mode=exit_config_mode,
-            config_mode_command=config_mode_command,
-            enter_config_mode=enter_config_mode,
             **kwargs,
         )
 
-    def save_config(self, cmd="save yes", confirm=False, confirm_response=""):
-        if not self.check_config_mode() or not self.check_running_config_mode():
+    def save_config(
+        self, cmd: str = "save yes", confirm: bool = False, confirm_response: str = ""
+    ) -> str:
+        if not self.check_config_mode() or not self._check_running_config_mode():
             raise ValueError("Cannot save if not in config or running config mode")
         # Some devices are slow so match on trailing-prompt if you can
-        output = self.send_command(
+        output = self._send_command_str(
             command_string=cmd, strip_prompt=False, strip_command=False
         )
         return output
 
-    def exit_config_mode(self):
+    def exit_config_mode(self, exit_config: str = "", pattern: str = "") -> str:
         return self._base_mode()
 
-    def _base_mode(self, exit_cmd: str ="\x10", pattern: str =r"\*") -> str:
+    def _base_mode(self, exit_cmd: str = "\x10", pattern: str = r"\*") -> str:
         """
         Exit from other modes (monitor, config, running config).
         Send CTRL+P to the device
@@ -157,14 +163,14 @@ class TeldatSSH(TeldatBase):
 class TeldatTelnet(TeldatBase):
     def telnet_login(
         self,
-        pri_prompt_terminator=r"\*",
-        alt_prompt_terminator=r"\*",
-        username_pattern="Username:",
-        pwd_pattern="Password:",
-        delay_factor=1,
-        max_loops=60,
-    ):
-        super().telnet_login(
+        pri_prompt_terminator: str = r"\*",
+        alt_prompt_terminator: str = r"\*",
+        username_pattern: str = "Username:",
+        pwd_pattern: str = "Password:",
+        delay_factor: float = 1.0,
+        max_loops: int = 60,
+    ) -> str:
+        return super().telnet_login(
             pri_prompt_terminator=pri_prompt_terminator,
             alt_prompt_terminator=alt_prompt_terminator,
             username_pattern=username_pattern,
