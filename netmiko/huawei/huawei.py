7e7905067c414c4c2e031fb6fb729a0ecc313fa4
Kirk Byers <ktbyers@twb-tech.com>
2023-04-18 11:31:01 -0700

    Huawei telnet login cleanup

--- netmiko/huawei/huawei.py
+++ netmiko/huawei/huawei.py
@@ -1,5 +1,4 @@
 from typing import Optional, Any, Union, Sequence, Iterator, TextIO
-import time
 import re
 import warnings
 
@@ -166,60 +165,41 @@ class HuaweiTelnet(HuaweiBase):
         max_loops: int = 20,
     ) -> str:
         """Telnet login for Huawei Devices"""
-        delay_factor = self.select_delay_factor(delay_factor)
         output = ""
         return_msg = ""
-        i = 1
-        while i <= max_loops:
-            try:
-                # Search for username pattern / send username
-                output = self.read_until_pattern(
-                    pattern=username_pattern, re_flags=re.I
-                )
+        try:
+            # Search for username pattern / send username
+            output = self.read_until_pattern(pattern=username_pattern, re_flags=re.I)
+            return_msg += output
+            self.write_channel(self.username + self.TELNET_RETURN)
+
+            # Search for password pattern / send password
+            output = self.read_until_pattern(pattern=pwd_pattern, re_flags=re.I)
+            return_msg += output
+            assert self.password is not None
+            self.write_channel(self.password + self.TELNET_RETURN)
+
+            # Waiting for the prompt or password change message
+            output = self.read_until_pattern(pattern=self.prompt_or_password_change)
+            return_msg += output
+
+            # If password change prompt, send "N"
+            if re.search(self.password_change_prompt, output):
+                self.write_channel("N" + self.TELNET_RETURN)
+                output = self.read_until_pattern(pattern=self.prompt_pattern)
                 return_msg += output
-                self.write_channel(self.username + self.TELNET_RETURN)
+                return return_msg
+            elif re.search(self.prompt_pattern, output):
+                return return_msg
 
-                # Search for password pattern / send password
-                output = self.read_until_pattern(pattern=pwd_pattern, re_flags=re.I)
-                return_msg += output
-                assert self.password is not None
-                self.write_channel(self.password + self.TELNET_RETURN)
+            # Should never be here
+            raise EOFError
 
-                # Waiting for combined output
-                output = self.read_until_pattern(pattern=self.prompt_or_password_change)
-                return_msg += output
-
-                # Search for password change prompt, send "N"
-                if re.search(self.password_change_prompt, output):
-                    self.write_channel("N" + self.TELNET_RETURN)
-                    output = self.read_until_pattern(pattern=self.prompt_pattern)
-                    return_msg += output
-                    return return_msg
-
-                self.write_channel(self.TELNET_RETURN)
-                time.sleep(0.5 * delay_factor)
-                i += 1
-
-            except EOFError:
-                assert self.remote_conn is not None
-                self.remote_conn.close()
-                msg = f"Login failed: {self.host}"
-                raise NetmikoAuthenticationException(msg)
-
-        # Last try to see if we already logged in
-        self.write_channel(self.TELNET_RETURN)
-        time.sleep(0.5 * delay_factor)
-        output = self.read_channel()
-        return_msg += output
-        if re.search(pri_prompt_terminator, output, flags=re.M) or re.search(
-            alt_prompt_terminator, output, flags=re.M
-        ):
-            return return_msg
-
-        assert self.remote_conn is not None
-        self.remote_conn.close()
-        msg = f"Login failed: {self.host}"
-        raise NetmikoAuthenticationException(msg)
+        except EOFError:
+            assert self.remote_conn is not None
+            self.remote_conn.close()
+            msg = f"Login failed: {self.host}"
+            raise NetmikoAuthenticationException(msg)
 
 
 class HuaweiVrpv8SSH(HuaweiSSH):
