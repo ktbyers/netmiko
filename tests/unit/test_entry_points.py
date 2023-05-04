65fca7dcd87cfcaa28bb87d4e91c3ee9f393d476
Kirk Byers <ktbyers@twb-tech.com>
2023-05-04 09:58:10 -0700

    Add tests to verify the entry points are installed

---
+++ tests/unit/test_entry_points.py
@@ -0,0 +1,12 @@
+# ChatGPT generated (and then modified)
+import subprocess
+
+def test_entry_points():
+    cmds = [
+        "netmiko-grep",
+        "netmiko-cfg",
+        "netmiko-show",
+    ]
+    for cmd in cmds:
+        r = subprocess.run(['poetry', 'run', cmd, "--help"], capture_output=True)
+        assert r.returncode == 0
