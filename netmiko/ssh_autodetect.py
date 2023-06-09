20e112c8f37689db4df011938a2b8c43f9d6b307
Kirk Byers <ktbyers@twb-tech.com>
2023-06-09 13:21:05 -0700

    Fix linting

--- netmiko/ssh_autodetect.py
+++ netmiko/ssh_autodetect.py
@@ -218,7 +218,10 @@ SSH_MAPPER_DICT = {
     },
     "extreme_slx": {
         "cmd": "show version",
-        "search_patterns": [r"SLX-OS Operating System Software", r"SLX-OS Operating System"],
+        "search_patterns": [
+            r"SLX-OS Operating System Software",
+            r"SLX-OS Operating System",
+        ],
         "priority": 99,
         "dispatch": "_autodetect_std",
     },
