bacf03f4e209225ef167af542d4a5b1b81d45b49
yone2ks <t.yoneda.pub@gmail.com>
2024-05-01 04:15:01 +0900

    add cisco apic device

---
+++ netmiko/cisco/cisco_apic.py
@@ -0,0 +1,12 @@
+"""Subclass specific to Cisco APIC."""
+from netmiko.linux.linux_ssh import LinuxSSH
+
+
+class CiscoApicSSH(LinuxSSH):
+    """
+    Subclass specific to Cisco APIC.
+
+    This class inherit from LinuxSSH because Cisco APIC is based on Linux
+    """
+
+    pass
--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -32,6 +32,7 @@ from netmiko.centec import CentecOSSSH, CentecOSTelnet
 from netmiko.checkpoint import CheckPointGaiaSSH
 from netmiko.ciena import CienaSaosSSH, CienaSaosTelnet, CienaSaosFileTransfer
 from netmiko.cisco import CiscoAsaSSH, CiscoAsaFileTransfer
+from netmiko.cisco import CiscoApicSSH
 from netmiko.cisco import CiscoFtdSSH
 from netmiko.cisco import (
     CiscoIosSSH,
@@ -181,6 +182,7 @@ CLASS_MAPPER_BASE = {
     "centec_os": CentecOSSSH,
     "ciena_saos": CienaSaosSSH,
     "cisco_asa": CiscoAsaSSH,
+    "cisco_apic": CiscoApicSSH,
     "cisco_ftd": CiscoFtdSSH,
     "cisco_ios": CiscoIosSSH,
     "cisco_nxos": CiscoNxosSSH,
