e451e159219eb08de5aac6233c8a9807de42c935
Iliya Iliev <iliya.iliev@x3me.net>
2024-08-05 15:44:32 +0300

    Fix op_eolt_9702 exit from config mode

--- netmiko/optilink/op_eolt_9702.py
+++ netmiko/optilink/op_eolt_9702.py
@@ -1,3 +1,6 @@
+import re
+import time
+from typing import Optional
 from netmiko.cisco_base_connection import CiscoBaseConnection
 
 
@@ -6,10 +9,39 @@ class OptilinkEOLT9702Base(CiscoBaseConnection):
         self._test_channel_read(pattern=r"[>#]")
         self.set_base_prompt()
         self.enable()
-        self.config_mode(config_command="config")
+        self.config_mode()
         self.send_command("vty output show-all")
-        self.exit_config_mode(exit_config="exit")
-        self.exit_enable_mode(exit_command="exit")
+        self.exit_config_mode()
+        self.exit_enable_mode()
+
+    def config_mode(
+        self,
+        config_command: str = "config",
+        pattern: str = "",
+        re_flags: int = 0,
+    ) -> str:
+        return super().config_mode(
+            config_command=config_command, pattern=pattern, re_flags=re_flags
+        )
+
+    def check_enable_mode(self, check_string: str = "#") -> bool:
+        self.write_channel(self.RETURN)
+        output = self.read_until_prompt(read_entire_line=True)
+        return check_string in output
+
+    def exit_enable_mode(self, exit_command: str = "exit") -> str:
+        output = ""
+        if self.check_enable_mode():
+            self.write_channel(self.normalize_cmd(exit_command))
+            self.find_prompt()
+            output += self.read_until_prompt()
+            if self.check_enable_mode():
+                raise ValueError("Failed to exit enable mode.")
+        return output
+
+    def exit_config_mode(self, exit_config: str = "exit", pattern: str = r"#.*") -> str:
+        """Exit from configuration mode."""
+        return super().exit_config_mode(exit_config=exit_config, pattern=pattern)
 
 
 class OptilinkEOLT9702Telnet(OptilinkEOLT9702Base):
--- netmiko/ssh_dispatcher.py
+++ netmiko/ssh_dispatcher.py
@@ -372,10 +372,10 @@ CLASS_MAPPER["tplink_jetstream_telnet"] = TPLinkJetStreamTelnet
 CLASS_MAPPER["yamaha_telnet"] = YamahaTelnet
 CLASS_MAPPER["zte_zxros_telnet"] = ZteZxrosTelnet
 CLASS_MAPPER["maipu_telnet"] = MaipuTelnet
-CLASS_MAPPER["op_golt_944"] = OptilinkGOLT944Telnet
-CLASS_MAPPER["op_golt_924"] = OptilinkGOLT92416Telnet
-CLASS_MAPPER["op_eolt_1144"] = OptilinkEOLT1144Telnet
-CLASS_MAPPER["op_eolt_9702"] = OptilinkEOLT9702Telnet
+CLASS_MAPPER["optilink_golt_944_telnet"] = OptilinkGOLT944Telnet
+CLASS_MAPPER["optilink_golt_924_telnet"] = OptilinkGOLT92416Telnet
+CLASS_MAPPER["optilink_eolt_1144_telnet"] = OptilinkEOLT1144Telnet
+CLASS_MAPPER["optilink_eolt_9702_telnet"] = OptilinkEOLT9702Telnet
 
 # Add serial drivers
 CLASS_MAPPER["cisco_ios_serial"] = CiscoIosSerial
