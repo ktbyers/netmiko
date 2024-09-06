fd0e8a31bbd8ec5ffd09a9b6fe9b9064324a150c
wangyuqi <wangyuqi@asterfusion.com>
2024-09-06 17:54:36 +0800

    A few changes to pass the type check
    Signed-off-by: wangyuqi <wangyuqi@asterfusion.com>

--- netmiko/asterfusion/asterfusion.py
+++ netmiko/asterfusion/asterfusion.py
@@ -5,22 +5,37 @@ AsterNOS platforms running Enterprise SONiC Distribution by AsterFusion Co.
 from netmiko import log
 from netmiko.cisco_base_connection import CiscoSSHConnection
 from netmiko.no_enable import NoEnable
+from typing import (
+    Optional,
+    Any,
+    List,
+    Dict,
+    Sequence,
+    Iterator,
+    TextIO,
+    Union,
+    TYPE_CHECKING,
+)
+
 
 import threading
 import time
 
+if TYPE_CHECKING:
+    from os import PathLike
+
 
 class AsterfusionBase(NoEnable, CiscoSSHConnection):
-    def __init__(self, cli_mode: str = "klish", **kwargs) -> None:
+    def __init__(self, cli_mode: str = "klish", **kwargs: Any) -> None:
         self.cli_mode = cli_mode
         super().__init__(**kwargs)
         self.close_thread_alive_check = False
         self.thread_alive()
 
-    def thread_alive(self):
+    def thread_alive(self) -> None:
         check_alive_time = 120  # 120s send is_alive chr
 
-        def send_alive():
+        def send_alive() -> None:
             time.sleep(check_alive_time)  # wait connection is initialised
             while True:
                 if self.close_thread_alive_check:
@@ -39,7 +54,7 @@ class AsterfusionBase(NoEnable, CiscoSSHConnection):
         th = threading.Thread(target=send_alive, daemon=True)
         th.start()
 
-    def close_alive_thread(self):
+    def close_alive_thread(self) -> None:
         self.close_thread_alive_check = True
 
     def session_preparation(self) -> None:
@@ -75,8 +90,19 @@ class AsterfusionBase(NoEnable, CiscoSSHConnection):
         log.debug(f"Host {self.host} {self.cli_mode} Enter vtysh shell")
         return self._send_command_str("vtysh", expect_string=r"\#")
 
-    def disable_paging(self) -> str:
-        return super().disable_paging(command="terminal raw-output")
+    def disable_paging(
+        self,
+        command: str = "terminal raw-output",
+        delay_factor: Optional[float] = None,
+        cmd_verify: bool = True,
+        pattern: Optional[str] = None,
+    ) -> str:
+        return super().disable_paging(
+            command=command,
+            delay_factor=delay_factor,
+            cmd_verify=cmd_verify,
+            pattern=pattern,
+        )
 
     def write_config(self) -> str:
         return super().save_config(cmd="write", confirm=True, confirm_response="y")
@@ -87,45 +113,58 @@ class AsterfusionSSH(AsterfusionBase):
     def send_command(
         self,
         command_string: str,
-        root_pwd: str = None,
-        **kwargs,
-    ):
+        expect_string: Optional[str] = None,
+        read_timeout: float = 10.0,
+        delay_factor: Optional[float] = None,
+        max_loops: Optional[int] = None,
+        auto_find_prompt: bool = True,
+        strip_prompt: bool = True,
+        strip_command: bool = True,
+        normalize: bool = True,
+        use_textfsm: bool = False,
+        textfsm_template: Optional[str] = None,
+        use_ttp: bool = False,
+        ttp_template: Optional[str] = None,
+        use_genie: bool = False,
+        cmd_verify: bool = True,
+    ) -> Union[str, List[Any], Dict[str, Any]]:
         log.debug(
             "Host [{}], run cmd [{}] , find prompt: {} ".format(
                 self.host, command_string, self.find_prompt()
             )
         )
-        expect_strings = kwargs.pop("expect_string", None)
         try:
             output = super().send_command(
                 command_string,
                 expect_string=(
-                    expect_strings if expect_strings else r"\$|#|password|sudoers"
+                    expect_string if expect_string else r"\$|#|password|sudoers"
                 ),
-                **kwargs,
+                read_timeout=read_timeout,
+                delay_factor=delay_factor,
+                max_loops=max_loops,
+                auto_find_prompt=auto_find_prompt,
+                strip_prompt=strip_prompt,
+                strip_command=strip_command,
+                normalize=normalize,
+                use_textfsm=use_textfsm,
+                textfsm_template=textfsm_template,
+                use_ttp=use_ttp,
+                ttp_template=ttp_template,
+                use_genie=use_genie,
+                cmd_verify=cmd_verify,
             )
-            if "sudoers" in output:
-                raise ValueError("Need to add user to sudo group")
-            if "password" in output:
-                if root_pwd is None:
-                    raise ValueError("Need a root password to execute sudo commands")
-                else:
-                    output_after_sending_pwd = super().send_command_timing(root_pwd)
-                    if "sudoers file" in output_after_sending_pwd:
-                        raise ValueError("Need to add user to sudo group")
-                    output = super().send_command(
-                        command_string,
-                        expect_string=expect_strings if expect_strings else r"\$|#",
-                    )
-
             return output
         except Exception as e:
             log.debug(
                 f"send_sonic_command got Exception, host:{self.host} , self: {self}"
             )
-            return e
+            return str(e)
 
-    def send_config_set(self, config_commands, **kwargs):
+    def send_config_set(
+        self,
+        config_commands: Union[str, Sequence[str], Iterator[str], TextIO, None] = None,
+        **kwargs: Any,
+    ) -> str:
         log.debug(
             "Host [{}], run cmd [{}] , find prompt: {} ".format(
                 self.host, config_commands, self.find_prompt()
@@ -134,7 +173,9 @@ class AsterfusionSSH(AsterfusionBase):
         output = super().send_config_set(config_commands, **kwargs)
         return output
 
-    def send_config_from_file(self, config_file, raise_errors: bool = True, **kwargs):
+    def send_config_from_file(
+        self, config_file: Union[str, bytes, "PathLike[Any]"], **kwargs: Any
+    ) -> str:
         """
         Send configuration commands down the SSH channel from a file.
 
@@ -148,7 +189,7 @@ class AsterfusionSSH(AsterfusionBase):
         :param kwargs: params to be sent to send_config_set method
         """
         log.debug(
-            "Host [{}] run cmd from file [{}] , find prompt: {} ".format(
+            "Host [{!r}] run cmd from file [{!r}] , find prompt: {!r} ".format(
                 self.host, config_file, self.find_prompt()
             )
         )
