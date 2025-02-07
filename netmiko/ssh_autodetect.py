126d5eccaee2105d544c872f768f4c29fc15c18f
Kirk Byers <ktbyers@twb-tech.com>
2025-02-07 11:53:56 -0800

    Update unintentional change

--- netmiko/ssh_autodetect.py
+++ netmiko/ssh_autodetect.py
@@ -79,19 +79,19 @@ SSH_MAPPER_DICT = {
     },
     "arista_eos": {
         "cmd": "show version",
-        "search_patterns": [r"Arista"],
-        "priority": 99,
-        "dispatch": "_autodetect_std",
-    },
-    "aruba_aoscx": {
-        "cmd": "show version",
-        "search_patterns": [r"ArubaOS-CX"],
+        "search_patterns": [r"Arista", r"vEOS"],
         "priority": 99,
         "dispatch": "_autodetect_std",
     },
     "arris_cer": {
         "cmd": "show version",
         "search_patterns": [r"CER"],
+        "priority": 99,
+        "dispatch": "_autodetect_std",
+    },
+    "aruba_aoscx": {
+        "cmd": "show version",
+        "search_patterns": [r"ArubaOS-CX"],
         "priority": 99,
         "dispatch": "_autodetect_std",
     },
