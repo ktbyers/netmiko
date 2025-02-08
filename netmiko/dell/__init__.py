d33f56eb6148b7792b8ed0a5b7f46ba459c2cd0b
Kirk Byers <ktbyers@twb-tech.com>
2025-02-07 17:28:15 -0800

    Dell SCP Sonic (#3579)
    Co-authored-by: aravindmani-1 <aravind_m1@dell.com>

--- netmiko/dell/__init__.py
+++ netmiko/dell/__init__.py
@@ -2,7 +2,7 @@ from netmiko.dell.dell_dnos6 import DellDNOS6SSH
 from netmiko.dell.dell_dnos6 import DellDNOS6Telnet
 from netmiko.dell.dell_force10_ssh import DellForce10SSH
 from netmiko.dell.dell_os10_ssh import DellOS10SSH, DellOS10FileTransfer
-from netmiko.dell.dell_sonic_ssh import DellSonicSSH
+from netmiko.dell.dell_sonic_ssh import DellSonicSSH, DellSonicFileTransfer
 from netmiko.dell.dell_powerconnect import DellPowerConnectSSH
 from netmiko.dell.dell_powerconnect import DellPowerConnectTelnet
 from netmiko.dell.dell_isilon_ssh import DellIsilonSSH
@@ -14,6 +14,7 @@ __all__ = [
     "DellOS10SSH",
     "DellSonicSSH",
     "DellOS10FileTransfer",
+    "DellSonicFileTransfer",
     "DellIsilonSSH",
     "DellDNOS6SSH",
     "DellDNOS6Telnet",
