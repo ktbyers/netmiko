d63503fe2e7c642aab354cb41cccc5f4fe1a1e78
Kirk Byers <ktbyers@twb-tech.com>
2025-03-10 16:17:58 -0700

    Fix commit test behavior

--- tests/test_netmiko_commit.py
+++ tests/test_netmiko_commit.py
@@ -206,21 +206,22 @@ def test_clear_msg(net_connect, commands, expected_responses):
     or since the last commit was made from this session.
     You can use the 'show configuration commit changes'
     command to browse the changes.
-    Do you wish to proceed with this commit anyway? [no]: yes
+    Do you wish to proceed with this commit anyway? [no]:
 
     Clear it
 
-    Doesn't look like this generally occurs during testing. You can make
-    it happen, by doing a commit in another session while this test is
-    in configuration mode.
+    commit() call for XR will raise a ValueError exception upon
+    this event (and send a "no")
     """
     # Setup the initial config state
     config_commands, support_commit, config_verify = retrieve_commands(commands)
 
     if net_connect.device_type == "cisco_xr":
-        output = net_connect.config_mode()
-        output += net_connect.send_config_set(config_commands)
-        output += net_connect.commit()
+        output = net_connect.send_config_set(config_commands)
+        output += net_connect.send_command_expect(
+            "commit", expect_string=r"Do you wish to"
+        )
+        output += net_connect.send_command_expect("yes", auto_find_prompt=False)
 
     assert True
 
