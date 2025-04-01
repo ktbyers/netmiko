628c4f03088e4923dcb624362457f73147f10f05
NivisPluma <matzeminecraft@gmail.com>
2025-04-01 12:15:56 +0200

    feat: add LANCOM LCOS SX 4.3 driver
    This commit adds initial support for LANCOM switches running LCOS SX 4.3 to Netmiko.
    
    Includes:
    - New device type definition
    - Base connection class
    
    Tested against LCOS SX 4.30.0147 RU4 on LANCOM GS-3xxx series switches.

---
+++ netmiko/lancom/lcos_sx_4.py
@@ -0,0 +1,77 @@
+from netmiko.cisco_base_connection import CiscoSSHConnection
+import re
+
+class LancomLcosSx4SSH(CiscoSSHConnection):
+    promt_pattern = r"[#$]"
+
+    def check_config_mode(
+        self,
+        check_string: str = "(config)#",
+        pattern: str = "#",
+        force_regex: bool = False,
+        ) -> bool:
+        """
+        Checks if the device is in configuration mode or not.
+
+        :param check_string: Identification of configuration mode from the device
+        :type check_string: str
+
+        :param pattern: Pattern to terminate reading of channel
+        :type pattern: str
+
+        :param force_regex: Use regular expression pattern to find check_string in output
+        :type force_regex: bool
+
+        :return: True if in configuration mode, False if not
+        """
+        return super().check_config_mode(
+            check_string=check_string, 
+            pattern=pattern, 
+            force_regex=force_regex
+        )
+
+    def cleanup(self, command: str = "logout") -> None:
+        """
+        Cleanup / Gracefully exit the SSH session
+
+        :param command: LANCOM LCOS SX 4.x uses logout to exit the session
+        :type command: str
+        """
+        if self.check_config_mode():
+            command = "do " + command
+        return super().cleanup(command)   
+
+    def save_config(
+        self, 
+        cmd: str = "copy running-config startup-config", 
+        confirm: bool = False, 
+        confirm_response: str = "",
+        output_pattern: str = "Saving \d+ bytes to flash:startup-config"
+        ) -> str:
+        """
+        Save the running Config.
+        
+        :param cmd: The command to send to the device to save the configuration
+        :type cmd: str
+        
+        :param confirm: Whether to confirm the save or not
+        :type confirm: bool
+
+        :param confirm_response: The response to send to the device to confirm the save
+        :type confirm_response: str
+
+        :param output_pattern: The pattern to match the output of the save command
+        :type output_pattern: str
+        """
+
+        # LANCOM does not allow running "Exec" commands in configuration mode
+        if self.check_config_mode():
+            cmd = "do " + cmd
+        save_config_result = self._send_command_str(cmd)
+
+        if not re.search(output_pattern, save_config_result):
+            raise ValueError(
+                f"Saving configuration was unsuccessful. Device said: {save_config_result}"
+            )
+        return save_config_result
+    pass
