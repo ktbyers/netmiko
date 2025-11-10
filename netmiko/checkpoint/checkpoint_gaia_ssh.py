336487b620b7a48603b260791bfb530223e01caa
Kirk Byers <ktbyers@twb-tech.com>
2025-11-10 12:04:23 -0800

    Working on check point gaia expert mode

--- netmiko/checkpoint/checkpoint_gaia_ssh.py
+++ netmiko/checkpoint/checkpoint_gaia_ssh.py
@@ -1,8 +1,10 @@
 import time
 import re
+from typing import Optional
 
 from netmiko.no_config import NoConfig
 from netmiko.base_connection import BaseConnection
+from netmiko.exceptions import ReadTimeout
 
 
 class CheckPointGaiaSSH(NoConfig, BaseConnection):
@@ -13,12 +15,20 @@ class CheckPointGaiaSSH(NoConfig, BaseConnection):
 
     prompt_pattern = r"[>#]"
 
+#    def __init__(self, *args, **kwargs) -> None:
+#        default_enter = kwargs.get("default_enter") or "\r\n"
+#        kwargs["default_enter"] = default_enter
+#        return super().__init__(*args, **kwargs)
+
     def session_preparation(self) -> None:
         """
         Prepare the session after the connection has been established.
 
         Set the base prompt for interaction ('>').
         """
+        # Kept running into issues with command_echo and duplicate
+        # echoes of commands.
+        self.fast_cli = False
         self._test_channel_read(pattern=self.prompt_pattern)
         self.set_base_prompt()
         self.disable_paging(command="set clienv rows 0")
@@ -27,26 +37,86 @@ class CheckPointGaiaSSH(NoConfig, BaseConnection):
         time.sleep(0.3 * self.global_delay_factor)
         self.clear_buffer()
 
-    def command_echo_read(self, cmd: str, read_timeout: float) -> str:
-        """Check Point clish double echoes the command (at least sometimes)"""
+    def check_enable_mode(self, check_string: str = "#") -> bool:
+        """Check if in enable mode. Return boolean."""
+        return super().check_enable_mode(check_string=check_string)
 
-        re_cmd = re.escape(cmd)
-        pattern = rf"{self.prompt_pattern}\s{re_cmd}"
+    def enable(
+        self,
+        cmd: str = "expert",
+        pattern: str = r"expert password",
+        enable_pattern: Optional[str] = r"\#",
+        check_state: bool = True,
+        re_flags: int = re.IGNORECASE,
+    ) -> str:
+        """
+        Enter expert mode.
 
-        # Make sure you read until you detect the command echo (avoid getting out of sync)
-        new_data = self.read_until_pattern(pattern=pattern, read_timeout=read_timeout)
+        Check Point Gaia is very finicky on the timing of sending this 'expert' password.
+        """
+        output = ""
+        msg = (
+            "Failed to enter enable mode. Please ensure you pass "
+            "the 'secret' argument to ConnectHandler."
+        )
 
-        # There can be echoed prompts that haven't been cleared before the cmd echo
-        # this can later mess up the trailing prompt pattern detection. Clear this out.
-        lines = new_data.split(cmd)
-        if len(lines) in [2, 3]:
-            # lines[-1] should realistically just be the null string
-            new_data = f"{cmd}{lines[-1]}"
-        else:
-            # cmd exists in the output multiple times? Just retain the original output
-            pass
+        # Check if in enable mode already.
+        if check_state and self.check_enable_mode():
+            return output
 
-        return new_data
+        # Send "enable" mode command
+        self.write_channel(self.normalize_cmd(cmd))
+        try:
+            # Read the command echo
+            if self.global_cmd_verify is not False:
+                output += self.read_until_pattern(pattern=re.escape(cmd.strip()))
+
+            # Gaia is really tricky as it frequently double echoes the cmd
+            try:
+                tmp_pattern = rf"(?:>\s{re.escape(cmd.strip())}|{pattern})"
+                output += self.read_until_pattern(pattern=tmp_pattern, read_timeout=3)
+            except ReadTimeout:
+                # No double echo / no prompt to enter password (give up).
+                raise ValueError(msg)
+
+            print(output)
+
+            # Must have hit double echo
+            if not re.search(pattern, output):
+                time.sleep(.3)
+                # Search for trailing prompt or password pattern
+                output += self.read_until_prompt_or_pattern(
+                    pattern=pattern, re_flags=re_flags, read_entire_line=True
+                )
+
+            print(output)
+
+            # Send the "secret" in response to password pattern
+            if re.search(pattern, output, flags=re_flags):
+                self.write_channel(self.secret)
+                time.sleep(.5)
+                self.write_channel("\r")
+
+            import ipdb; ipdb.set_trace()
+            # Search for terminating pattern if defined
+            if enable_pattern:
+                output += self.read_until_pattern(pattern=enable_pattern)
+            else:
+                output += self.read_until_prompt()
+                if not self.check_enable_mode():
+                    raise ValueError(msg)
+
+        except NetmikoTimeoutException:
+            raise ValueError(msg)
+
+        self.set_base_prompt()
+        return output
+
+    def exit_enable_mode(self, exit_command: str = "exit") -> str:
+        """Exits enable (privileged exec) mode."""
+        output = super().exit_enable_mode(exit_command=exit_command)
+        self.set_base_prompt()
+        return output
 
     def save_config(
         self, cmd: str = "", confirm: bool = False, confirm_response: str = ""
