18802ba9149185e1f447a9e023c10fe02e985609
Kirk Byers <ktbyers@twb-tech.com>
2025-11-15 16:44:52 -0800

    Add note

--- netmiko/moxa/moxa_nos.py
+++ netmiko/moxa/moxa_nos.py
@@ -1,3 +1,13 @@
+"""
+Tested with
+
+EDS-508A
+EDS-516A
+
+Note:
+This only works in CLI mode. If the device is in Menu mode, you need to change that first.
+"""
+
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
